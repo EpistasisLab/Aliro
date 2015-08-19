7cdecbd0ba5537efb288c1ba447169cc299b2d5e
Kaixhin <design@kaixhin.com>
2015-08-19 15:27:39 +0100

    Use existing info or register new specs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* Modules */
 require("dotenv").config({silent: true}); // Load configuration variables
 var os = require("os");
+var fs = require("fs");
 var spawnSync = require("child_process").spawnSync;
 var http = require("http");
 var bytes = require("bytes");
@@ -14,35 +15,40 @@ app.use(morgan("tiny")); // Log requests
 
 /* Machine specifications */
 var specs = {};
-// Operating System details
-specs.os = {
-  type: os.type(),
-  platform: os.platform(),
-  arch: os.arch(),
-  release: os.release()
-};
-// CPU models
-specs.cpus = os.cpus().map(function(cpu) {return cpu.model;})
-// Total RAM
-specs.mem = bytes(os.totalmem());
-// GPU models
-specs.gpus = [];
-var lspci = spawnSync("lspci", []);
-var grep = spawnSync("grep", ["-i", "vga"], {input: lspci.stdout});
-var gpuStrings = grep.stdout.toString().split("\n");
-for (var i = 0; i < gpuStrings.length - 1; i++) {
-  specs.gpus.push(gpuStrings[i].replace(/.*controller: /g, ""));
-}
-
-/* Registration */
-/*
-request({uri: process.env.FGLAB_URL + "/machines", method: "POST", json: specs}, function(err, response, body) {
-  if (err) {
-    return console.log(err);
+try {
+  // Attempt to read existing specs
+  specs = JSON.parse(fs.readFileSync("specs.json", "utf-8"));
+} catch (err) {
+  specs = {
+    hostname: os.hostname(),
+    os: {
+      type: os.type(),
+      platform: os.platform(),
+      arch: os.arch(),
+      release: os.release()
+    },
+    cpus: os.cpus().map(function(cpu) {return cpu.model;}),
+    mem: bytes(os.totalmem()),
+    gpus: []
+  };
+  // GPU models
+  specs.gpus = [];
+  var lspci = spawnSync("lspci", []);
+  var grep = spawnSync("grep", ["-i", "vga"], {input: lspci.stdout});
+  var gpuStrings = grep.stdout.toString().split("\n");
+  for (var i = 0; i < gpuStrings.length - 1; i++) {
+    specs.gpus.push(gpuStrings[i].replace(/.*controller: /g, ""));
   }
-  console.log(body);
-});
-*/
+
+  // Register details
+  request({uri: process.env.FGLAB_URL + "/api/machines", method: "POST", json: specs}, function(err, response, body) {
+    if (err) {
+      return console.log(err);
+    }
+    // Save ID and specs
+    fs.writeFileSync("specs.json", JSON.stringify(body));
+  });
+}
 
 /* HTTP Server */
 var server = http.createServer(app); // Create HTTP server
