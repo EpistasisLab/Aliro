27d4d3bbd8588468039b931bed994bcf1161f4c7
Kaixhin <design@kaixhin.com>
2015-08-19 15:01:50 +0100

    Get machine info

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FGMachine
-Future Gadget Machine
+
+## Requirements
+
+- Linux (for getting GPUs with `lspci`)
+- [Node.js](https://nodejs.org/) >= 0.12
+
+## TODO
+
+- Enable Mac OS X GPU support via `system_profiler`
--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 /* Modules */
 require("dotenv").config({silent: true}); // Load configuration variables
+var os = require("os");
+var spawnSync = require("child_process").spawnSync;
 var http = require("http");
+var bytes = require("bytes");
 var express = require("express");
 var morgan = require("morgan");
+var request = require("request");
 
 /* App instantiation */
 var app = express();
 app.use(morgan("tiny")); // Log requests
+
+/* Machine specifications */
+var specs = {};
+// Operating System details
+specs.os = {
+  type: os.type(),
+  platform: os.platform(),
+  arch: os.arch(),
+  release: os.release()
+};
+// CPU models
+specs.cpus = os.cpus().map(function(cpu) {return cpu.model;})
+// Total RAM
+specs.mem = bytes(os.totalmem());
+// GPU models
+specs.gpus = [];
+var lspci = spawnSync("lspci", []);
+var grep = spawnSync("grep", ["-i", "vga"], {input: lspci.stdout});
+var gpuStrings = grep.stdout.toString().split("\n");
+for (var i = 0; i < gpuStrings.length - 1; i++) {
+  specs.gpus.push(gpuStrings[i].replace(/.*controller: /g, ""));
+}
+
+/* Registration */
+/*
+request({uri: process.env.FGLAB_URL + "/machines", method: "POST", json: specs}, function(err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(body);
+});
+*/
 
 /* HTTP Server */
 var server = http.createServer(app); // Create HTTP server
