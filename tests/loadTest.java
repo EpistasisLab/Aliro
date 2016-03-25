465af3fa9ac2f0e0b2530cdfafba1e7165dce70d
Steven Vitale <svitale@upenn.edu>
2016-03-25 11:56:16 -0400

    working on tests

---
+++ tests/loadTest.java
@@ -0,0 +1,10 @@
+import org.epistasis.emergent.TinyGP;
+
+public class loadTest extends TestCase  {
+	public static void main(String[] args) {
+		TinyGP gp = new TinyGP(oneDfname,s);
+	    //jocl.evolve();
+	    gp.evolve();
+
+	}
+}
