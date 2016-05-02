0f19c7d9bc9e7f48296e2b554895940f5368b3b9
Steven Vitale <svitale@upenn.edu>
2016-05-02 10:24:27 -0400

    adding tests, tomcat stuff

---
+++ src/test/java/hardwareTest.java
@@ -0,0 +1,22 @@
+package test.java;
+import org.epistasis.emergent.*;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class hardwareTest {
+
+	@Test
+	public void testHardwareAdd() {
+
+		Assert.assertEquals(getHardwareAdd(), "Hello World");
+
+	}
+
+	public static String getHardwareAdd() {
+
+		return "Hello World";
+
+	}
+
+}
