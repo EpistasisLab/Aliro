30ecb6df36a05be7acecdd6f136a3788804389a6
Steven Vitale <svitale@upenn.edu>
2016-05-02 10:24:27 -0400

    adding tests, tomcat stuff

---
+++ src/test/java/gpTest.java
@@ -0,0 +1,27 @@
+package test.java;
+import org.epistasis.emergent.*;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class gpTest {
+
+	@Test
+	public void testTinyGp() {
+
+		Assert.assertEquals(getTinyGP(), "Hello World");
+
+	}
+
+	public static String getTinyGP() {
+String oneDfname = "tests/oneD.txt";
+			String twoDfname="tests/twoD.txt";
+			long s = -1;
+TinyGP gp = new TinyGP(oneDfname,s);
+//gp.evolve();
+
+		return "Hello World";
+
+	}
+
+}
