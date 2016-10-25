a326d2d25ca983f30db0bbce59c1ecfc96e5e190
Sharon Tartarone <sharonpt@upenn.edu>
2016-10-25 19:00:21 -0400

    get data from file, process data, run alg, output
    Former-commit-id: d3b233e6a9968cbcf2567dda73b4d4dfb432a70f

--- learn/BernoulliNB/main.py
+++ learn/BernoulliNB/main.py
@@ -7,14 +7,16 @@ import argparse
 import os
 import json
 import urllib3
-import pycurl
 import time
 
 # basedir = '/share/devel/Gp/learn/BernoulliNB/'
-basedir = '/'
+basedir = 'C:/Users/Sharon Tartarone/Box Sync/gp-project/Gp/learn/BernoulliNB/'
 tmpdir = basedir + 'tmp/'
 http = urllib3.PoolManager()
 
+# put this into a utilities.prepare_input() function, which will return the arguments and the data
+# then in the main, we just call util.prepare_input and then run algorithm! 
+# set up a utilities file for this?
 def parse_args():
 	parser = argparse.ArgumentParser('Perform BernoulliNB')
 
@@ -34,6 +36,7 @@ def parse_args():
 
 	return args
 
+# add this to the utilities file, and others for other type functions (float between 0 and 1)
 def bool_type(val):
 	if(val.lower() == 'true'):
 		return True
@@ -42,55 +45,65 @@ def bool_type(val):
 	else:	
 		raise argparse.ArgumentTypeError(val + ' is not a valid boolean value')	
 
-def grab_file(_id):
-	if not os.path.exists(tmpdir + _id):
-		os.makedirs(tmpdir + _id)
+# add this to the utilities file that prepares input?
+def get_input(_id):
+	expdir = tmpdir + _id + '/'
+
+	if not os.path.exists(expdir):
+		os.makedirs(expdir)
 
 	# response = http.request('GET', 'http://lab:5080/api/v1/experiments/' + _id)
 	response = http.request('GET', 'http://localhost:5080/api/v1/experiments/' + _id)
 	jsondata = json.loads(response.data.decode('utf-8'))
 	files = jsondata['_files']
+	input_file = ''
+
+	# do we need the loop if upon submission, there will only be one file?
 	numfiles = 0;
-	file_name = ''
-
-	for x in files:
-		time.sleep(5)
-		file_id = x['_id']
-		file_name = x['filename']
-		c = pycurl.Curl()
-		# c.setopt(c.URL, 'http://lab:5080/api/v1/files/' + file_id)
-		c.setopt(c.URL, 'http://localhost:5080/api/v1/files/' + file_id)
-		with open(tmpdir + file_name, 'wb') as f:
-			c.setopt(c.WRITEFUNCTION, f.write)
-			c.perform()
-			c.close()
+	for file in files:
+		#time.sleep(5) # do we need this?
+		# response = http.request('GET', 'http://lab:5080/api/v1/files/' + file['_id'])
+		response = http.request('GET', 'http://localhost:5080/api/v1/files/' + file['_id'])
+		input_file = expdir + file['filename']
+		with open(input_file, 'w') as f:
+			f.write(response.data)
 			numfiles += 1
+
 	if numfiles == 1:
-		print 'yo'
-		#result = lr(params['penalty'],_id,file_name)
+		return input_file
 	else:
-		result = 0;
+		return 0
 
-	print files	
+def run_algorithm(args, input_file):
+	input_data = np.recfromcsv(input_file, delimiter='\t', dtype=np.float64, case_sensitive=True)
 
-def run_algorithm():
-	# generate data
-	dataset = datasets.load_iris()
-	#dataset = np.genfromtxt('fold_2_testFeatVec.csv', delimiter=',')
-	print dataset
+	target_name = 'relationship'
+	random_state = 42
 
-	X, y = dataset.data, dataset.target
-	# determine constant test_size value, or if it will vary
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=5)
+	features = np.delete(input_data.view(np.float64).reshape(input_data.size, -1),
+						 input_data.dtype.names.index(target_name), axis=1)
 
-	model = BernoulliNB(**self.args)
-	model.fit(X_train, y_train)
-	print model.score(X_test, y_test)
+	training_features, testing_features, training_classes, testing_classes = \
+		train_test_split(features, input_data[target_name], random_state=random_state)
 
-	return model.predict(X_test)
+	model = BernoulliNB(alpha=args['alpha'], binarize=args['binarize'], fit_prior=args['fit_prior'])
+	print model.get_params()
+	model.fit(training_features, training_classes)
+
+	# add other things that we want returned, like time elapsed, etc (look at notes for this). should predict actually be saved?
+	score =  model.score(testing_features, testing_classes)
+	predict = model.predict(testing_features)
+
+	# should i use to list here?
+	return { 'score': score, 'predict': predict.tolist() }
 
 if __name__ == "__main__":
 	args = parse_args()
-	grab_file(args['_id'])
+	input_file = get_input(args['_id'])
+	result = run_algorithm(args, input_file)
 
+	# put this in another variable to access globally, which will work since this is only one experiment!!!
+	expdir = tmpdir + args['_id'] + '/'
 
+	with open(os.path.join(expdir, 'value.json'), 'w') as outfile:
+		json.dump({ '_scores': { 'score': result['score'], 'predict': result['predict'] } }, outfile)
