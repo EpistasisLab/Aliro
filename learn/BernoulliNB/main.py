3432e13015736a772e77e46037bb47163f31e396
Sharon Tartarone <sharonpt@upenn.edu>
2016-10-26 18:10:23 -0400

    moved common functions to Experiment class

--- learn/BernoulliNB/main.py
+++ learn/BernoulliNB/main.py
@@ -1,80 +1,8 @@
 import numpy as np
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.cross_validation import train_test_split
-from sklearn import datasets # just for testing
-import pandas as pd #for file parsing?
-import argparse
-import os
-import json
-import urllib3
-import time
+from utilities import Experiment
 
-# basedir = '/share/devel/Gp/learn/BernoulliNB/'
-basedir = 'C:/Users/Sharon Tartarone/Box Sync/gp-project/Gp/learn/BernoulliNB/'
-tmpdir = basedir + 'tmp/'
-http = urllib3.PoolManager()
-
-# put this into a utilities.prepare_input() function, which will return the arguments and the data
-# then in the main, we just call util.prepare_input and then run algorithm! 
-# set up a utilities file for this?
-def parse_args():
-	parser = argparse.ArgumentParser('Perform BernoulliNB')
-
-	parser.add_argument('--alpha', action='store', dest='alpha', default=1.0, type=float, 
-		help='Additive (Laplace/Lidstone) smoothing parameter (0 for no smoothing).')
-
-	parser.add_argument('--binarize', action='store', dest='binarize', default=0.0, type=float,
-		help='Threshold for binarizing (mapping to booleans) of sample features. If None, input is presumed to already consist of binary vectors.')
-
-	parser.add_argument('--fit_prior', action='store', dest='fit_prior', default=True, type=bool_type,
-		help='Whether to learn class prior probabilities or not. If false, a uniform prior will be used.')
-
-	parser.add_argument('--_id', action='store', dest='_id', default=None, type=str,
-		help="Experiment id in database")
-
-	args = vars(parser.parse_args())
-
-	return args
-
-# add this to the utilities file, and others for other type functions (float between 0 and 1)
-def bool_type(val):
-	if(val.lower() == 'true'):
-		return True
-	elif(val.lower() == 'false'):
-		return False
-	else:	
-		raise argparse.ArgumentTypeError(val + ' is not a valid boolean value')	
-
-# add this to the utilities file that prepares input?
-def get_input(_id):
-	expdir = tmpdir + _id + '/'
-
-	if not os.path.exists(expdir):
-		os.makedirs(expdir)
-
-	# response = http.request('GET', 'http://lab:5080/api/v1/experiments/' + _id)
-	response = http.request('GET', 'http://localhost:5080/api/v1/experiments/' + _id)
-	jsondata = json.loads(response.data.decode('utf-8'))
-	files = jsondata['_files']
-	input_file = ''
-
-	# do we need the loop if upon submission, there should only be one file?
-	numfiles = 0;
-	for file in files:
-		#time.sleep(5) # do we need this?
-		# response = http.request('GET', 'http://lab:5080/api/v1/files/' + file['_id'])
-		response = http.request('GET', 'http://localhost:5080/api/v1/files/' + file['_id'])
-		input_file = expdir + file['filename']
-		with open(input_file, 'w') as f:
-			f.write(response.data)
-			numfiles += 1
-
-	if numfiles == 1:
-		return input_file
-	else:
-		return 0
-
-# should everything up to the run of the algorithm be automated in a utilities function?
 def run_algorithm(args, input_file):
 	input_data = np.recfromcsv(input_file, delimiter='\t', dtype=np.float64, case_sensitive=True)
 
@@ -95,21 +23,11 @@ def run_algorithm(args, input_file):
 	score =  model.score(testing_features, testing_classes)
 	predict = model.predict(testing_features)
 
-	# should i use to list here?
+	# should i use tolist() here?
 	return { 'score': score, 'predict': predict.tolist() }
 
 if __name__ == "__main__":
-	# maybe get input file function first, because here we also get information about the parameters (just for the experiment though)
-	# or maybe not...
-	# FOR PARAMS, do an http request on the project itself to retrieve all the parameter information!!
-	# this will make it easier to support additions to algorithms, if both the input page in the ui and the function itself is based on the json representation
-
-	args = parse_args()
-	input_file = get_input(args['_id'])
+	exp = Experiment('BernoulliNB')
+	args, input_file = exp.get_input()
 	result = run_algorithm(args, input_file)
-
-	# put this in another variable to access globally, which will work since this is only one experiment!!!
-	expdir = tmpdir + args['_id'] + '/'
-
-	with open(os.path.join(expdir, 'value.json'), 'w') as outfile:
-		json.dump({ '_scores': { 'score': result['score'], 'predict': result['predict'] } }, outfile)
+	exp.save_output(result)
