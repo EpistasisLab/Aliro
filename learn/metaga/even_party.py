611cb52ae1802c1906446bfc326a9532f936669f
Weixuan Fu <weixuanf@upenn.edu>
2016-10-27 18:59:18 -0400

    update even party problem
    Former-commit-id: 7f8251bb7890c3b7dc3c3e3df3f5e802320e0afc

--- learn/metaga/even_party.py
+++ learn/metaga/even_party.py
@@ -1,18 +1,3 @@
-#    This file is part of EAP.
-#
-#    EAP is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of
-#    the License, or (at your option) any later version.
-#
-#    EAP is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public
-#    License along with EAP. If not, see <http://www.gnu.org/licenses/>.
-
 import random
 import operator
 
@@ -25,7 +10,7 @@ from deap import tools
 from deap import gp
 
 # Initialize Parity problem input and output matrices
-PARITY_FANIN_M = 6
+PARITY_FANIN_M = 7
 PARITY_SIZE_M = 2**PARITY_FANIN_M
 
 inputs = [None] * PARITY_SIZE_M
@@ -46,36 +31,40 @@ for i in range(PARITY_SIZE_M):
             inputs[i][j] = 0
     outputs[i] = parity
 
-pset = gp.PrimitiveSet("MAIN", PARITY_FANIN_M, "IN")
-pset.addPrimitive(operator.and_, 2)
-pset.addPrimitive(operator.or_, 2)
-pset.addPrimitive(operator.xor, 2)
-pset.addPrimitive(operator.not_, 1)
-pset.addTerminal(1)
-pset.addTerminal(0)
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
+def EvenParty(population_size,generations,crossover_rate,mutation_rate,tournsize,random_state):
+    pset = gp.PrimitiveSet("MAIN", PARITY_FANIN_M, "IN")
+    pset.addPrimitive(operator.and_, 2)
+    pset.addPrimitive(operator.or_, 2)
+    pset.addPrimitive(operator.xor, 2)
+    pset.addPrimitive(operator.not_, 1)
+    pset.addTerminal(1)
+    pset.addTerminal(0)
 
-toolbox = base.Toolbox()
-toolbox.register("expr", gp.genFull, pset=pset, min_=3, max_=5)
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("compile", gp.compile, pset=pset)
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
 
-def evalParity(individual):
-    func = toolbox.compile(expr=individual)
-    return sum(func(*in_) == out for in_, out in zip(inputs, outputs)),
+    toolbox = base.Toolbox()
+    toolbox.register("expr", gp.genFull, pset=pset, min_=3, max_=5)
+    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("compile", gp.compile, pset=pset)
 
-toolbox.register("evaluate", evalParity)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", gp.cxOnePoint)
-toolbox.register("expr_mut", gp.genGrow, min_=0, max_=2)
-toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+    def evalParity(individual):
+        func = toolbox.compile(expr=individual)
+        return sum(func(*in_) == out for in_, out in zip(inputs, outputs)),
 
-def main():
-    random.seed(21)
-    pop = toolbox.population(n=300)
+    toolbox.register("evaluate", evalParity)
+    toolbox.register("select", tools.selTournament, tournsize=tournsize)
+    toolbox.register("mate", gp.cxOnePoint)
+    toolbox.register("expr_mut", gp.genGrow, min_=0, max_=2)
+    toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+    # avoid MemoryError in python
+    toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=40))
+    toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=40))
+
+    random.seed(random_state)
+    pop = toolbox.population(n=population_size)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
@@ -83,9 +72,11 @@ def main():
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
-    algorithms.eaSimple(pop, toolbox, 0.5, 0.2, 40, stats, halloffame=hof)
+    algorithms.eaSimple(pop, toolbox, cxpb=crossover_rate,
+    mutpb=mutation_rate, ngen=generations, stats=stats, halloffame=hof, verbose=True)
 
     return pop, stats, hof
 
 if __name__ == "__main__":
-    main()
+    EvenParty(population_size = 100,generations=100,crossover_rate = 0.5,mutation_rate = 0.2,tournsize = 3,random_state = 42)
+
