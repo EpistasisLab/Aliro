fe0ebdc221c62b49fff4c408cf379f65670d59d3
weixuanfu2016 <weixuanf@upenn.edu>
2016-10-28 17:00:02 -0400

    add func_dict for more universal func

---
+++ learn/metaga/func_dict.py
@@ -0,0 +1,13 @@
+
+
+
+from sr_deapgp import SymbReg, SymbReg_Best_GP_Individual
+from even_party import EvenParty, EvenParty_Best_GP_Individual
+
+
+func_dict={'SymbReg':SymbReg,
+            'EvenParty': EvenParty}
+
+
+fitness_dict={'SymbReg':SymbReg_Best_GP_Individual,
+            'EvenParty': EvenParty_Best_GP_Individual}
