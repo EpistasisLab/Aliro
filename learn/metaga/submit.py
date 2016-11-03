23a2a44e9b7942629c6086f4df597304d71839d2
Weixuan Fu <weixuanfu@modv-vlan533.0299.apn.wlan.wireless-pennnet.upenn.edu>
2016-11-03 16:35:50 -0400

    make metaGA runing FGlab and paralize metaGA 2

--- learn/metaga/submit.py
+++ learn/metaga/submit.py
@@ -7,26 +7,30 @@ import argparse
 import requests
 import time
 from requests_toolbelt.multipart.encoder import MultipartEncoder
-import multiprocessing
+# no need multiprocessing in submit functions
+#import multiprocessing
 
 project_id = '57ffd3c1fa76cb0022258722'
 baseuri='http://lab:5080/api/v1/projects/'+project_id+'/experiment'
 expbase='http://lab:5080/api/v1/experiments/'
 
 
-def launch(population_size,generations):
+#def launch_lowerGP(population_size, generations, crossover_rate, mutation_rate, tournsize):
+# launch_individual
+def launch_lowerGP(individual):
     # Parse arguments
-    parser = argparse.ArgumentParser("Perform Logistic Regression")
+    parser = argparse.ArgumentParser("Perform lower level deapGP")
     parser.add_argument('--_id', dest='_id', default=None)
     params = vars(parser.parse_args())
     multipart_data = MultipartEncoder(
         fields={
-                'population_size': str(population_size),
-                'generations': str(generations),
-                'crossover_rate': '0.1',
-                'mutation_rate': '0.05',
-                'tournsize': '3',
+                'population_size': str(individual[0]),
+                'generations': str(individual[1]),
+                'crossover_rate': str(individual[2]),
+                'mutation_rate': str(individual[3]),
+                'tournsize': str(individual[4]),
                 'random_state': '42',
+                'GPeval': 'True'
                }
         )
 
@@ -45,15 +49,17 @@ def launch(population_size,generations):
             print(exp_data)
         print(exp_status)
         if exp_status == 'running':
-            time.sleep(5)
+            time.sleep(2) # check every 2 seconds
         if exp_status == 'fail':
             break
+    #return exp_data
 
-
+"""
 if __name__ == "__main__":
     jobs = []
+    # launch multiprocessing jobs
     for i in range(5):
-        p = multiprocessing.Process(target=launch, args=(i*100, i*200))
+        p = multiprocessing.Process(target=launch_lowerGP, args=(i*100, i*200))
         jobs.append(p)
         p.start()
-
+"""
