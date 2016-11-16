2f93336043bd1bd424e59a8c1d622ee1cd2fc01d
Weixuan Fu <weixuanfu@modv-vlan533.0442.apn.wlan.wireless-pennnet.upenn.edu>
2016-11-16 18:08:01 -0500

    mega_GA with batch submitting func
    Former-commit-id: 86ea387b98d6fe8a0de71ec3e61155d20b3cb4ea

--- learn/metaga/submit.py
+++ learn/metaga/submit.py
@@ -17,6 +17,11 @@ project_id = '57ffd3c1fa76cb0022258722'
 baseuri='http://'+lab_host+':5080/api/v1/projects/'+project_id+'/experiment'
 expbase='http://'+lab_host+':5080/api/v1/experiments/'
 
+url = 'http://lab:5080/api/v1/projects/'+project_id+'/batch'
+headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+
+
+
 
 #def launch_lowerGP(population_size, generations, crossover_rate, mutation_rate, tournsize):
 # launch_individual
@@ -33,7 +38,6 @@ def launch_lowerGP(individual):
                 'mutation_rate': str(individual[3]),
                 'tournsize': str(individual[4]),
                 'random_state': '42',
-                'GPeval': 'True'
                }
         )
 
@@ -49,6 +53,48 @@ def launch_lowerGP(individual):
         exp_data = exp_response.json()
         exp_status = exp_data['_status']
         if exp_status == 'success' and 'best_fitness_score' in exp_data:
+            #print(exp_data)
+            break
+        #print(exp_status)
+        if exp_status == 'running':
+            time.sleep(2) # check every 2 seconds
+        if exp_status == 'success':
+            time.sleep(2) # check every 2 seconds
+        if exp_status == 'fail':
+            break
+    print(exp_data['best_fitness_score'])
+    return exp_data['best_fitness_score'],
+
+
+def SymbReg_FGlab_submit(population):
+    """
+    Population: A list of individual
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser("Perform lower level deapGP")
+    parser.add_argument('--_id', dest='_id', default=None)
+    param_list = []
+#    params = vars(parser.parse_args())
+    for individual in population:
+        param_set = {
+                'population_size': str(individual[0]),
+                'generations': str(individual[1]),
+                'crossover_rate': str(individual[2]),
+                'mutation_rate': str(individual[3]),
+                'tournsize': str(individual[4]),
+                'random_state': '42'
+                }
+        param_list.append(param_set)
+
+    response = requests.post(url, data=, headers=headers)
+    json_data = response.json()
+    exp_status = 'init'
+    experimenturi =  expbase + _id
+    while (exp_status != 'success'): #or 'best_fitness_score' not in exp_data):  # This constructs an infinite loop
+        exp_response = requests.get(experimenturi)
+        exp_data = exp_response.json()
+        exp_status = exp_data['_status']
+        if exp_status == 'success': #and 'best_fitness_score' in exp_data:
             #print(exp_data)
             break
         #print(exp_status)
