6f7d5e67e1385ce6e08803181f8221114acaca9d
Weixuan Fu <weixuanfu@modv-vlan533.0808.apn.wlan.wireless-pennnet.upenn.edu>
2016-12-06 14:12:59 -0500

    clean code
    Former-commit-id: fee3a47dff2aadafb46c5eba1d53c750e052582c

--- learn/metaga/submit.py
+++ learn/metaga/submit.py
@@ -5,43 +5,41 @@ import pycurl
 import time
 import argparse
 import requests
-import multiprocessing
-import time
 import os
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 basedir='/share/devel/Gp/learn/metaga/'
 tmpdir=basedir+'tmp/'
+param_batch_json = tmpdir + 'batch.json'
+args_list = ["population_size", "generations", "crossover_rate", "mutation_rate", "tournsize"]
+if not os.path.exists(tmpdir):
+    os.makedirs(tmpdir)
 
+#lab_host = os.environ['LAB_HOST']
+# change the problem id to different problem
+project_id = '57ffd3c1fa76cb0022258722'
+baseuri='http://lab:5080/api/v1/projects/'+project_id+'/batch'
+expbase='http://lab:5080/api/v1/batches/'
 
 def SymbReg_FGlab_submit(population):
     """
     Population: A list of individual
     """
-    #lab_host = os.environ['LAB_HOST']
-    # change the problem id to different problem
-    project_id = '57ffd3c1fa76cb0022258722'
-    baseuri='http://lab:5080/api/v1/projects/'+project_id+'/batch'
-    expbase='http://lab:5080/api/v1/batches/'
-    if not os.path.exists(tmpdir):
-        os.makedirs(tmpdir)
     # Parse arguments
     parser = argparse.ArgumentParser("Perform lower level deapGP")
     parser.add_argument('--_id', dest='_id', default=None)
     # a file has all the individual (params_set)
-    param_batch_json = tmpdir + 'batch.json'
+
     param_list_file = open(param_batch_json, 'w')
     param_list_file.write('[')
-    # parameter name settings
-    keylist = ["population_size", "generations", "crossover_rate", "mutation_rate", "tournsize"]
-
+    # make json file for batch submittion
     num_ind = len(population)
 #    params = vars(parser.parse_args())
     inpos = 1
     for individual in population:
         param_list_file.write('{')
-        for key in range(len(keylist)):
-            param_list_file.write("\"{}\" : \"{}\", ".format(keylist[key], str(individual[key])))
+        for key in range(len(args_list)):
+            param_list_file.write("\"{}\" : \"{}\", ".format(args_list[key], str(individual[key])))
         param_list_file.write("\"{}\" : \"{}\" ".format('random_state', '42'))
         if inpos < num_ind:
             param_list_file.write('},\n')
@@ -50,6 +48,7 @@ def SymbReg_FGlab_submit(population):
             param_list_file.write('}')
     param_list_file.write(']\n')
     param_list_file.close()
+
     headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
     response = requests.post(baseuri, data=open(param_batch_json, 'rb'), headers=headers)
     # check after 5 seconds
@@ -83,18 +82,8 @@ def SymbReg_FGlab_submit(population):
     fitnesses = []
     for experiment,individual in zip(exp_data['_experiments'],population):
         tmpdict = experiment['_options']
-        for key in range(len(keylist)):
-            individual[key] = tmpdict[keylist[key]]
+        for key in range(len(args_list)):
+            individual[key] = tmpdict[args_list[key]]
         fitnesses.append(experiment['best_fitness_score'])
     os.system('rm -f {}/batch.json'.format(tmpdir))
     return population, fitnesses
-
-
-
-"""if __name__ == "__main__":
-    jobs = []
-    # launch multiprocessing jobs
-    for i in range(5):
-        p = multiprocessing.Process(target=launch_lowerGP, args=(i*100, i*200))
-        jobs.append(p)
-        p.start()"""
