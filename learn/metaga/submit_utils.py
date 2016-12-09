387bec0f7cbd6a8f8038e9e822a29df4eb311dc2
Weixuan Fu <weixuanfu@modv-vlan533.0281.apn.wlan.wireless-pennnet.upenn.edu>
2016-12-09 12:33:28 -0500

    meta_ga over ML method structure done

--- learn/metaga/submit_utils.py
+++ learn/metaga/submit_utils.py
@@ -6,6 +6,8 @@ import time
 import argparse
 import requests
 import os
+
+from datetime import datetime
 #from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 #http = urllib3.PoolManager()
@@ -15,7 +17,7 @@ from utils_lib.io_utils import Experiment
 basedir='/share/devel/Gp/learn/metaga/'
 tmpdir=basedir+'tmp/'
 # a tmp json file for a meta-population
-param_batch_json = tmpdir + 'batch.json'
+
 if not os.path.exists(tmpdir):
     os.makedirs(tmpdir)
 
@@ -23,18 +25,19 @@ fglab_url = os.environ['FGLAB_URL']
 expbase='{}/api/v1/batches/'.format(fglab_url)
 
 def config_exp(Chosen_ML_algorithms):
-    exp = Experiment(Chosen_ML)
+    exp = Experiment(Chosen_ML_algorithms)
     #args, input_file = exp.get_input()
     # get project id and args_list
     project_id = exp.get_project_id()
     args_list = exp.get_args_list()
     baseuri='{}/api/v1/projects/{}/batch'.format(fglab_url, project_id)
+
     return baseuri, args_list
 
 
-def json_submit(population, param_batch_json, args_list):
+def json_submit_file(population, param_batch_json, args_list, input_file = None):
     # Parse arguments
-    parser = argparse.ArgumentParser("Perform lower level deapGP")
+    parser = argparse.ArgumentParser("Perform lower level ML algorithms")
     parser.add_argument('--_id', dest='_id', default=None)
     # a file has all the individual (params_set)
     param_list_file = open(param_batch_json, 'w')
@@ -45,10 +48,12 @@ def json_submit(population, param_batch_json, args_list):
 #    params = vars(parser.parse_args())
     inpos = 1
     for individual in population:
+        args = list(individual)
         param_list_file.write('{')
-        for key in range(len(args_list)):
-            param_list_file.write("\"{}\" : \"{}\", ".format(args_list[key], str(individual[key])))
-        param_list_file.write("\"{}\" : \"{}\" ".format('random_state', '42'))
+        for arg_name, arg in zip(args_list, args):
+            param_list_file.write("\"{}\" : \"{}\", ".format(arg_name, arg))
+        if input_file: # add input_file argumnet and check on lower_level algorithms
+            param_list_file.write("\"{}\" : \"{}\" ".format('input_file', input_file))
         if inpos < num_ind:
             param_list_file.write('},\n')
             inpos += 1
@@ -57,23 +62,22 @@ def json_submit(population, param_batch_json, args_list):
     param_list_file.write(']\n')
     param_list_file.close()
 
-def FGlab_submit(population, Chosen_ML_algorithms):
+def FGlab_submit(population, Chosen_ML_algorithms, input_file = None):
     """
     Population: A list of individual
-    method: method name (string type)
+    Chosen_ML_algorithms: method name (string type)
     """
 
     baseuri, args_list = config_exp(Chosen_ML_algorithms)
-
-    json_submit(population, param_batch_json, args_list)
+    timestamp = datetime.now().strftime("%y-%m-%d-%H-%M")
+    param_batch_json = '{}batch.json'.format(tmpdir, timestamp)
+    json_submit_file(population, param_batch_json, args_list, input_file = input_file)
     # change the problem id to different problem
 
     headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
     response = requests.post(baseuri, data=open(param_batch_json, 'rb'), headers=headers)
     # check after 5 seconds
     time.sleep(5)
-    #model = MultinomialNB(alpha=args['alpha'], fit_prior=args['fit_prior'])
-	#generate_results(model, input_file, exp.tmpdir, args['_id'])
 
     json_data = response.json()
     # get batch ID
@@ -91,7 +95,7 @@ def FGlab_submit(population, Chosen_ML_algorithms):
         # reset nofinished
         nofinished =  0
         for exp_ind in exps:
-            if 'best_fitness_score' in exp_ind:
+            if '_scores' in exp_ind:
                 nofinished += 1
         if exp_status == 'success' or num_ind == nofinished:
             break
@@ -105,16 +109,6 @@ def FGlab_submit(population, Chosen_ML_algorithms):
         tmpdict = experiment['_options']
         for key in range(len(args_list)):
             individual[key] = tmpdict[args_list[key]]
-        fitnesses.append(experiment['best_fitness_score'])
+        fitnesses.append(experiment['_scores']['accuracy_score'])
     os.system('rm -f {}/batch.json'.format(tmpdir))
     return population, fitnesses
-
-
-
-"""if __name__ == "__main__":
-    jobs = []
-    # launch multiprocessing jobs
-    for i in range(5):
-        p = multiprocessing.Process(target=launch_lowerGP, args=(i*100, i*200))
-        jobs.append(p)
-        p.start()"""
