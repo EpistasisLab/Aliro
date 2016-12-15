15890fa901025c0c175171d2ff81f47b1b81dbed
Weixuan Fu <weixuanfu@modv-vlan533.0435.apn.wlan.wireless-pennnet.upenn.edu>
2016-12-15 15:23:51 -0500

    add output from best ind
    Former-commit-id: 8b4f5438964e4b8132226168beec63e92e783a81

--- learn/metaga/submit_utils.py
+++ learn/metaga/submit_utils.py
@@ -16,6 +16,8 @@ from utils_lib.io_utils import Experiment
 
 basedir='/share/devel/Gp/learn/metaga/'
 tmpdir=basedir+'tmp/'
+http = urllib3.PoolManager()
+
 # a tmp json file for a meta-population
 
 if not os.path.exists(tmpdir):
@@ -110,11 +112,14 @@ def FGlab_submit(population, Chosen_ML_algorithms, input_file = None, pid = 9999
             time.sleep(2) # check every 2 seconds
     # rebuild population based on experiments
     fitnesses = []
+
     #print(exp_data['_experiments'])
     for experiment,individual in zip(exp_data['_experiments'],population):
         tmpdict = experiment['_options']
         for key in range(len(args_list)):
             individual[key] = tmpdict[args_list[key]]
+        # add a exp_id to track result
+        setattr(individual, 'exp_id', experiment['_id'])
         try:
             if 'accuracy_score' in experiment['_scores']:
                 fitnesses.append(experiment['_scores']['accuracy_score'])
@@ -125,3 +130,48 @@ def FGlab_submit(population, Chosen_ML_algorithms, input_file = None, pid = 9999
             fitnesses.append(0.0) # fail experiment may change value later
     os.system('rm -f {}{}'.format(tmpdir, param_batch_json))
     return population, fitnesses
+
+
+"""def FGlab_metaga_best_result(individual, Chosen_ML_algorithms, input_file = None, pid = 9999):
+    """
+    Population: A list of individual
+    Chosen_ML_algorithms: method name (string type)
+    """
+
+    baseuri, args_list = config_exp(Chosen_ML_algorithms)
+    timestamp = datetime.now().strftime("%y-%m-%d-%H-%M")
+    param_batch_json = '{}best_ind_{}_{}.json'.format(tmpdir, pid, timestamp)
+    num_ind = json_submit_file([individual], param_batch_json, args_list, input_file = input_file)
+    # change the problem id to different problem
+    headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+    response = requests.post(baseuri, data=open(param_batch_json, 'rb'), headers=headers)
+    # check after 5 seconds
+    time.sleep(5)
+
+    json_data = response.json()
+    # get batch ID
+    batch_id = json_data['_id']
+    # initial status
+    exp_status = 'init'
+    # batch URI
+    batchuri =  expbase + batch_id
+    nofinished =  0
+    while num_ind != nofinished:
+        exp_response = requests.get(batchuri)
+        exp_data = exp_response.json()
+        exp_status = exp_data['_status']
+        exps = exp_data['_experiments']
+        # reset nofinished
+        nofinished =  0
+        nofail = 0 # Experiment fail maybe due to unsupprot combination
+        for exp_ind in exps:
+            if '_scores' in exp_ind:
+                nofinished += 1
+            elif exp_ind['_status'] == 'fail':
+                nofail += 1
+        if exp_status == 'success' or num_ind == nofinished:
+            break
+        if nofail + nofinished == num_ind:
+            break
+        if exp_status == 'running':
+            time.sleep(2) # check every 2 seconds"""
