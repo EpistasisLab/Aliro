50f37d3e69c2712bf69700699f8b015dbeca2081
Steven Vitale <svitale@upenn.edu>
2017-03-15 13:06:54 -0400

    importing ui branch
    Former-commit-id: 92b218fb7b0ef86e44e31785e3dfaf4d8f85f9fd

--- lab/react-ui/test/reducer_spec.js
+++ lab/react-ui/test/reducer_spec.js
@@ -1,5 +1,37 @@
 import { List, Map, fromJS } from 'immutable';
 import { expect } from 'chai';
 
-//import reducer from '../src/reducer';
+import reducer from '../src/reducer';
+
+beforeEach(function() {
+	const initialState = Map();
+});
+
+describe('reducer', () => {
+
+	/*it('handles SET_DATASETS', () => {
+		console.log(reducer);
+		const initialState = List();
+		const datasets = fromJS([
+           { name: 'Gametes' },
+           { name: 'Adults' },
+           { name: 'Hypothyroid' }
+        ]);
+        const action = {type: 'SET_DATASETS', datasets};
+        const nextState = reducer(initialState, action);
+
+       	expect(nextState.get('datasets')).to.equal(datasets);
+	});*/
+
+	/*it('handles SET_CURRENT_DATASET', () => {
+		const initialState = Map();
+       	const action = {type: 'SET_CURRENT_DATASET', currentDataset: fromJS({name: "Adults"})};
+       	const nextState = reducer(initialState, action);
+
+	 	expect(nextState).to.equal(fromJS({
+	      currentDataset: fromJS({name: "Adults"})
+	    }));
+	});*/
+
+});
 
