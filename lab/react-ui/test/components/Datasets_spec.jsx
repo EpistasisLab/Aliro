92b218fb7b0ef86e44e31785e3dfaf4d8f85f9fd
Steven Vitale <svitale@upenn.edu>
2017-03-15 13:06:54 -0400

    importing ui branch

--- lab/react-ui/test/components/Datasets_spec.jsx
+++ lab/react-ui/test/components/Datasets_spec.jsx
@@ -1,90 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {
     renderIntoDocument,
-    findRenderedDOMComponentWithTag,
     scryRenderedDOMComponentsWithTag,
-    scryRenderedDOMComponentsWithClass,
     Simulate
 } from 'react-addons-test-utils';
 import { fromJS } from 'immutable';
 import { Datasets } from '../../src/components/Datasets';
+import { initialDatasets } from './testValues.js';
 import { expect } from 'chai';
 
 describe('Datasets', () => {
 
-   it('renders the list of available datasets in modal', () => {
-       let datasets = fromJS([
-           { name: 'gametes.csv' },
-           { name: 'adults.csv' },
-           { name: 'hypothyroid.csv' }
-       ]);
-       let currentDataset = new Map();
+   it('renders button for each available dataset', () => {
+       let datasets = fromJS(initialDatasets);
+       let currentDataset = datasets.first();
 
        const component = renderIntoDocument(
            <Datasets datasets={datasets} currentDataset={currentDataset} />
        );
 
-       // simulate click to open modal
-       const button = findRenderedDOMComponentWithTag(component, 'button');
-       Simulate.click(button);
+       const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
-       // must use window.document in order to get elements in modal which is attached to body
-       const modalList = window.document.getElementsByClassName('item');
-
-       expect(modalList.length).to.equal(3);
+       expect(buttons.length).to.equal(3);
+       expect(buttons[0].textContent).to.equal('Gametes');
+       expect(buttons[1].textContent).to.equal('Adults');
+       expect(buttons[2].textContent).to.equal('Hypothyroid');
    });
 
-
-   it('displays selected dataset as none when none selected', () => {
-       let datasets = fromJS([
-           { name: 'gametes.csv' },
-           { name: 'adults.csv' },
-           { name: 'hypothyroid.csv' }
-       ]);
-       let currentDataset = new Map();
+   it('displays selected dataset as active', () => {
+       let datasets = fromJS(initialDatasets);
+       let currentDataset = datasets.first();
 
        const component = renderIntoDocument(
            <Datasets datasets={datasets} currentDataset={currentDataset} />
        );
 
-       // get node that displays selected dataset
-       const selected = scryRenderedDOMComponentsWithClass(component, 'header')[2];
+       const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
-       expect(selected).to.be.ok;
-       expect(selected.textContent).to.contain('none');
+       expect(buttons[0].className).to.include('active');
+       expect(buttons[1].className).to.not.include('active');
+       expect(buttons[2].className).to.not.include('active');
    });
 
-   it('does not display checkmark when no selected dataset', () => {
-       let datasets = fromJS([
-           { name: 'gametes.csv' },
-           { name: 'adults.csv' },
-           { name: 'hypothyroid.csv' }
-       ]);
-       let currentDataset = new Map();
-
-       const component = renderIntoDocument(
-           <Datasets datasets={datasets} currentDataset={currentDataset} />
-       );
-
-       // simulate click to open modal
-       const button = findRenderedDOMComponentWithTag(component, 'button');
-       Simulate.click(button);
-
-       // must use window.document in order to get elements in modal which is attached to body
-       const checkmark = window.document.getElementsByClassName('check');
-
-       expect(checkmark.length).to.equal(0);
-   });
-
-
-   it('displays correct selected dataset upon selection', () => {
-       let datasets = fromJS([
-           { name: 'gametes.csv' },
-           { name: 'adults.csv' },
-           { name: 'hypothyroid.csv' }
-       ]);
-       let currentDataset = new Map();
+   it('updates selected dataset upon user click event', () => {
+       let datasets = fromJS(initialDatasets);
+       let currentDataset = datasets.first();
 
        const setCurrentDataset = (item) => currentDataset = item;
 
@@ -92,18 +52,11 @@ describe('Datasets', () => {
            <Datasets datasets={datasets} currentDataset={currentDataset} setCurrentDataset={setCurrentDataset} />
        );
 
-       // simulate click to open modal
-       const button = findRenderedDOMComponentWithTag(component, 'button');
-       Simulate.click(button);
+       const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
 
-       // must use window.document in order to get elements in modal which is attached to body
-       const modalList = window.document.getElementsByClassName('item');
-
-       // simulate click on a list item
-       const current = modalList[0];
-       console.log(current);
-       Simulate.click(current);
-
+       expect(currentDataset.get('name')).to.equal('Gametes');
+       Simulate.click(buttons[1]);
+       expect(currentDataset.get('name')).to.equal('Adults');
    });
 
 });
