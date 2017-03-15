166447387ff65d368acd40c577f656c1e88b7358
Steven Vitale <svitale@upenn.edu>
2017-03-15 16:30:00 -0400

    adding users
    Former-commit-id: c3770573697037a3819733db64de8add9d04a721

---
+++ lab/examples/Algorithms/MDR/MDR.js
@@ -0,0 +1,14 @@
+{
+ "default-label": { 
+    "description": "Default Label",
+    "type": "enum-radio",
+    "values": [0, 1],
+    "default": 0
+ },
+ "tie-break": { 
+    "description": "Tie Break",
+    "type": "enum-radio",
+    "values": [0, 1],
+    "default": 1
+ }
+}
