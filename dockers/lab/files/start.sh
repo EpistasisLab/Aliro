a1d6c5d63834e481afc5227c74b06206c9af42c2
Sharon Tartarone <sharonpt@upenn.edu>
2017-03-16 13:40:25 -0400

    update to mongoimport

--- dockers/lab/files/start.sh
+++ dockers/lab/files/start.sh
@@ -1,6 +1,6 @@
 mongod -f /etc/mongod.conf &
 mongorestore /dump
 cd /share/devel/Gp/lab
-#mongoimport -d FGLab -c users --file examples/Users/Users.js --type json
+#mongoimport -d FGLab -c users --file examples/Users/Users.json --type json
 pm2 start lab.config.js --watch
 bash
