927c2d71b5c01028a19963b726c76ae4a32a57d8
Sharon Tartarone <sharonpt@upenn.edu>
2017-04-28 18:00:29 -0400

    fixed navbar issues
    Former-commit-id: fb716c1725115bf2d355703b308742d3a7e8086c

--- lab/src/components/App/Navbar/BasicNavbar/index.jsx
+++ lab/src/components/App/Navbar/BasicNavbar/index.jsx
@@ -1,87 +1,56 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
+import { breakpoints } from '../../../../breakpoints';
 import { Menu, Dropdown, Icon } from 'semantic-ui-react';
-import { getMenuItems } from '../menuItems.js';
 import { Link } from 'react-router';
 
 export class BasicNavbar extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-      menu: getMenuItems({user: 'Test'}),
-      minBtnNameWidth: 560,
-      minSubheaderWidth: 768
-    };
+	render() {
+		const userTrigger = (
+			<Menu.Item>
+				<Icon name='user' />
+				<MediaQuery minWidth={breakpoints.MIN_TABLET}>
+					User <Icon name='caret down' />
+				</MediaQuery>
+			</Menu.Item>
+		);
+		return <Menu inverted color='grey' fixed='top' size='large' borderless>
+			<Menu.Item header name='PennAI' />
+			<MediaQuery minWidth={breakpoints.MAX_MOBILE}>
+				<Menu.Item name='Your friendly AI assistant' />
+			</MediaQuery>
+			<Menu.Menu position='right'>
+				<Link to='datasets' className='link' activeClassName='active'> 
+					<Menu.Item name='Datasets'>
+						<Icon name='file text outline' />
+						<MediaQuery minWidth={breakpoints.MIN_TABLET}>
+							Datasets
+						</MediaQuery>
+					</Menu.Item>
+				</Link>
+				<Link to='experiments' className='link' activeClassName='active'> 
+					<Menu.Item name='Experiments'>
+						<Icon name='lab' />
+						<MediaQuery minWidth={breakpoints.MIN_TABLET}>
+							Experiments
+						</MediaQuery>
+					</Menu.Item>
+				</Link>
+				<Dropdown trigger={userTrigger} icon={null}>
+					<Dropdown.Menu>
+						<Dropdown.Item 
+							as={Link} 
+							to='settings'
+							icon='setting' 
+							text='Settings' 
+						/>
+						<Dropdown.Item
+							icon='sign out' 
+							text='Sign out' 
+						/>
+					</Dropdown.Menu>
+				</Dropdown>
+			</Menu.Menu>
+		</Menu>;
 	}
-
-  render() {
-    return <Menu inverted color='grey' fixed='top' size='large' borderless>
-      <Menu.Item header name={this.state.menu.header} />
-      <MediaQuery minWidth={this.state.minSubheaderWidth}>
-        <Menu.Item name={this.state.menu.subheader} />
-      </MediaQuery>
-      <Menu.Menu position='right'>
-        {this.state.menu.items.map(item =>
-          this.renderMenuItem(item)
-        )}
-      </Menu.Menu>
-    </Menu>;
-  }
-
-  renderMenuItem(item) {
-    if(item.type === 'button') {
-      return this.renderButton(item);
-    } else if(item.type === 'dropdown') {
-      return this.renderDropdown(item);
-    }
-  }
-
-  renderButton(item) {
-    return (
-      <Link to={item.path} className='link' activeClassName='active'>
-        <Menu.Item
-          key={item.name}
-          name={item.name}>
-            <Icon name={item.icon} />
-            <MediaQuery minWidth={this.state.minBtnNameWidth}>
-              {item.name}
-            </MediaQuery>
-        </Menu.Item>
-      </Link>
-    );
-  }
-
-  renderDropdown(item) {
-    const trigger = (
-      <Menu.Item>
-        <Icon name={item.icon} />
-        <MediaQuery minWidth={this.state.minBtnNameWidth}>
-          {item.text} <Icon name='caret down' />
-        </MediaQuery>
-      </Menu.Item>
-    );
-
-    return (
-      <Dropdown trigger={trigger} icon={null}>
-        <Dropdown.Menu>
-          {item.items.map(item =>
-            this.renderDropdownItem(item)
-          )}
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  }
-
-  renderDropdownItem(item) {
-    if(item.type === 'button') {
-      return <Dropdown.Item
-        key={item.name} 
-        icon={item.icon} 
-        text={item.name}
-        href={item.path}
-      />;
-    } else if(item.type === 'divider') {
-      return <Dropdown.Divider />;
-    }
-  }
 }
