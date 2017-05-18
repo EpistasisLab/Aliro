d5de610950f3ba05c7201af3a5ea04c6d03ec16e
Sharon Tartarone <sharonpt@upenn.edu>
2017-05-18 12:23:21 -0400

    updates to build experiment
    Former-commit-id: c4798ef6b24250c9dfd08cf18dbadacb1ee294c9

---
+++ lab/src/scenes/Builder/components/SelectedAlgorithm/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Grid, Segment, Header, Button } from 'semantic-ui-react';
+
+export class SelectedAlgorithm extends React.Component {
+	render() {
+
+		const { 
+			algorithms,
+			currentAlgorithm,
+			setCurrentAlgorithm
+		} = this.props;
+
+		const isActive = (algorithm) => {
+			return currentAlgorithm && (algorithm.get('name') === currentAlgorithm.get('name'));
+		};
+
+		const color = 'orange';
+
+		return (
+			<Grid.Column width={16}>
+				<Segment inverted attached="top" className="panel-header">
+					<Header 
+						inverted
+						size="large"
+						color={color} 
+						content="Select Algorithm" 
+					/>
+				</Segment>
+				<Segment inverted attached="bottom">	
+					<Grid columns={3} stackable className="compressed">
+						{algorithms.map(algorithm =>
+							<Grid.Column key={algorithm.get('name')}>
+								<Button
+									inverted 
+									color={color}
+									size="large"
+									fluid
+									active={isActive(algorithm)}
+									onClick={() => setCurrentAlgorithm(algorithm)}
+								>
+									{algorithm.get('name')}
+									<div className="param-count">
+										{algorithm.get('params').size} parameters
+									</div>
+								</Button>
+							</Grid.Column>
+						)}
+					</Grid>	
+				</Segment>
+			</Grid.Column>
+		);
+	}
+}
