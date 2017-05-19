9dbca8e5c322d5198cd137680f5c150761c99f96
Sharon Tartarone <sharonpt@upenn.edu>
2017-05-19 17:04:35 -0400

    finished sorting/filtering
    Former-commit-id: d37381dd2302122f067eb11060ae320e4e6f6fb5

--- lab/src/scenes/Experiments/data/actions.js
+++ lab/src/scenes/Experiments/data/actions.js
@@ -1,8 +1,5 @@
 export const REQUEST_EXPERIMENTS = 'REQUEST_EXPERIMENTS';
 export const RECEIVE_EXPERIMENTS = 'RECEIVE_EXPERIMENTS';
-export const SET_FILTER = 'SET_FILTER';
-export const SET_SORT = 'SET_SORT';
-export const RESET_FILTERS = 'RESET_FILTERS';
 
 export const requestExperiments = () => {
 	return {
@@ -15,27 +12,5 @@ export const receiveExperiments = (json) => {
 		type: RECEIVE_EXPERIMENTS,
 		experiments: json,
 		receivedAt: Date.now()
-	}
-};
-
-export const setFilter = (filter, value) => {
-	return {
-		type: SET_FILTER,
-		filter,
-		value
-	}
-};
-
-export const setSort = (column, direction) => {
-	return {
-		type: SET_SORT,
-		column,
-		direction
-	}
-};
-
-export const resetFilters = () => {
-	return {
-		type: RESET_FILTERS
 	}
 };
