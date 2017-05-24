739690c1939fe7363fcc49f068107df3b9e704a6
lacava <williamlacava@gmail.com>
2017-05-24 11:23:04 -0400

    updates to metalearning parsing. adds ai over-arching agen tot direct calls to recommender and handle api with DB.
    Former-commit-id: eaed5f0fdc8d43315019f0acc8cdb94e1fae81a7

--- metalearning/make_meta_ml_dataset.py
+++ metalearning/make_meta_ml_dataset.py
@@ -5,19 +5,6 @@ import dask.dataframe as dd
 from tqdm import tqdm
 pd.options.mode.chained_assignment = None
 
-# get data metafeatures
-# metafeatures = pd.read_csv('data_metafeatures.csv',sep=',',index_col=0)
-# print('loaded ', metafeatures.shape[1]-1, ' metafeatures for ', metafeatures.shape[0], ' datasets')
-# # get ML results
-print('loading pmlb results data...')
-data = pd.read_csv('sklearn-benchmark5-data.tsv.gz', sep='\t',
-                   names=['dataset',
-                         'classifier',
-                         'parameters',
-                         'accuracy',
-                         'macrof1',
-                         'bal_accuracy']).fillna('')
-# data.describe()
 # function to check if parameter is a number.
 def is_number(s):
     try:
@@ -25,13 +12,13 @@ def is_number(s):
         return True
     except ValueError:
         return False
-
+############################################################## data column types
 column_types = {
     'fit_intercept':bool,
     'loss':str,
     'C':float,
     'alpha':float,
-    'learning_rate':float,
+    'learning_rate':str,
     'penalty':str,
     'l1_ratio':float,
     'eta0':float,
@@ -43,7 +30,7 @@ column_types = {
     'Unnamed: 19':str,
     'degree':int,
     'kernel':str,
-    'gamma':float,
+    'gamma':str,
     'coef0':float,
     'dual':bool,
     'weights':str,
@@ -54,10 +41,27 @@ column_types = {
 }
 
 
+######################################################### get data metafeatures
+# metafeatures = pd.read_csv('data_metafeatures.csv',sep=',',index_col=0)
+# print('loaded ',
+#       metafeatures.shape[1]-1, ' metafeatures for ',
+#       metafeatures.shape[0], ' datasets')
+################################################################# get ML results
+print('loading pmlb results data...')
+data = pd.read_csv('sklearn-benchmark5-data.tsv.gz', sep='\t',
+                   names=['dataset',
+                         'classifier',
+                         'parameters',
+                         'accuracy',
+                         'macrof1',
+                         'bal_accuracy']).fillna('')
+data.describe()
+
+
 ############################################## make typed columns for parameters
 print('converting parameters to features...')
 
-for i,p in tqdm(enumerate(data['parameters'])):
+for i,p in tqdm(enumerate(data['parameters'][:1000])):
     d = dict()
     for ps in p.split(','):
         if is_number(ps.split('=')[-1]):
@@ -83,48 +87,56 @@ for i,p in tqdm(enumerate(data['parameters'])):
                     data[key] = data[key].apply(lambda x: int(x))
 
 
-data.to_csv('sklearn-benchmark5-data-ml-parameters-nan.tsv',
+data.to_csv('sklearn-benchmark5-data-ml-parameters-nan-small.tsv',
             sep='\t',index=False)
 ################################################################# fill in values
-# def write_param(x):
-#     #x: dataframe partition
-#     for i,p in enumerate(x['parameters']):
-#         d = dict()
-#         for ps in p.split(','):
-#             if is_number(ps.split('=')[-1]):
-#                 d[str(ps.split('=')[0])] = float(ps.split('=')[-1])
-#             else:
-#                 d[str(ps.split('=')[0])] = ps.split('=')[-1]
-#
-#         for key, val in d.items():
-#             if key not in x.columns:
-#                 print('key:',key,' not in ',x.columns)
-#             # if type(val) == bool:
-#             #     x[key] = x[key].apply(lambda z: bool(z))
-#             # elif type(val) == str:
-#             #     x[key] = x[key].apply(lambda z: str(z))
-#             # elif type(val) == float:
-#             #
-#             # if type(x[key][0]) is not column_types[key]:
-#             #     x[key] = x[key].apply(lambda z: column_types[key](z))
-#             # x[key][i] = val
-#             if key == 'loss':
-#                 x[key] = x[key].apply(lambda i: str(i))
-#             try:
-#                 x.set_value(i,key,val)
-#             except ValueError:
-#                 print('key:',key,'val:',val)
-#                 raise ValueError
-#             # assert( x[i][key] == val)
-#
-#     return x
-# import pandas as pd
-# from dask import delayed
-# import dask.dataframe as dd
-# pdf = pd.read_csv('sklearn-benchmark5-data-ml-parameters-nan.tsv', sep='\t')
-# df = dd.from_pandas(pdf,npartitions=1000)
-#
-# df2 = df.map_partitions(lambda x: write_param(x))
-#
-# df2.to_csv('sklearn-benchmark5-data-ml-parameters.tsv',
-#             sep='\t',index=False)
+def write_param(x):
+    #x: dataframe partition
+    #gNB has no parameters so ignore
+
+    for i,row in x.iterrows():
+        p = row['parameters']
+        # try:
+        if row['classifier'] == 'GaussianNB' or type(row['parameters'])==str:
+            continue
+        # except KeyError:
+        #     print('x[''parameters'']:',x['parameters'])
+        #     print('i:',i,'p:',p)
+
+        d = dict()
+        try:
+            for ps in p.split(','):
+                if ps.split('=')[0] != '':
+                    if is_number(ps.split('=')[-1]):
+                        d[str(ps.split('=')[0])] = float(ps.split('=')[-1])
+                    else:
+                        d[str(ps.split('=')[0])] = ps.split('=')[-1]
+
+                for key, val in d.items():
+                    if key not in x.columns:
+                        print('key:',key,' not in ',x.columns)
+                        continue
+                    if key == 'loss':
+                        x[key] = x[key].apply(lambda i: str(i))
+                    # try:
+                    x.set_value(i,key,val)
+                    # except ValueError:
+                    #     print('key:',key,'val:',val)
+                    #     raise ValueError
+                    # assert( x[i][key] == val)
+        except Exception:
+            print('i:',i,'p:',p)
+            # print(repr(e))
+            raise ValueError
+    return x
+import pandas as pd
+from dask import delayed
+import dask.dataframe as dd
+print('reading results data...')
+pdf = pd.read_csv('sklearn-benchmark5-data-ml-parameters-nan-small.tsv', sep='\t')
+df = dd.from_pandas(pdf,npartitions=20)
+print('mapping parameters values to data...')
+df2 = df.map_partitions(lambda x: write_param(x))
+
+df2.to_csv('sklearn-benchmark5-data-ml-parameters.tsv',
+            sep='\t',index=False)
