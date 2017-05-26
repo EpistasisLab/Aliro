dd4e6a60a834f3884c2465d4ddd195832d3e2a2f
Sharon Tartarone <sharonpt@upenn.edu>
2017-05-26 14:54:38 -0400

    ai toggle
    Former-commit-id: 73ee0851e86682dc58532f3408d4afb1e4bf07e3

--- lab/src/scenes/Datasets/data/reducer.js
+++ lab/src/scenes/Datasets/data/reducer.js
@@ -1,7 +1,9 @@
 import { fromJS } from 'immutable';
 import { 
     REQUEST_DATASETS,
-    RECEIVE_DATASETS
+    RECEIVE_DATASETS,
+    REQUEST_AI_TOGGLE,
+    RECEIVE_AI_TOGGLE
 } from './actions';
 
 import { initialDatasets } from './initialDatasets'; // for testing
@@ -21,6 +23,19 @@ const datasets = (state = initialState, action) => {
                 isFetching: false,
                 items: action.datasets
             });
+        case REQUEST_AI_TOGGLE:
+            /*return state.items.map({
+                isFetching: false,
+                items: action.datasets
+            });*/
+            console.log(action.datasetId);
+            return state;
+        case RECEIVE_AI_TOGGLE:
+            /*return state.merge({
+                isFetching: false,
+                items: action.datasets
+            });*/
+            return state;
         default:
             return state;
     }
