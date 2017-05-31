2a99dda7ba33b0d1be88ca03c26df52bc58cf097
lacava <williamlacava@gmail.com>
2017-05-31 17:49:47 -0400

    adds test extraction of experiments code directly to ai check_results method
    Former-commit-id: ea3cf74c870e5b81872ec55ab7f8e9b9c964bac2

--- tests/extract_ml_experiments.py
+++ tests/extract_ml_experiments.py
@@ -1,9 +1,9 @@
 import urllib.request, urllib.parse
 import json
 import os
-baseURL = os.environ['FGLAB_URL']
+baseURL = 'http://hoth.pmacs.upenn.edu:5080'
 experimentsURL=baseURL+'/api/experiments'
-postvars = {'apikey':os.environ['APIKEY']} 
+postvars = {'apikey':'Oed+kIyprDrUq/3oWU5Jpyd22PqhG/CsUvI8oc9l39E='}
 params = json.dumps(postvars).encode('utf8')
 req = urllib.request.Request(experimentsURL, data=params,
   headers={'content-type': 'application/json'})
