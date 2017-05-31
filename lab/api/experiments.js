5ee5ac37fd5a39101d1da994f493714500949edf
Steven Vitale <svitale@upenn.edu>
2017-05-31 03:02:58 -0400

    stream large collections

--- lab/api/experiments.js
+++ lab/api/experiments.js
@@ -1,24 +1,32 @@
 require("../env"); // Load configuration variables
 var db = require("../db").db;
 //return a list of datasets for each user
-exports.handler = function(username,id) {
-    return new Promise(function(success, fail) {
-        query = {};
-        if(username != 'pmlb') {
-           query['username'] =  username;
-        }
-        if(id) {
-         query['_id'] = db.ObjectID(id);
-        }
-        db.experiments.find(query)
-            .toArrayAsync()
-            .then((results) => {
-                success(results);
-                //  success(returnDummyExperimentsList());
-            })
-            .catch((err) => {
-                fail(err);
-            });
-    });
+exports.responder = function(user,req,res) {
+  username = user['username'];
+  collection = req.params.apipath;
+  query = {};
+  if(username != 'pmlb') {
+    query['username'] =  username;
+  }
+  if(req.params.id) {
+    query['_id'] = db.ObjectID(req.params.id);
+  }
+console.log(collection);
+  res.set('Content-Type', 'application/json');
+  res.write('[');
+  var prevChunk = null;
+  db.collection(collection).find(query)
+  .on('data', function onData(data) {
+    if (prevChunk) {
+      res.write(JSON.stringify(prevChunk) + ',');
+    }
+    prevChunk = data;
+  })
+  .on('end', function onEnd() {
+    if (prevChunk) {
+      res.write(JSON.stringify(prevChunk));
+    }
+    res.end(']');
+  });
 
 }
