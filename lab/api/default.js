1a85667e2808bcdb37e04a35936ddc48973ba358
Steven Vitale <svitale@upenn.edu>
2017-06-02 11:56:56 -0400

    get datasets working

--- lab/api/default.js
+++ lab/api/default.js
@@ -3,6 +3,97 @@ var db = require("../db").db;
 var user_collections = ['users', 'experiments', 'datasets'];
 var global_collections = ['projects'];
 var fs = require("fs");
+//helper function for searching and formatting results
+var convert_to_dict = function(array) {
+    var new_array = {};
+    for (var i = 0; i < array.length; i++) {
+        var entry = array[i];
+        var _id = entry['_id'];
+        new_array[_id] = entry;
+    }
+    return(new_array);
+}
+exports.convert_to_dict = convert_to_dict;
+var group_on_key = function(array,key) {
+    var new_array = {};
+    for (old_key in  array) {
+        var entry = array[old_key];
+        var _id = entry[key];
+        if (new_array[_id] === undefined) {
+          new_array[_id] = [];
+        }
+        new_array[_id].push(entry);
+    }
+    return(new_array);
+}
+exports.group_on_key = group_on_key;
+var annotate_dataset = function(dataset) {
+               var validation = dataset;
+                var pending = 0;
+                var running = 0;
+                var finished = 0;
+                var failed = 0;
+                var best_accuracy_score = 0;
+                var best_experiment_id;
+                var best_experiment_name;
+                if (dataset['experiments']) {
+                    experiments = dataset['experiments']
+                    for (var j = 0; j < experiments.length; j++) {
+                        var experiment = experiments[j];
+                        var _status = experiment['status'];
+                        var _scores = experiment['_scores'];
+                        if (_scores !== undefined && _scores['accuracy_score'] >= best_accuracy_score) {
+                            best_accuracy_score = _scores['accuracy_score']
+                            best_experiment_id = experiment['_id']
+                            best_experiment_name = experiment['algorithm']['name']
+                        }
+
+                        if (_status == 'pending') {
+                            pending += 1;
+                        } else if (_status == 'running') {
+                            running += 1;
+                        } else {
+                            finished += 1;
+                            if (_status == 'failed') {
+                                failed += 1;
+                            }
+                        }
+                    }
+                }
+                if (!("ai" in dataset) || (validation['ai'] == null)) {
+                    validation['ai'] = false;
+                } else {
+                   if (validation['ai'] == 'finished');
+                    validation['ai'] = true;
+                }
+
+                validation['has_metadata'] = true;
+                validation['experiments'] = {
+                    pending: pending,
+                    running: running,
+                    finished: finished
+                }
+                validation['best_result'] = undefined;
+                if (best_experiment_id) {
+                    validation['best_result'] = {}
+                    validation['best_result']['_id'] = best_experiment_id;
+                    validation['best_result']['algorithm'] = best_experiment_name;
+                    validation['best_result']['accuracy_score'] = best_accuracy_score;
+                }
+                validation['notifications'] = {
+                    new: 0,
+                    error: failed
+                }
+
+return(validation);
+}
+exports.annotate_dataset = annotate_dataset
+
+
+
+
+
+
 
 exports.responder = function(req, res) {
     if (fs.existsSync('api/' + req.params.apipath + ".js")) {
@@ -11,6 +102,7 @@ exports.responder = function(req, res) {
     return(responder(req, res));
     }
 }
+
 
 //return a list of datasets for each user
 var responder = function(req, res) {
