c77ea41a6ffe69af078406d62c9ee7b76194b48d
Sharon Tartarone <sharonpt@upenn.edu>
2017-06-07 12:21:16 -0400

    some changes

--- lab/src/scenes/Datasets/data/api.js
+++ lab/src/scenes/Datasets/data/api.js
@@ -12,48 +12,47 @@ export const fetchDatasets = () => {
 	const route = 'api/userdatasets';
 	
 	return function(dispatch) {
-			dispatch({
-        type: FETCH_DATASETS_REQUEST
-    	});
+		dispatch({
+			type: FETCH_DATASETS_REQUEST
+		});
 
-			return fetch(route)
-					.then(response => {
-							console.log(response);
-							if(response.status >= 400) {
-									//dispatch()
-							}  
-							return response.json();
-					})
-					.then(response => {
-						console.log(response);
-						dispatch({
-				      type: FETCH_DATASETS_SUCCESS,
-				      receivedAt: Date.now(),
-				      response
-				    })
-					}
-					);
+		return fetch(route)
+			.then(response => {
+				console.log(response);
+				if(response.status >= 400) {
+					//dispatch()
+				}  
+				return response.json();
+			})
+			.then(response => {
+				console.log(response);
+				dispatch({
+					type: FETCH_DATASETS_SUCCESS,
+					receivedAt: Date.now(),
+					response
+				})
+			});
 	}
 };
 
 export const toggleAI = (datasetId, aiState) => {
-		const route = `api/v1/datasets/${datasetId}/ai`;
+	const route = `api/v1/datasets/${datasetId}/ai`;
 		
-		let myHeaders = new Headers();
-		myHeaders.append('Content-Type', 'application/json');
+	let myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
 
-		return function(dispatch) {
-				dispatch(requestAIToggle(datasetId));
-				return fetch(route, {
-						method: 'PUT',
-						headers: myHeaders,
-						mode: 'cors',
-						cache: 'default',
-						body: JSON.stringify({ai: aiState})
-				})
-						.then(response => response.json())
-						.then(json =>
-								dispatch(receiveAIToggle(datasetId, aiState))
-						);
-		}  
+	return function(dispatch) {
+		dispatch(requestAIToggle(datasetId));
+		return fetch(route, {
+			method: 'PUT',
+			headers: myHeaders,
+			mode: 'cors',
+			cache: 'default',
+			body: JSON.stringify({ai: aiState})
+		})
+		.then(response => response.json())
+		.then(json =>
+			dispatch(receiveAIToggle(datasetId, aiState))
+		);
+	}  
 };
