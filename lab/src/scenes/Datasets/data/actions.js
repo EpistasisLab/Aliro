c7500231e40b3624b2fdde601abfd1b09ed44de7
Sharon Tartarone <sharonpt@upenn.edu>
2017-06-07 12:21:16 -0400

    some changes
    Former-commit-id: 34e23fb6d19a81a195c8598c9059f10ef20dca61

--- lab/src/scenes/Datasets/data/actions.js
+++ lab/src/scenes/Datasets/data/actions.js
@@ -7,42 +7,42 @@ export const REQUEST_AI_TOGGLE = 'REQUEST_AI_TOGGLE';
 export const RECEIVE_AI_TOGGLE = 'RECEIVE_AI_TOGGLE';
 
 export const fetchDatasets = (dispatch, getState) => {
-        /*if(getIsFetching(getState())) {
-                return Promise.resolve();
-        }*/
+	/*if(getIsFetching(getState())) {
+		return Promise.resolve();
+	}*/
 
-        dispatch({
-                type: FETCH_DATASETS_REQUEST
-        });
+	dispatch({
+		type: FETCH_DATASETS_REQUEST
+	});
 
-        return api.fetchDatasets().then(
-                response => {
-                        dispatch({
-                                type: FETCH_DATASETS_SUCCESS,
-                                receivedAt: Date.now(),
-                                response
-                        });
-                },
-                error => {
-                        dispatch({
-                                type: FETCH_DATASETS_FAILURE,
-                                receivedAt: Date.now(),
-                                message: error.message || 'Something went wrong.'
-                        });
-                }
-        );
+	return api.fetchDatasets().then(
+		response => {
+			dispatch({
+				type: FETCH_DATASETS_SUCCESS,
+					receivedAt: Date.now(),
+					response
+				});
+			},
+			error => {
+				dispatch({
+					type: FETCH_DATASETS_FAILURE,
+					receivedAt: Date.now(),
+					message: error.message || 'Something went wrong.'
+				});
+			}
+	);
 };
 
 export const requestAIToggle = (datasetId) => ({
-    type: REQUEST_AI_TOGGLE,
-    datasetId
+	type: REQUEST_AI_TOGGLE,
+	datasetId
 });
 
 export const receiveAIToggle = (datasetId, aiState) => ({
-    type: RECEIVE_AI_TOGGLE,
-    receivedAt: Date.now(),
-    datasetId,
-    aiState
+	type: RECEIVE_AI_TOGGLE,
+	receivedAt: Date.now(),
+	datasetId,
+	aiState
 });
 
 // export const actions;
