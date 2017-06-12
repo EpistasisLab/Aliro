a8aa0398d38dd2ea8279d5cd704052faa006fd76
Sharon Tartarone <sharonpt@upenn.edu>
2017-06-12 13:04:57 -0400

    toggle ai
    Former-commit-id: 70aa0af38b723a094ad123e14f0a0b72a11c4a6b

--- lab/src/components/Root/index.jsx
+++ lab/src/components/Root/index.jsx
@@ -9,20 +9,22 @@ import { ResultsContainer } from '../../scenes/Results';
 import { BuilderContainer } from '../../scenes/Builder';
 import { NotFound } from '../../scenes/NotFound';
 
-const Root = ({ store }) => (
-	<Provider store={store}>
-		<Router history={hashHistory}>
-			<Route path='/' component={AppContainer}>
-				<IndexRedirect to="datasets" />
-				<Route path='datasets' component={DatasetsScene} />
-				<Route path='experiments' component={ExperimentsContainer} />
-				<Route path='results/:id' component={ResultsContainer} />
-				<Route path='build/:id' component={BuilderContainer} />
-				<Route path='build/:id/:exp' component={BuilderContainer} />
-				<Route path='*' component={NotFound} />
-			</Route>
-		</Router>
-	</Provider>
-);
+function Root({ store }) {
+	return (
+		<Provider store={store}>
+			<Router history={hashHistory}>
+				<Route path='/' component={AppContainer}>
+					<IndexRedirect to="datasets" />
+					<Route path='datasets' component={DatasetsScene} />
+					<Route path='experiments' component={ExperimentsContainer} />
+					<Route path='results/:id' component={ResultsContainer} />
+					<Route path='build/:id' component={BuilderContainer} />
+					<Route path='build/:id/:exp' component={BuilderContainer} />
+					<Route path='*' component={NotFound} />
+				</Route>
+			</Router>
+		</Provider>
+	);
+};
 
 export default Root;
