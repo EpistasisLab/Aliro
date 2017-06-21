5ce1bb0a8a9eb86105135c4e39f2fb5b31ed3ba6
Sharon Tartarone <sharonpt@upenn.edu>
2017-06-21 18:26:43 -0400

    experiments updates
    Former-commit-id: 2dfbac975bc8f9cfc02693547e93cbfefe9243ef

--- lab/src/reducer.js
+++ lab/src/reducer.js
@@ -1,15 +1,15 @@
 import { combineReducers } from 'redux-immutable';
 import preferences from './components/App/data';
 import datasets from './components/Datasets/data';
-import experiments from './scenes/Experiments/data';
+import experiments from './components/Experiments/data';
 import results from './scenes/Results/data';
 import builder from './scenes/Builder/data';
 
 const app = combineReducers({
    preferences,
    datasets,
-   results,
    experiments,
+   results,
    builder
 });
 
