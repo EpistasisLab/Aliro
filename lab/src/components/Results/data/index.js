b383e61e42223879da20b2c0294a8183a79ef5fd
Sharon Tartarone <sharonpt@upenn.edu>
2017-06-28 15:45:06 -0400

    working on results dash

--- lab/src/components/Results/data/index.js
+++ lab/src/components/Results/data/index.js
@@ -1,3 +1,57 @@
-import results from './reducer';
+import { Map } from 'immutable';
+import { combineReducers } from 'redux-immutable';
+import {
+	RESULTS_FETCH_REQUEST,
+	RESULTS_FETCH_SUCCESS,
+	RESULTS_FETCH_FAILURE
+} from './actions';
+
+export const getResults = (state) => 
+	state.getIn(['results', 'data']);
+
+const data = (state = Map(), action) => {
+	switch(action.type) {
+		case RESULTS_FETCH_SUCCESS:
+			return state.merge(action.response[0]);
+		default:
+			return state;
+	}
+};
+
+export const getIsFetching = (state) => 
+	state.getIn(['results', 'isFetching']);
+
+const isFetching = (state = false, action) => {
+	switch(action.type) {
+		case RESULTS_FETCH_REQUEST:
+			return true;
+		case RESULTS_FETCH_SUCCESS:
+		case RESULTS_FETCH_FAILURE:
+			return false;   
+		default:
+			return state;
+	}
+};
+
+export const getErrorMessage = (state) =>
+	state.getIn(['results', 'errorMessage']);
+
+const errorMessage = (state = null, action) => {
+	switch(action.type) {
+		case RESULTS_FETCH_FAILURE:
+			return action.message;
+		case RESULTS_FETCH_REQUEST:
+		case RESULTS_FETCH_SUCCESS:
+			return null;
+		default:
+			return state;	
+	}
+};
+
+const results = combineReducers({
+	data,
+	isFetching,
+	errorMessage
+});
 
 export default results;
--- lab/src/reducer.js
+++ lab/src/reducer.js
@@ -2,15 +2,15 @@ import { combineReducers } from 'redux-immutable';
 import preferences from './components/App/data';
 import datasets from './components/Datasets/data';
 import experiments from './components/Experiments/data';
-import results from './components/Results/data';
 import builder from './components/Builder/data';
+import results from './components/Results/data';
 
 const app = combineReducers({
-   preferences,
-   datasets,
-   experiments,
-   results,
-   builder
+  preferences,
+  datasets,
+  experiments,
+  builder,
+  results
 });
 
 export default app;
