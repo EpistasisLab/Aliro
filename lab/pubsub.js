0e66e86d7370a619588e54bc38aed621f09dae97
Steven Vitale <svitale@upenn.edu>
2017-06-30 16:58:17 -0400

    check for redis
    Former-commit-id: 73560710dc2090b3b6649cc24acaacde03086eac

--- lab/pubsub.js
+++ lab/pubsub.js
@@ -1,19 +1,19 @@
 require("./env");
 var redis = require("redis");
-var subscriber,publisher
-if (process.env.REDIS_PORT  && process.env.REDIS_HOST) {
-subscriber = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST); 
-publisher = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST);
-subscriber.on('subscribe', function(channel, count) {
-	console.log('Subscribed to channel: ' + channel);
-});
-subscriber.subscribe('toggleAI');
-subscriber.subscribe('startExperiment');
+var subscriber, publisher
+if (process.env.REDIS_PORT && process.env.REDIS_HOST) {
+    subscriber = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+    publisher = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+    subscriber.on('subscribe', function(channel, count) {
+        console.log('Subscribed to channel: ' + channel);
+    });
+    subscriber.subscribe('toggleAI');
+    subscriber.subscribe('startExperiment');
 } else {
     console.log("No redis server defined, pubsub disabled");
 }
 
 module.exports = {
-	publisher: publisher,
-	subscriber: subscriber
+    publisher: publisher,
+    subscriber: subscriber
 };
