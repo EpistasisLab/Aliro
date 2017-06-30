3a19825662c22abce1472d54602904bee7a19882
Steven Vitale <svitale@upenn.edu>
2017-06-30 15:43:10 -0400

    docker updates for redis
    Former-commit-id: e42242641dac07ca229adbbe4aad7127c80be9ad

--- dockers/machine/files/start.sh
+++ dockers/machine/files/start.sh
@@ -1,3 +1,6 @@
 cd ${IFROOT}/Gp/machine
+for metadata in `find ${IFROOT}/Gp/machine/datasets/ | grep metadata`;do rm -f $metadata;done
+rm -f ${IFROOT}/Gp/machine/specs.json
+tar -xzf /files/gei.tgz
 pm2 start machine.config.js --watch
 bash
