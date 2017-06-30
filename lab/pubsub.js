26bf73f10a3a8a18365c77272f54dacbe8a6514d
Steven Vitale <svitale@upenn.edu>
2017-06-30 16:57:20 -0400

    update pubsub

--- lab/pubsub.js
+++ lab/pubsub.js
@@ -1,13 +1,17 @@
+require("./env");
 var redis = require("redis");
-var subscriber = redis.createClient(); 
-var publisher = redis.createClient();
-
+var subscriber,publisher
+if (process.env.REDIS_PORT  && process.env.REDIS_HOST) {
+subscriber = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST); 
+publisher = redis.createClient(process.env.REDIS_PORT,process.env.REDIS_HOST);
 subscriber.on('subscribe', function(channel, count) {
 	console.log('Subscribed to channel: ' + channel);
 });
-
 subscriber.subscribe('toggleAI');
 subscriber.subscribe('startExperiment');
+} else {
+    console.log("No redis server defined, pubsub disabled");
+}
 
 module.exports = {
 	publisher: publisher,
