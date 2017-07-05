455fb2f362c3759858e750089b34bba06f92ad9e
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-05 13:53:04 -0400

    only use pubsub if redis is available
    Former-commit-id: 9251b464bceb942f73de6fad300aeb7b517c4bba

--- lab/socketServer.js
+++ lab/socketServer.js
@@ -9,7 +9,26 @@ function socketServer(server) {
 	io.on('connection', socket => {
 		connections.push(socket);
 
+		/*socket.on('startExperiment', data => {
+			connections.forEach(connectedSocket => {
+				connectedSocket.emit('startExperiment', data);
+				if(connectedSocket !== socket) {
+					connectedSocket.emit('startExperiment', data);
+				}
+			});
+		});*/
+	 
+		socket.on('disconnect', () => {
+			console.log('HERE');
+		  var index = connections.indexOf(socket);
+		  connections.splice(index, 1);
+		});
+
+	});
+
+	if(subscriber) {
 		subscriber.on('message', function(channel, message) {
+
 			if(channel === 'toggleAI') {
 				connections.forEach(connectedSocket => {
 					connectedSocket.emit('toggleAI', message);
@@ -27,21 +46,7 @@ function socketServer(server) {
 		    .catch(() => {}); // Ignore failures
 			}*/
 		});
-
-		/*socket.on('startExperiment', data => {
-			connections.forEach(connectedSocket => {
-				connectedSocket.emit('startExperiment', data);
-				if(connectedSocket !== socket) {
-					connectedSocket.emit('startExperiment', data);
-				}
-			});
-		});*/
-	 
-		socket.on('disconnect', () => {
-		  var index = connections.indexOf(socket);
-		  connections.splice(index, 1);
-		});
-	});
+	}
 };
 
 module.exports = socketServer;
