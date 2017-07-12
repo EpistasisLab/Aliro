b2bb9a6d0ea8eb09d85d93350886cdb2bba7af98
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-12 16:39:01 -0400

    pubsub to correct user

--- lab/pubsub.js
+++ lab/pubsub.js
@@ -1,6 +1,7 @@
 require("./env");
 var redis = require("redis");
-var subscriber, publisher
+var users = require("./users");
+var subscriber, publisher;
 if (process.env.REDIS_PORT && process.env.REDIS_HOST) {
     subscriber = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
     publisher = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
@@ -15,7 +16,21 @@ if (process.env.REDIS_PORT && process.env.REDIS_HOST) {
     console.log("No redis server defined, pubsub disabled");
 }
 
+function publishToUser(channel, data, req) {
+    if(!publisher) { return false; }
+
+    users.returnUserData(req)
+        .then((user) => {
+            data.userid = user._id;
+            publisher.publish(channel, JSON.stringify(data));
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
 module.exports = {
     publisher: publisher,
-    subscriber: subscriber
+    subscriber: subscriber,
+    publishToUser: publishToUser
 };
