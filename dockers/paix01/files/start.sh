66e115349d03b81a19d2829b776ad24ebe19b8ba
Steven Vitale <svitale@upenn.edu>
2017-07-12 09:01:49 -0400

    reorg dockers to boot in alphabetical order
    Former-commit-id: 8d078bffefe03d025903e5294a414f9948615e87

--- dockers/paix01/files/start.sh
+++ dockers/paix01/files/start.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
 cd ${IFROOT}/penn-ai
+file=mongo_export.log
+#results already in the db?
+if [ -f "$file" ]
+then
+        echo "$file found."
+else
+        python3 /opt/penn-ai/tests/export_to_mongo.py > $file
+fi
+#start the ai
+python3 -m ai.ai
 bash
