21ab166cab8c2e47bd53e8b80e7470dc2cf31163
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-13 12:45:08 -0400

    pubsub user auth + refactor

--- lab/socketServer.js
+++ lab/socketServer.js
@@ -1,14 +1,12 @@
-var FGLAB_URL = 'http://localhost:5080';
-
 var socket = require('socket.io');
-var subscriber = require("./pubsub").subscriber;
-var rp = require('request-promise');
+var auth = require('socketio-auth');
+
+var connections = [];
 
 function socketServer(server) {
 	var io = socket(server);
-	var connections = [];
 
-	require('socketio-auth')(io, {
+	auth(io, {
 	  authenticate: function (socket, data, callback) {
 	    socket.client.userid = data.userid;
 	    connections.push(socket);
@@ -25,81 +23,14 @@ function socketServer(server) {
 		});
 
 	});
+}
 
-	if(subscriber) {
-		subscriber.on('message', function(channel, message) {
-			var msg = JSON.parse(message);
+function getSockets() {
+	return connections;
+}
 
-			var userSockets = [];
-			if(msg.userid) {
-				connections.forEach(function(connectedSocket) {
-					if(connectedSocket.client.userid === msg.userid) {
-						userSockets.push(connectedSocket);
-					}
-				});
-			}
-
-			if(channel === 'toggledAI') {
-				userSockets.forEach(connectedSocket => {
-					connectedSocket.emit('updateAIToggle', message);
-				});	
-			}
-
-			if(channel === 'startedExperiment') {
-				rp({
-					uri: FGLAB_URL + "/api/userexperiments/" + JSON.parse(message)._id,
-		      method: "GET"
-		    })
-		    .then((body) => {
-		    	userSockets.forEach(connectedSocket => {
-		    		if(connectedSocket !== socket) {
-							connectedSocket.emit('addExperiment', body);
-						}
-					});
-		    })
-		    .catch(() => {}); // Ignore failures
-			}
-
-			if(channel === 'finishedExperiment') {
-				// call api directly from file to get information
-				rp({
-					uri: FGLAB_URL + "/api/userexperiments/" + JSON.parse(message)._id,
-		      method: "GET"
-		    })
-		    .then((body) => {
-		    	userSockets.forEach(connectedSocket => {
-						connectedSocket.emit('updateExperiment', body);
-					});
-
-		    	rp({
-						uri: FGLAB_URL + "/api/userdatasets/" + JSON.parse(body)[0].dataset_id,
-			      method: "GET"
-			    }).then((body) => {
-		    		userSockets.forEach(connectedSocket => {
-							connectedSocket.emit('updateDataset', body);
-						});
-		   		})
-		    	.catch(() => {}); // Ignore failures
-		    })
-		    .catch(() => {}); // Ignore failures
-			}
-
-			if(channel === 'failedExperiment') {
-				// call api directly from file to get information
-				rp({
-					uri: FGLAB_URL + "/api/userexperiments/" + JSON.parse(message)._id,
-		      method: "GET"
-		    })
-		    .then((body) => {
-		    	userSockets.forEach(connectedSocket => {
-						connectedSocket.emit('updateExperiment', body);
-					});
-		    })
-		    .catch(() => {}); // Ignore failures
-			}
-		});
-	}
+module.exports = {
+	socketServer: socketServer,
+	getSockets: getSockets
 };
-
-module.exports = socketServer;
 
