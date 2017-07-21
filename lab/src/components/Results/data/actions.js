cd3735e1123997927eae7446430282f1ee058c1b
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-20 22:18:15 -0400

    add clear results action
    Former-commit-id: 7c8fce9a60ce52d706555bcbd1cdc212abca4830

--- lab/src/components/Results/data/actions.js
+++ lab/src/components/Results/data/actions.js
@@ -4,10 +4,7 @@ import { getIsFetching } from './index';
 export const RESULTS_FETCH_REQUEST = 'RESULTS_FETCH_REQUEST';
 export const RESULTS_FETCH_SUCCESS = 'RESULTS_FETCH_SUCCESS';
 export const RESULTS_FETCH_FAILURE = 'RESULTS_FETCH_FAILURE';
-
-export const ROC_CURVE_FETCH_REQUEST = 'ROC_CURVE_FETCH_REQUEST';
-export const ROC_CURVE_FETCH_SUCCESS = 'ROC_CURVE_FETCH_SUCCESS';
-export const ROC_CURVE_FETCH_FAILURE = 'ROC_CURVE_FETCH_FAILURE';
+export const RESULTS_CLEAR = 'RESULTS_CLEAR';
 
 export const fetchResults = (id) => (dispatch, getState) => {
   if(getIsFetching(getState())) {
@@ -36,29 +33,8 @@ export const fetchResults = (id) => (dispatch, getState) => {
   );
 };
 
-export const fetchROCCurve = (id) => (dispatch /*, getState*/) => {
-  /*if(getIsFetching(getState())) {
-    return Promise.resolve();
-  }*/
-
+export const clearResults = () => (dispatch) => {
   dispatch({
-    type: ROC_CURVE_FETCH_REQUEST
+    type: RESULTS_CLEAR
   });
-
-  return api.fetchFile(id).then(
-    response => {
-      dispatch({
-        type: ROC_CURVE_FETCH_SUCCESS,
-        receivedAt: Date.now(),
-        response
-      });
-    },
-    error => {
-      dispatch({
-        type: ROC_CURVE_FETCH_FAILURE,
-        receivedAt: Date.now(),
-        message: error.message || 'Something went wrong.'
-      });
-    }
-  );
 };
