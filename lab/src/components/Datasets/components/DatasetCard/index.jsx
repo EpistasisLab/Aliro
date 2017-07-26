25082cd6e57bcef9107f4f7d567419359398c436
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-26 13:53:09 -0400

    add proptypes, refactor, error message
    Former-commit-id: af5baf029493e389e4e98e92f2d552efb17f4fd1

--- lab/src/components/Datasets/components/DatasetCard/index.jsx
+++ lab/src/components/Datasets/components/DatasetCard/index.jsx
@@ -1,64 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from './data/actions';
-import DatasetActions from './components/DatasetActions';
-import BestResult from './components/BestResult';
-import ExperimentStatus from './components/ExperimentStatus';
-import { Grid, Segment, Header, Button } from 'semantic-ui-react';
-import { formatDataset } from '../../../../utils/formatter';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import DatasetCard from './DatasetCard';
 
-class DatasetCard extends Component {
+class DatasetCardContainer extends Component {
   render() {
-
-    const {
-      dataset,
-      toggleAI
-    } = this.props;
-
-    const datasetLink = `/#/datasets/${dataset.get('_id')}`;
-
-    const builderLink = `/#/builder?dataset=${dataset.get('_id')}`;
-
     return (
-      <Grid.Column className="dataset-card">
-        <Segment inverted attached="top" className="panel-header">
-          <Header 
-            as="a"
-            inverted 
-            size="large"
-            content={formatDataset(dataset.get('name'))}
-            href={datasetLink}
-            className="title"
-          />
-          <span className="float-right">
-            <DatasetActions
-              dataset={dataset}
-              toggleAI={toggleAI}
-            />
-          </span>
-        </Segment>
-        <BestResult
-          result={dataset.get('best_result')}
-          hasMetadata={dataset.get('has_metadata')}
-        />
-        <ExperimentStatus
-          filter={dataset.get('_id')}
-          experiments={dataset.get('experiments')}
-          notifications={dataset.get('notifications')}
-        />
-        <Button
-          as="a"
-          color="blue"
-          attached="bottom"
-          content="Build New Experiment"
-          href={builderLink}
-        />
-      </Grid.Column>
+      <DatasetCard {...this.props} />
     );
   }
 }
 
+DatasetCardContainer.propTypes = {
+  dataset: ImmutablePropTypes.map.isRequired
+};
+
 export default connect(
   null,
   actions
-)(DatasetCard);
+)(DatasetCardContainer);
