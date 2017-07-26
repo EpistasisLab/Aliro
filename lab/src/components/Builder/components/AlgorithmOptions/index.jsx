e72d3c655e667055cf9515a4bd079319929a7b07
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-26 17:36:46 -0400

    added proptypes, loader, made stateless
    Former-commit-id: b170976b1baa0793f88e2d1e2e0a6a6012a68363

--- lab/src/components/Builder/components/AlgorithmOptions/index.jsx
+++ lab/src/components/Builder/components/AlgorithmOptions/index.jsx
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Grid, Segment, Header, Button } from 'semantic-ui-react';
 import { formatAlgorithm } from '../../../../utils/formatter';
 
-class AlgorithmOptions extends Component {
-  getIsActive(algorithm) {
-    const { currentAlgorithm } = this.props;
+function AlgorithmOptions({
+  algorithms,
+  currentAlgorithm,
+  setCurrentAlgorithm
+}) {
+  const getIsActive = (algorithm) => {
     return currentAlgorithm && (algorithm.get('_id') === currentAlgorithm.get('_id'));
-  }
+  };
 
-  render() {
-
-    const { 
-      algorithms,
-      setCurrentAlgorithm
-    } = this.props;
-
-    return (
-      <Grid.Column width={16}>
-        <Segment inverted attached="top" className="panel-header">
-          <Header 
-            inverted
-            size="large"
-            color="orange"
-            content="Select Algorithm" 
-          />
-        </Segment>
-        <Segment inverted attached="bottom">  
-          <Grid columns={3} stackable className="compressed">
-            {algorithms && algorithms.map(algorithm => (
-              <Grid.Column key={algorithm.get('_id')}>
-                <Button
-                  inverted 
-                  color="orange"
-                  size="large"
-                  fluid
-                  active={this.getIsActive(algorithm)}
-                  onClick={() => setCurrentAlgorithm(algorithm)}
-                >
-                  {formatAlgorithm(algorithm.get('name'))}
-                  <div className="param-count">
-                    {algorithm.get('schema').size} parameters
-                  </div>
-                </Button>
-              </Grid.Column>
-            ))}
-          </Grid> 
-        </Segment>
-      </Grid.Column>
-    );
-  }
+  return (
+    <Grid.Column width={16}>
+      <Segment inverted attached="top" className="panel-header">
+        <Header 
+          inverted
+          size="large"
+          color="orange"
+          content="Select Algorithm" 
+        />
+      </Segment>
+      <Segment inverted attached="bottom">  
+        <Grid columns={3} stackable className="compressed">
+          {algorithms && algorithms.map(algorithm => (
+            <Grid.Column key={algorithm.get('_id')}>
+              <Button
+                inverted 
+                color="orange"
+                size="large"
+                fluid
+                active={getIsActive(algorithm)}
+                onClick={() => setCurrentAlgorithm(algorithm)}
+              >
+                {formatAlgorithm(algorithm.get('name'))}
+                <div className="param-count">
+                  {algorithm.get('schema').size} parameters
+                </div>
+              </Button>
+            </Grid.Column>
+          ))}
+        </Grid> 
+      </Segment>
+    </Grid.Column>
+  );
 }
+
+AlgorithmOptions.propTypes = {
+  algorithms: ImmutablePropTypes.list,
+  currentAlgorithm: ImmutablePropTypes.map.isRequired,
+  setCurrentAlgorithm: PropTypes.func.isRequired
+};
 
 export default AlgorithmOptions;
