d6b197db8524c13be74cd2aeddd4b706535b89a3
Sharon Tartarone <sharonpt@upenn.edu>
2017-07-26 18:25:26 -0400

    proptypes, refactoring, added username
    Former-commit-id: b177cc8172936bb04b79d91be55ee71ff0e7a412

--- lab/src/components/App/Navbar/index.jsx
+++ lab/src/components/App/Navbar/index.jsx
@@ -1,25 +1,70 @@
 import React from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import MediaQuery from 'react-responsive';
-import BasicNavbar from './BasicNavbar';
-//import { DraggableNavbar } from './DraggableNavbar';
+import DeviceWatcher from '../../../utils/device-watcher';
+import { Menu, Dropdown, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router';
 
-// add tests for this and hook up with username
-// use the built-in linking with react router
-// remove hashtag
-// make sure it resizes nicely/smoothly
-class Navbar extends React.Component {
-  render() {
+function Navbar({ preferences }) {
+  const getUserTrigger = () => {
     return (
-      <span>
-        <MediaQuery maxWidth={1824}>
-          <BasicNavbar />
+      <Menu.Item>
+        <Icon name="user" />
+        <MediaQuery minWidth={DeviceWatcher.breakpoints.MIN_TABLET}>
+          {preferences.get('username')} <Icon name="caret down" />
         </MediaQuery>
-        <MediaQuery minWidth={1825}>
-          <BasicNavbar />
-        </MediaQuery>
-      </span>
+      </Menu.Item>
     );
-  }
+  };
+
+  return (
+    <Menu inverted color="grey" fixed="top" size="large" borderless>
+      <Link to="datasets" className="link"> 
+        <Menu.Item header name="PennAI" />
+      </Link>
+      <MediaQuery minWidth={DeviceWatcher.breakpoints.MAX_MOBILE}>
+        <Menu.Item name="Your friendly AI assistant" />
+      </MediaQuery>
+      {preferences.size &&
+        <Menu.Menu position="right">
+          <Link to="datasets" className="link" activeClassName="active"> 
+            <Menu.Item name="Datasets">
+              <Icon name="file text outline" />
+              <MediaQuery minWidth={DeviceWatcher.breakpoints.MIN_TABLET}>
+                Datasets
+              </MediaQuery>
+            </Menu.Item>
+          </Link>
+          <Link to="experiments" className="link" activeClassName="active"> 
+            <Menu.Item name="Experiments">
+              <Icon name="lab" />
+              <MediaQuery minWidth={DeviceWatcher.breakpoints.MIN_TABLET}>
+                Experiments
+              </MediaQuery>
+            </Menu.Item>
+          </Link>
+          <Dropdown trigger={getUserTrigger()} icon={null}>
+            <Dropdown.Menu>
+              <Dropdown.Item 
+                as={Link} 
+                to="settings"
+                icon="setting" 
+                text="Settings" 
+              />
+              <Dropdown.Item
+                icon="sign out" 
+                text="Sign out" 
+              />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
+      }
+    </Menu>
+  );
 }
+
+Navbar.propTypes = {
+  preferences: ImmutablePropTypes.map
+};
 
 export default Navbar;
