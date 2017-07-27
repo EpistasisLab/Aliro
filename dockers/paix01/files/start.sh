a5cb3cbe7f2e1bcd89d2717af0861b59103948ec
Steven Vitale <svitale@upenn.edu>
2017-07-27 13:47:34 -0400

    update readme and remove cached results from ai
    Former-commit-id: bdbb212928c2de507794aeb5513deae50c7e47e9

--- dockers/paix01/files/start.sh
+++ dockers/paix01/files/start.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!b/in/bash
 cd ${IFROOT}/penn-ai
-file=mongo_export.log
 #results already in the db?
-if [ -f "$file" ]
+if [ -f "mongo_export.log" ]
 then
         echo "$file found."
 else
         python3 /opt/penn-ai/tests/export_to_mongo.py > $file
+fi
+if [ -f "rec_state.obj" ]
+then
+        rm -f rec_state.obj
 fi
 #start the ai
 python3 -m ai.ai
