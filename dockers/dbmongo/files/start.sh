7f78f4ad43662ed21e2ec47f5ea430b31050cb8a
Steven Vitale <svitale@upenn.edu>
2017-07-28 23:12:27 -0400

    breaking EXPERIMENT_UPDATE and EXPERIMENT_ADD

--- dockers/dbmongo/files/start.sh
+++ dockers/dbmongo/files/start.sh
@@ -1,4 +1,9 @@
 mongod -f /etc/mongod.conf &
 mongorestore /dump
 mongoimport -d FGLab -c users --file /root/users.json --type json
+mongo --eval 'db.datasets.createIndex({username: 1})'
+mongo --eval 'db.datasets.createIndex({name: 1})'
+mongo --eval 'db.experiments.createIndex({username: 1})'
+mongo --eval 'db.users.createIndex({username: 1})'
+mongo --eval 'db.experiments.createIndex({_dataset_id: 1})'
 bash
