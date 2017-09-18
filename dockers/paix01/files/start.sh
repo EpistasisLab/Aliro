23a9f8c7694b82f753bc193a1cf580c8079a3752
Steven Vitale <svitale@upenn.edu>
2017-09-17 22:09:30 -0400

    less failed experiments because we've replaced bool with enum and stopped uploading csv files
    Former-commit-id: d8b378a4aedf1ac8eaeb7108c1959586a30ceea8

--- dockers/paix01/files/start.sh
+++ dockers/paix01/files/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd ${IFROOT}/penn-ai
-#results already in the db?
-#python3 tests/export_to_mongo.py >> mongo_export.log
-python3 tests/export_to_mongo.py
 #start the ai
-#while true;do python3 -m ai.ai;sleep 5;done
+python3 -m ai.ai
 bash
