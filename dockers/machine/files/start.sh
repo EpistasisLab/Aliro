1a96837a25efd475353bb28bd236361adb8ad2e7
Steven Vitale <svitale@upenn.edu>
2017-09-28 18:31:29 +0000

    move datasets out of machine
    Former-commit-id: c23018931160b36a46210217b05d52294698c937

--- dockers/machine/files/start.sh
+++ dockers/machine/files/start.sh
@@ -1,5 +1,7 @@
 cd ${IFROOT}/Gp/machine
-for metadata in `find ${IFROOT}/Gp/machine/datasets/ | grep metadata`;do rm -f $metadata;done
+#for metadata in `find ${IFROOT}/Gp/machine/datasets/ | grep metadata`;do rm -f $metadata;done
 rm -f ${IFROOT}/Gp/machine/specs.json
-pm2 start machine.config.js --watch
-bash
+#pm2 start machine.config.js --watch
+/etc/init.d/sshd start
+ifconfig eth0
+node machine.js
