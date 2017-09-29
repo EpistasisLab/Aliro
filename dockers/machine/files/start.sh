11e4468e5eaee8aaf87f292e92960e80238ebf9e
Steven Vitale <svitale@upenn.edu>
2017-09-29 03:23:35 +0000

    changes to get things working with aws
    Former-commit-id: babee4cc6940845e073727ffc528b5385d6f6fd2

--- dockers/machine/files/start.sh
+++ dockers/machine/files/start.sh
@@ -1,7 +1,5 @@
 cd ${IFROOT}/Gp/machine
-#for metadata in `find ${IFROOT}/Gp/machine/datasets/ | grep metadata`;do rm -f $metadata;done
+for metadata in `find ${IFROOT}/Gp/machine/datasets/ | grep metadata`;do rm -f $metadata;done
 rm -f ${IFROOT}/Gp/machine/specs.json
-#pm2 start machine.config.js --watch
-/etc/init.d/sshd start
-ifconfig eth0
-node machine.js
+pm2 start machine.config.js --watch
+bash
