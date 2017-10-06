673021dca7814d09a4d70d19fba541a4b9232e92
weixuanfu2016 <weixuanf@upenn.edu>
2017-10-06 11:46:38 -0400

    reformat to PEP8

--- learn/DecisionTreeClassifier/main.py
+++ learn/DecisionTreeClassifier/main.py
@@ -1,10 +1,11 @@
 from sklearn.tree import DecisionTreeClassifier
-#run without X
+# run without X
 import matplotlib as mpl
 mpl.use('Agg')
 
 # will eventually do this in the correct way -- install a library/package
-import os, sys
+import os
+import sys
 parentPath = os.path.abspath("..")
 if parentPath not in sys.path:
     sys.path.insert(0, parentPath)
@@ -13,8 +14,8 @@ from io_utils import Experiment
 from skl_utils import generate_results
 
 if __name__ == "__main__":
-	exp = Experiment('DecisionTreeClassifier')
-	args, input_file = exp.get_input()
-	model = DecisionTreeClassifier(criterion=args['criterion'], max_depth=args['max_depth'], 
-				min_samples_split=args['min_samples_split'], min_samples_leaf=args['min_samples_leaf'])
-	generate_results(model, input_file, exp.tmpdir, args['_id'])
+    exp = Experiment('DecisionTreeClassifier')
+    args, input_file = exp.get_input()
+    model = DecisionTreeClassifier(criterion=args['criterion'], max_depth=args['max_depth'],
+                                   min_samples_split=args['min_samples_split'], min_samples_leaf=args['min_samples_leaf'])
+    generate_results(model, input_file, exp.tmpdir, args['_id'])
