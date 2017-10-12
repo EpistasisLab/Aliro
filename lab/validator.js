5d75db2ab0257064dd373005d3a9754097ddb9da
Steven Vitale <svitale@upenn.edu>
2017-10-12 09:35:08 -0400

    hardcode moment.js
    Former-commit-id: 5f64867607b0694a5c46a6bbe211234471e5479d

---
+++ lab/validator.js
@@ -0,0 +1,33 @@
+require("./env"); // Load configuration variables
+var Promise = require("bluebird");
+var db = require("./db").db;
+exports.validateParams = function(projId, options, datasetId, callback) {
+    var prevExp = db.experiments.find({
+        _dataset_id: db.toObjectID(datasetId),
+        _project_id: db.toObjectID(projId),
+        _options: options,
+        _status: "success"
+    }).toArrayAsync();
+    Promise.all(prevExp)
+        .then((results) => {
+            if (results.length >= 1) {
+                error = {
+                    error: "already exists"
+                };
+                callback(error);
+            } else {
+                success = {
+                    success: "Options validated"
+                };
+                callback(success);
+            }
+        })
+        .catch((err) => {
+            error = {
+                error: err
+            };
+            callback(error);
+
+        });
+
+}
