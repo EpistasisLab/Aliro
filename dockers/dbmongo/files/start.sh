9298559ab456ee243cadfbf2c7baa2fbd48aceff
Steven Vitale <svitale@upenn.edu>
2017-10-12 22:41:22 -0400

    machine capacity, indexes, efficiency

--- dockers/dbmongo/files/start.sh
+++ dockers/dbmongo/files/start.sh
@@ -1,9 +1,12 @@
 mongod -f /etc/mongod.conf &
+sleep 2
 mongoimport -d FGLab -c projects --file /root/projects.json --type json
 mongoimport -d FGLab -c users --file /root/users.json --type json
-mongo --eval 'db.datasets.createIndex({username: 1})'
-mongo --eval 'db.datasets.createIndex({name: 1})'
-mongo --eval 'db.experiments.createIndex({username: 1})'
-mongo --eval 'db.users.createIndex({username: 1})'
-mongo --eval 'db.experiments.createIndex({_dataset_id: 1})'
+mongo FGLab --eval 'db.datasets.createIndex({username: 1})'
+mongo FGLab --eval 'db.datasets.createIndex({name: 1})'
+mongo FGLab --eval 'db.users.createIndex({username: 1})'
+mongo FGLab --eval 'db.users.createIndex({apikey: 1})'
+mongo FGLab --eval 'db.experiments.createIndex({username: 1})'
+mongo FGLab --eval 'db.experiments.createIndex({_dataset_id: 1})'
+mongo FGLab --eval 'db.projects.createIndex({name: 1})'
 bash
