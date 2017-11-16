d282c1f1858d3832903d0cfa5cfac73b606a8771
Steven Vitale <svitale@upenn.edu>
2017-11-16 12:07:59 -0500

    merging projects

--- dockers/machine/files/start.sh
+++ dockers/machine/files/start.sh
@@ -1,10 +1,10 @@
-cd ${PROJECT_ROOT}/Gp/machine
+cd ${PROJECT_ROOT}/machine
 if [ -f '/root/forum' ]; then
     echo "forum exists."
 else
     #clear out metadata and start fro scratch
-    rm -f ${PROJECT_ROOT}/Gp/machine/specs.json
-    for metadata in `find ${PROJECT_ROOT}/Gp/machine/datasets/ | grep metadata`;do rm -f $metadata;done
+    rm -f ${PROJECT_ROOT}/machine/specs.json
+    for metadata in `find ${PROJECT_ROOT}/machine/datasets/ | grep metadata`;do rm -f $metadata;done
     touch /root/forum
 fi
     pm2 start machine.config.js
