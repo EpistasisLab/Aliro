7a394a45ac1cb58fb28bb3c56c41e6c5d76d6894
Steven Vitale <svitale@upenn.edu>
2017-11-19 10:28:37 -0500

    fix broken machine

--- dockers/machine/files/start.sh
+++ dockers/machine/files/start.sh
@@ -1,5 +1,5 @@
 #    #clear out metadata and start from scratch
-#    rm -f ${PROJECT_ROOT}/machine/specs.json
+ rm -f ${PROJECT_ROOT}/machine/specs.json
 #    for metadata in `find ${PROJECT_ROOT}/machine/datasets/ | grep metadata`;do rm -f $metadata;done
 cd ${PROJECT_ROOT}/machine
 if [ -d 'node_modules' ]; then
