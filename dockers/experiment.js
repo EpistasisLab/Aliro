de66063fdd275c40817d3ec1b236132cb3ccac21
Steven Vitale <svitale@upenn.edu>
2017-11-29 23:07:14 -0500

    working on it
    Former-commit-id: eda86b0963530ceab952e7e802f96ba7497280d2

--- dockers/experiment.js
+++ dockers/experiment.js
@@ -6,6 +6,49 @@ AWS.config.update({
 var ecs = new AWS.ECS({
     apiVersion: '2014-11-13'
 });
+var ec2 = new AWS.EC2();
+
+var makeForum = function(forum) {
+    var params = {
+        MaxCount: 1,
+        /* required */
+        MinCount: 1,
+        /* required */
+        ImageId: 'ami-7f735a1a',
+        InstanceType: 't2.medium',
+        IamInstanceProfile: {
+            Name: "ecsInstanceRole"
+        },
+        //  Placement: {
+        //    AvailabilityZone: 'us-east-2',
+        //  },
+        SecurityGroups: [
+            //   'STRING_VALUE',
+            'ssh', 'backend'
+            /* more items */
+        ],
+        //  SubnetId: 'STRING_VALUE',
+        TagSpecifications: [{
+                ResourceType: 'instance',
+                Tags: [{
+                    Key: 'forum',
+                    Value: forum
+                }, {
+                    Key: 'Name',
+                    Value: forum
+                }]
+            },
+            /* more items */
+        ],
+        UserData: new Buffer("#!/bin/bash\necho ECS_CLUSTER=" + forum + " >> /etc/ecs/ecs.config\n").toString('base64')
+
+    };
+    ec2.runInstances(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data); // successful response
+    });
+}
+
 
 
 
@@ -26,21 +69,23 @@ for (i in grouped) {
 
 
     var params = {
-      clusters: [forum]
+        clusters: [forum]
     };
     ecs.describeClusters(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data); // successful response
     });
 
-    /*
     var params = {
-        cluster: forum
+        //    cluster: forum
     };
     ecs.listContainerInstances(params, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data); // successful response
     });
+
+
+
     /*
     var params = {
       clusterName: forum,
