26b832c9b3df8cb5ddf2840032768f5c5477de27
Steven Vitale <svitale@upenn.edu>
2017-11-29 19:20:45 -0500

    ec2 commands
    Former-commit-id: 05ff056071ea201ef2fa081ec376515f1610e1fa

--- dockers/experiment.js
+++ dockers/experiment.js
@@ -1,5 +1,13 @@
-var awsm = require('./awsm');
 var grouped = require('./ranman').retData();
+var AWS = require('aws-sdk');
+AWS.config.update({
+    region: 'us-east-2'
+});
+var ecs = new AWS.ECS({
+    apiVersion: '2014-11-13'
+});
+
+
 
 var all;
 // format the array returned by randomizer
@@ -10,17 +18,55 @@ for (i in grouped) {
         delete(grouped[i]);
     }
 }
-var grouped = grouped.slice(0,3)
+var grouped = grouped.slice(0, 3)
 for (i in grouped) {
-var forum = grouped[i]['forum'];
-var num_hosts = 2;
-//console.log('creating stack to process group ' + forum)
+    var forum = grouped[i]['forum'];
+    var num_hosts = 2;
 
 
-//var stack = awsm.makeStack(forum,num_hosts,function(data) {
-//var stack = awsm.rmStack(forum,function(data) {
-var stack = awsm.grokStack(forum,function(data) {
-console.log('f',forum)
-console.log('d',data);
-});
+
+    var params = {
+      clusters: [forum]
+    };
+    ecs.describeClusters(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+
+    /*
+    var params = {
+        cluster: forum
+    };
+    ecs.listContainerInstances(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data); // successful response
+    });
+    /*
+    var params = {
+      clusterName: forum,
+    };
+
+
+    ecs.createCluster(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+
+
+    /*
+ var params = {
+  cluster: forum, 
+  taskDefinition: "pilottaskdefinition:2"
+ };
+ ecs.runTask(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
+ });
+   */
+    //var stack = awsm.makeStack(forum,num_hosts,function(data) {
+    //var stack = rmStack(forum,function(data) {
+    //var stack = awsm.grokStack(forum,function(data) {
+    //console.log('f',forum)
+    //console.log('d',data);
+    //});
 }
