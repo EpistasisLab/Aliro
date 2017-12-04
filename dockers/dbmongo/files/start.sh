6aaff3c773e31207405addd8028b1d07e0da70da
Steven Vitale <svitale@upenn.edu>
2017-12-04 11:54:21 -0500

    move users out of dumps
    Former-commit-id: 2f38a90c6fb98803f4b47e6c8bb63b7ebf539012

--- dockers/dbmongo/files/start.sh
+++ dockers/dbmongo/files/start.sh
@@ -6,6 +6,9 @@ else
     if [ -v PARENTDB ]; then
         dumpdir=${SHARE_PATH}/${NETWORK}/forums/${PARENTDB}
         cd $dumpdir  && mongorestore dump/
+        mongoimport -d FGLab -c users --file /root/users.json --type json
+        mongo FGLab --eval 'db.users.createIndex({username: 1})'
+        mongo FGLab --eval 'db.users.createIndex({apikey: 1})'
     else
         mongoimport -d FGLab -c projects --file /root/projects.json --type json
         mongoimport -d FGLab -c users --file /root/users.json --type json
