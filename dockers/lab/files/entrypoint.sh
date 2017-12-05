b333969be8b2fae642055c115e3e58507d089fb9
Steven Vitale <svitale@upenn.edu>
2017-12-04 19:53:11 -0500

    adding entrypoints
    Former-commit-id: 489166a1e0069ce0564ad451186890e752269559

---
+++ dockers/lab/files/entrypoint.sh
@@ -0,0 +1,15 @@
+cd ${PROJECT_ROOT}/lab
+if [ -d 'node_modules' ]; then
+    echo "npm ready"
+else
+    echo "installing npm, bower and webpack"
+    npm install
+    npm install -g bower pm2 webpack webpack-dev-server
+fi;
+if [ -d 'bower_components' ]; then
+    echo "bower ready"
+else
+    bower install --allow-root
+fi;
+webpack --watch &
+pm2 start lab.config.js --watch
---
+++ dockers/paix01/files/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export USERNAME=${PAIX_USER:-testuser}
+cd ${PROJECT_ROOT}
+#start the ai
+python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u ${USERNAME}
