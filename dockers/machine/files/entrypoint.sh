107166ae35ef0ea9069049e76cb7adb70000d49c
Steven Vitale <svitale@upenn.edu>
2017-12-06 18:05:59 +0000

    fix path

--- dockers/machine/files/entrypoint.sh
+++ dockers/machine/files/entrypoint.sh
@@ -1,7 +1,5 @@
-cd ${PROJECT_ROOT}/pennai/machine
-rm -f ${PROJECT_ROOT}/pennai/machine/specs.json
-#/etc/init.d/ssh start
-#ifconfig eth0
+cd ${PROJECT_ROOT}/machine
+rm -f ${PROJECT_ROOT}/machine/specs.json
 export IP=`wget -qO- http://instance-data/latest/meta-data/local-ipv4`
 export MACHINE_URL=http://${IP}:${MACHINE_PORT}
 pm2 start machine.config.js --watch
