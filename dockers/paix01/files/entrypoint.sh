44d38ff64d8dd5c9fc45b7f6bad107d21100bc62
Steven Vitale <svitale@upenn.edu>
2017-12-07 02:43:04 -0500

    loop
    Former-commit-id: b3fbf53cb064ef6196101772bad316845468e6f7

--- dockers/paix01/files/entrypoint.sh
+++ dockers/paix01/files/entrypoint.sh
@@ -1,25 +1,10 @@
 #figure out where we are running 
-wget localhost:51678/v1/metadata -t 1 -qO- &> /dev/null
-if [ $? -eq 0 ]
-then
-    export ISAWS=1
-else
-    export ISAWS=0
-fi
 cd ${PROJECT_ROOT}
-if [ ${ISAWS} -eq 1 ]
-then
-    sleep 60
-fi
-python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u ${PAIX_USER}
-if [ ${ISAWS} -eq 1 ]
-then
-    while [ ! -f /tmp/die.txt ]
+while [ ! -f /tmp/die.txt ]
     do
+      python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u ${PAIX_USER}
       echo "sleep"
       sleep 2
     done
     cat /tmp/die.txt
-else
     bash
-fi
