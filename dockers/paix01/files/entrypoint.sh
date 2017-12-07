428b544a62b808b6e6b30feb13cad5f2a93a6fd5
lacava <williamlacava@gmail.com>
2017-12-07 16:51:23 -0500

    adds check for datasets, printout'

--- dockers/paix01/files/entrypoint.sh
+++ dockers/paix01/files/entrypoint.sh
@@ -1,11 +1,11 @@
 #figure out where we are running 
 cd ${PROJECT_ROOT}
-while [ ! -f /tmp/die.txt ]
-    do
-      env
-      python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u testuser
-      echo "sleep"
-      sleep 2
-    done
+#while [ ! -f /tmp/die.txt ]
+#    do
+#      env
+#      python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u ${PAIX_USER} 
+#      echo "sleep"
+#      sleep 2
+#    done
     cat /tmp/die.txt
     bash
