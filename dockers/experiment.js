b9e2c2da22217e16d77f29fd884072baefaf6f70
Steven Vitale <svitale@upenn.edu>
2017-12-07 17:34:34 -0500

    adding cloud

--- dockers/experiment.js
+++ dockers/experiment.js
@@ -5,11 +5,11 @@ var Promise = require('q');
 // cloud functions
 var awsm = require('./awsm');
 // randomizing the order of experiments
-var make = require('./make');
+var make = awsm.make
 //make a particular host
 var host = false
 //
-var ranman = require('./ranman').retData();
+var ranman = awsm.randomizer.retData();
 // many forums, one complete list of datasets
 var forums = ranman['grouped'];
 var alldata = ranman['datasets'];
@@ -41,73 +41,8 @@ if (argv['d']) {
 for (var i in forums) {
     var forum = forums[i];
     var forumName = forum['forumName'];
-    var iP, cP, tP;
     var cloudP = awsm.getCloud(forum);
     cloudP.then(function(finfo) {
-        if (finfo['instances'].length == finfo['ccount'] && finfo['ccount'] == finfo['icount']) {
-            finfo['settled'] = true;
-        } else {
-            finfo['settled'] = false;
-        }
-        if (action == 'info') {
-            if (doSync) {
-                awsm.syncForum(finfo);
-            }
-            console.log(finfo)
-        }
-        if (action == 'stop') {
-            if (finfo['instances'].length > 0) {
-                iP = awsm.stopInstances(finfo['instances']);
-            } else {
-                console.log('instances already stopped');
-                iP = Promise.when();
-            }
-            iP.then(function(instances) {
-                if (finfo['ccount'] == 0 && finfo['settled']) {
-                    cP = awsm.stopCluster(finfo['forumName']);
-                } else {
-                    console.log('waiting for counts to settle');
-                    cP = Promise.when();
-                }
-                cP.then(function(cluster) {
-                    console.log('done')
-                    // console.log('stopped cluster')
-                }).catch(function(err) {
-                    console.log('error', err);
-                });
-            });
-        } else if (action == 'start') {
-            if (finfo['cstatus'] != 'ACTIVE') {
-                cP = awsm.startCluster(finfo['forumName']);
-            } else {
-                console.log('cluster already running');
-                cP = Promise.when();
-            }
-            cP.then(function(cluster) {
-                if (finfo['icount'] == 0) {
-                    iP = awsm.startInstances(finfo);
-                } else {
-                    console.log('instances already running');
-                    iP = Promise.when();
-                }
-                iP.then(function(instances) {
-                    //make sure cluster and instances agree on count
-                    if (finfo['settled'] && finfo['services'].length == finfo['instances'].length) {
-                        // && finfo['tcount'] ==0) {
-                        tP = awsm.startTasks(finfo,tasks);
-                    } else {
-                        tP = Promise.when();
-                    }
-                    tP.then(function(tasks) {
-                        console.log('done');
-                        awsm.syncForum(finfo)
-                        console.log(tasks);
-                    }).catch(function(err) {
-                        console.log('error', err);
-                    });
-                }).catch(function(err) {
-                    console.log('error', err);
-                });
             }).catch(function(err) {
                 console.log('error', err);
             });
--- dockers/paix01/files/entrypoint.sh
+++ dockers/paix01/files/entrypoint.sh
@@ -1,11 +1,21 @@
 #figure out where we are running 
+wget localhost:51678/v1/metadata -t 1 -qO- &> /dev/null
+if [ $? -eq 0 ]
+then
+    export ISAWS=1
+else
+    export ISAWS=0
+fi
 cd ${PROJECT_ROOT}
-#while [ ! -f /tmp/die.txt ]
-#    do
-#      env
-#      python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u ${PAIX_USER} 
-#      echo "sleep"
-#      sleep 2
-#    done
-    cat /tmp/die.txt
+if [ ${ISAWS} -eq 1 ]
+then
+while [ ! -f /tmp/die.txt ]
+    do
+      env
+      python -m ai.ai -rec ${RECOMMENDER} -v -n ${NUMRECOMMEND} -u ${PAIX_USER} 
+      echo "sleep"
+      sleep 2
+    done
+else
     bash
+fi
