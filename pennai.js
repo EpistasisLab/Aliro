22d2d474e3b03f34993a75354ccd0102f60d2607
Steven Vitale <svitale@upenn.edu>
2017-12-13 00:59:03 -0500

    almost done reorg

--- pennai.js
+++ pennai.js
@@ -7,8 +7,6 @@
 var awsm = require('./awsm');
 var argv = require('minimist')(process.argv.slice(2));
 //the maximum number of forums we can run at once
-var num_forums = 3;
-var set_size = 10;
 var action = 'info';
 if (argv['_'].length > 0) {
     action = argv['_'][0];
@@ -16,11 +14,13 @@ if (argv['_'].length > 0) {
 
 //run in the cloud
 var doCloud = true;
+/*
 if (!argv['c']) {
     doCloud = false;
     //only one forum at a time on metal
     num_forums = 1;
 }
+*/
 
 //run from share 
 var doShared = false
@@ -43,38 +43,54 @@ if (argv['v']) {
 var exP = awsm.syncFile('experiment.json');
 exP.then(function(experiment) {
     //console.log(experiment);
-    //randomize the order of datasets and split them into groups of set_size
     var randomized = awsm.ranman.retRandomized(experiment);
-    var forums = randomized.slice(0, num_forums);
-    var cloud = awsm.cloud.handleCloud(experiment, action);
+    //break it down to just the first bit
+    var forums = randomized.slice(0, experiment.max_forums);
+    //get cloud resources
+    //init - create the base resources to run the experiment
+    //destroy - delete the base resources created for this experiment
+    var cloud = awsm.cloud.handleCloud(experiment, action, doCloud);
+    cloud.then(function(cf) {
+        var cloudResources = {}
+        if (cf !== undefined && cf['StackResources'] !== undefined) {
 
-
-    //    console.log(forums);
-    for (var i in forums) {
-        var forum = forums[i];
-        var forumName = forum['forumName'];
-        var build;
-        var options = {};
-        options['action'] = action;
-        options['tasks'] = tasks;
-        options['shared'] = doShared;
-        options['verbose'] = verbose;
-        if (doCloud) {
-            build = awsm.cloudMan
-        } else {
-            build = awsm.metalMan
-        }
-        var infP = build(forum, experiment, options);
-        infP.then(function(finfo) {
-            if (finfo) {
-                //                console.log(finfo);
-                //console.log(finfo);
+            var resources = cf['StackResources'];
+            for (var i in resources) {
+                var resource = resources[i];
+                var LogicalResourceId = resource['LogicalResourceId']
+                var PhysicalResourceId = resource['PhysicalResourceId']
+                cloudResources[LogicalResourceId] = PhysicalResourceId;
             }
-        }).catch(function(err) {
-            console.log('error', err);
-        });
-        //}
-    }
+        }
+        //    console.log(forums);
+        for (var i in forums) {
+            var forum = forums[i];
+            var forumName = forum['forumName'];
+            var build;
+            var options = {};
+            options['action'] = action;
+            options['tasks'] = tasks;
+            options['shared'] = doShared;
+            options['verbose'] = verbose;
+            options['cloudResources'] = cloudResources;
+            if (doCloud) {
+                build = awsm.cloudMan
+            } else {
+                build = awsm.metalMan
+            }
+            var infP = build(forum, experiment, options);
+            infP.then(function(finfo) {
+
+                if (finfo) {}
+            }).catch(function(err) {
+                console.log('error', err);
+            });
+
+
+
+            //}
+        }
+    });
 }).catch(function(err) {
     console.log('something went wrong')
     console.log(err);
