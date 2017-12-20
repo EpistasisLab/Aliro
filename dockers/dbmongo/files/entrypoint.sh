558692cdd4403b7c0a3c9bfb18a2c60e4eb5c2e8
Steven Vitale <svitale@upenn.edu>
2017-12-19 21:36:27 -0500

    project updates
    Former-commit-id: 2345f34038d0d827a68e7afb3f9fd9f31f290c1e

--- dockers/dbmongo/files/entrypoint.sh
+++ dockers/dbmongo/files/entrypoint.sh
@@ -6,32 +6,30 @@ then
 else
     export ISAWS=0
 fi
+if [ ! -v PROJECTS_FILE ]; then
+    PROJECTS_FILE='projects.json';
+fi
 mongod -f /etc/mongod.conf &
 #check to see if db was loaded
 if [ ! -f '/root/forum' ]; then
     if [ -v FORUM ] && [ -d ${SHARE_PATH}/${NETWORK}/forums/${FORUM}/dump ]; then
         dumpdir=${SHARE_PATH}/${NETWORK}/forums/${FORUM}
         cd $dumpdir  && mongorestore dump/
-        mongoimport -d FGLab -c users --file /root/users.json --type json
-        mongo FGLab --eval 'db.users.createIndex({username: 1})'
-        mongo FGLab --eval 'db.users.createIndex({apikey: 1})'
     elif [ -v PARENTDB ]; then
         dumpdir=${SHARE_PATH}/${NETWORK}/forums/${PARENTDB}
         cd $dumpdir  && mongorestore dump/
-        mongoimport -d FGLab -c users --file /root/users.json --type json
-        mongo FGLab --eval 'db.users.createIndex({username: 1})'
-        mongo FGLab --eval 'db.users.createIndex({apikey: 1})'
     else
-        mongoimport -d FGLab -c projects --file /root/projects.json --type json
         mongoimport -d FGLab -c users --file /root/users.json --type json
         mongo FGLab --eval 'db.datasets.createIndex({username: 1})'
         mongo FGLab --eval 'db.datasets.createIndex({name: 1})'
-        mongo FGLab --eval 'db.users.createIndex({username: 1})'
-        mongo FGLab --eval 'db.users.createIndex({apikey: 1})'
         mongo FGLab --eval 'db.experiments.createIndex({username: 1})'
         mongo FGLab --eval 'db.experiments.createIndex({_dataset_id: 1})'
-        mongo FGLab --eval 'db.projects.createIndex({name: 1})'
     fi
+    mongoimport -d FGLab -c users --file /root/users.json --type json
+    mongoimport -d FGLab -c projects --file /root/${PROJECTS_FILE} --type json
+    mongo FGLab --eval 'db.projects.createIndex({name: 1})'
+    mongo FGLab --eval 'db.users.createIndex({username: 1})'
+    mongo FGLab --eval 'db.users.createIndex({apikey: 1})'
     touch /root/forum
 fi
 if [ ${ISAWS} -eq 1 ]  ||  [ -v NOBASH ];then
