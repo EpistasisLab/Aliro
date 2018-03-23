2d66dfabb88a2dad1561e5462271594e3cbc313b
miguelehernandez <miguelehernandez@gmail.com>
2018-03-23 17:09:18 -0400

    Cleanup: remove bower and webpack
    Former-commit-id: 5e3861570cfa35ea7e02a3396dc09a9d5fa4f0c4

--- dockers/lab/files/entrypoint.sh
+++ dockers/lab/files/entrypoint.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 wget localhost:51678/v1/metadata -t 1 -qO- &> /dev/null
+
 if [ $? -eq 0 ]
 then
     export ISAWS=1
 else
     export ISAWS=0
 fi
+
 cd ${PROJECT_ROOT}/lab
+
 if [ -d 'node_modules' ]; then
     echo "npm ready"
 else
-    echo "installing npm, bower and webpack"
+    echo "installing npm dependencies and pm2"
     npm install
     npm install -g pm2 
-fi
-if [ -d 'bower_components' ]; then
-    echo "bower ready"
-else
-    bower install --allow-root
-fi
-webpack &
-if [ -f '.env' ]; then
-    touch .env
 fi
 
 if [ -d "../tmp" ]; then
