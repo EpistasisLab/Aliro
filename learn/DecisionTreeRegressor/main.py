4ae2d5b936bf2140e38521e412bc603a3dbc7418
weixuanfu2016 <weixuanf@pennmedicine.upenn.edu>
2018-06-14 10:33:31 -0400

    clean codes
    Former-commit-id: 8d2d51ccc86b788f0e14038844f93146ae475ef9

--- learn/DecisionTreeRegressor/main.py
+++ learn/DecisionTreeRegressor/main.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     args, input_file = exp.get_input()
     model = DecisionTreeRegressor(max_depth=args['max_depth'], min_samples_split=args['min_samples_split'],
                                   min_samples_leaf=args['min_samples_leaf'])
-    generate_results(model, input_file, exp.tmpdir, args['_id'],mode='regression')
+    generate_results(model, input_file, exp.tmpdir, args['_id'], mode='regression')
