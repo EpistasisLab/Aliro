e892ef742dd284e76c11bdf32e0594b867be04cd
hjwilli <hjwilli@gmail.com>
2018-06-14 19:02:03 -0400

    expanded jest testing
    References #40

--- tests/integration/jest/labApi.js
+++ tests/integration/jest/labApi.js
@@ -3,6 +3,18 @@
 *
 */
 
-import { get } from './util/apiHelper';
+import { get, post } from './util/apiHelper';
 
 export const fetchDatasets = () => get('http://lab:5080/api/userdatasets');
+export const fetchDataset = (id) => get(`http://lab:5080/api/datasets/${id}`);
+
+export const fetchMachines = () => get('http://lab:5080/api/v1/machines');
+
+export const fetchExperiments = () => get('http://lab:5080/api/userexperiments');
+export const fetchExperiment = (id) => get(`http://lab:5080/api/experiments/${id}`);
+
+export const fetchAlgorithms = () => get('http://lab:5080/api/projects');
+
+export const submitExperiment = (algorithm, params) => 
+	post(`'http://lab:5080/api/v1/projects/${algorithm}/experiment`, JSON.stringify(params));
+  //post(`api/v1/projects/${algorithm}/experiment`, params.toJSON());
