73cb4e0b925b95129782872a320593476e075c5c
hjwilli <hjwilli@gmail.com>
2018-08-06 17:35:36 -0400

    set cancelled ml status to 'cancelled' instead of 'fail'
    If a machine experiment was cancelled via machine
    <experiments/:id/kill>, set the experiment status to 'cancelled' instead
    of 'fail' upon exit

--- tests/integration/jest/machineApi.js
+++ tests/integration/jest/machineApi.js
@@ -6,3 +6,5 @@ import { get, post } from './util/apiHelper';
 
 export const fetchCapacity = (id) => get(`http://machine:5081/projects/${id}/capacity`);
 export const fetchProjects = () => get(`http://machine:5081/projects`);
+
+export const killExperiment = (id) => post(`http://machine:5081/experiments/${id}/kill`);
