d5d8989e507dcbd904601acf0eec1a04d22b1559
lacava <williamlacava@gmail.com>
2018-08-16 16:18:56 -0400

    adds label encoding and one hot encoding. need to handle transformations of incoming data and nail down the metafeatures so training can be standardized in update().

--- ai/recommender/meta_recommender.py
+++ ai/recommender/meta_recommender.py
@@ -7,7 +7,11 @@ import urllib.request, urllib.parse
 from .base import BaseRecommender
 from ..metalearning import get_metafeatures
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.pipeline import Pipeline
+import numpy as np
 
+from ..db_utils import get_all_ml_p_from_db  
 class MetaRecommender(BaseRecommender):
     """Penn AI meta recommender.
 
@@ -24,7 +28,7 @@ class MetaRecommender(BaseRecommender):
 
     """
 
-    def __init__(self, ml_type='classifier', metric=None, db_path='',api_key=''):
+    def __init__(self, ml_type='classifier', metric=None, db_path='',api_key='', ml_p=None):
         """Initialize recommendation system."""
         if ml_type not in ['classifier', 'regressor']:
             raise ValueError('ml_type must be "classifier" or "regressor"')
@@ -44,11 +48,40 @@ class MetaRecommender(BaseRecommender):
         # training data
         self.training_data = pd.DataFrame()
         # store metafeatures of datasets that have been seen
-        self.dataset_metafeatures = pd.DataFrame(columns=['dataset'])
+        # self.dataset_metafeatures = None
         # maintain a set of dataset-algorithm-parameter combinations that have already been 
         # evaluated
         self.trained_dataset_models = set()
         # TODO: add option for ML estimator
+        self.first_update = True
+
+	# load ML Parameter combinations and fit an encoding to them that can be used for
+	# learning a model : score = f(ml,p,dataset,metafeatures)
+        
+        if ml_p is None:
+            # pull algorithm/parameter combinations from the server. 
+            self.ml_p = get_all_ml_p_from_db('/'.join([db_path,'api/preferences']),api_key)
+        else:
+            self.ml_p = ml_p
+
+        print('ml_p:',self.ml_p)
+        from collections import defaultdict
+        self.LE = defaultdict(LabelEncoder)
+        self.OHE = OneHotEncoder()
+        # Encoding the variable
+        self.ml_p = self.ml_p.apply(lambda x: self.LE[x.name].fit_transform(x))
+        print('ml_p after LE:',self.ml_p)
+        X = self.OHE.fit_transform(self.ml_p)
+        # self.ml_p = self.ml_p.apply(lambda x: self.OHE[x.name].fit_transform(x))
+        print('X after OHE:',X.shape)
+
+        # Inverse the encoded
+        # fit.apply(lambda x: self.LE[x.name].inverse_transform(x))
+
+        # Using the dictionary to label future data
+        # self.ml_p.apply(lambda x: self.LE[x.name].transform(x))
+
+        # one hot encode algorithms 
 
     def update(self, results_data):
         """Update ML / Parameter recommendations based on overall performance in results_data.
@@ -67,38 +100,55 @@ class MetaRecommender(BaseRecommender):
         # # one hot encode algorith-parameter combinations?
         # regress on scores for each dataset
 
-        # make combined data columns of datasets, classifiers, and parameters
-        results_data.loc[:, 'algorithm-parameters'] = (
-                                       results_data['algorithm'].values + '|' +
-                                       results_data['parameters'].values)
+        # # make combined data columns of datasets, classifiers, and parameters
+        # results_data.loc[:, 'algorithm-parameters'] = (
+        #                                results_data['algorithm'].values + '|' +
+        #                                results_data['parameters'].values)
 
-        results_data.loc[:, 'dataset-algorithm-parameters'] = (
-                                       results_data['dataset'].values + '|' +
-                                       results_data['algorithm'].values + '|' +
-                                       results_data['parameters'].values)
-
-        for d in results_data['dataset'].unique():
-            if d not in self.dataset_metafeatures['dataset']:
-                # fetch metafeatures from server for dataset and append
-                print('fetching data for', d)
-                 
-                payload={}
-                # payload = {'metafeatures'}
-                payload.update(self.static_payload)
-                req = urllib.request.Request((self.mf_path+'/'+d).encode(), data=payload)
-                r = urllib.request.urlopen(req)
-                
-                data = json.loads(r.read().decode(r.info().get_param('charset')
-                                          or 'utf-8'))
-
-                # data = json.loads('/'.join([self.mf_path,d,'metafeatures.json']))
-                print(data)
-
+        dap = (results_data['dataset'].values + '|' +
+               results_data['algorithm'].values + '|' +
+               results_data['parameters'].values)
         # get unique dataset / parameter / classifier combos in results_data
-        ml_p = results_data['algorithm-parameters'].unique()
-        d_ml_p = results_data['dataset-algorithm-parameters'].unique()
+        # ml_p = results_data['algorithm-parameters'].unique()
+        print(dap)
+        d_ml_p = np.unique(dap)
         self.trained_dataset_models.update(d_ml_p)
+ 
+        dataset_metafeatures = []
+        for d in results_data['dataset'].unique():
+            # fetch metafeatures from server for dataset and append
+            print('fetching data for', d)
+            payload={}
+            # payload = {'metafeatures'}
+            payload.update(self.static_payload)
+            params = json.dumps(payload).encode('utf8')
+            print('full path:', self.mf_path+'/'+d)
+            req = urllib.request.Request(self.mf_path+'/'+d, data=params)
+            r = urllib.request.urlopen(req)
+            
+            data = json.loads(r.read().decode(r.info().get_param('charset')
+                                      or 'utf-8'))[0]
+            mf = [data['metafeatures']]
+            # print('mf:',mf)
+            df = pd.DataFrame.from_records(mf,columns=mf[0].keys())
+            # print('df:',df)
+            df['dataset'] = data['_id']
+            print('metafeatures:',df)
 
+            dataset_metafeatures.append(df)
+            
+        df_mf = pd.concat(dataset_metafeatures,ignore_index=True)
+        print('df_mf:',df_mf)
+        print('dataset_metafeatures:',dataset_metafeatures)
+        # make training data
+        # smash metafeature into results_data
+        self.training_data = pd.concat([results_data,df_mf],axis=1)
+        self.training_data.dropna(how='any',axis=1,inplace=True)
+         
+        print('training data:',self.training_data)
+        print('training columns:',self.training_data.columns)
+        # one-hot encode parameter combinations?
+         
         # update internal model
         self.update_model()
         
