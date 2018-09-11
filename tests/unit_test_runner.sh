d4b6597d8ecab65e27d7076bea002bf11c73709b
hjwilli <hjwilli@gmail.com>
2018-09-11 12:13:30 -0400

    unit test script

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -3,8 +3,8 @@ rm -drf target
 mkdir -p target/test-reports/cobertura/html
 
 REPORT_PATH="target/test-reports"
-COVERAGE_PATH="target/test-reports/cobertura/html"
+COVERAGE_PATH="target/test-reports/cobertura"
 
-nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}" ai/tests/test_recommender.py machine/test/learn_tests.py
+nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" ai/tests/test_recommender.py machine/test/learn_tests.py
 
 rm .coverage
