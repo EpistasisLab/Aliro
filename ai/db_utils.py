da2d861a4cb4b1bb7a7e4f2063a8908ce4f72f1e
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2018-09-13 10:34:43 -0400

    let ai find correct values in db ref #88

--- ai/db_utils.py
+++ ai/db_utils.py
@@ -5,7 +5,7 @@ Databse utility functions for Penn-AI
 import pandas as pd
 import numpy as np
 import pdb
-import json 
+import json
 import requests
 import itertools as it
 
@@ -19,43 +19,46 @@ def get_all_ml_p_from_db(path,key):
     print('r:',r)
     response = json.loads(r.text)
     algorithms = response[0]['algorithms']
-    result = [] # returned value 
+    result = [] # returned value
     good_def = True # checks that json for ML is in good form
-    
+
     for i,x in enumerate(algorithms):
     #for i,x in enumerate(response):
         #print('ML: ',x['name'])
         hyperparams = x['schema'].keys()
         hyperparam_dict = {}
-                
+
         # get a dictionary of hyperparameters and their values
         for h in hyperparams:
             #print('x[''schema''][h]',x['schema'][h])
             if 'ui' in x['schema'][h]:
-                hyperparam_dict.update({h: x['schema'][h]['ui']['choices']})
+                if 'values' in x['schema'][h]['ui']:
+                    hyperparam_dict.update({h: x['schema'][h]['ui']['values']})
+                else:
+                    hyperparam_dict.update({h: x['schema'][h]['ui']['choices']})
             else:
                 good_def = False
         if good_def:
             sorted_hp = sorted(hyperparam_dict)
             # enumerate all possible hyperparameter combinations
-            all_hyperparam_combos = [dict(zip(sorted_hp,prod)) 
-                                      for prod in it.product(*(hyperparam_dict[k] 
+            all_hyperparam_combos = [dict(zip(sorted_hp,prod))
+                                      for prod in it.product(*(hyperparam_dict[k]
                                       for k in sorted_hp))]
-            
+
             #print('\thyperparams: ',hyperparam_dict)
             #print(len(all_hyperparam_combos),' total hyperparameter combinations')
-                             
+
             for ahc in all_hyperparam_combos:
                 result.append({'algorithm':x['_id'],'parameters':str(ahc)})
         else:
             print('warning: ', x['name'], 'was skipped')
         good_def = True
-        
-    # convert to dataframe, making sure there are no duplicates    
+
+    # convert to dataframe, making sure there are no duplicates
     all_ml_p = pd.DataFrame(result).drop_duplicates()
-    
-    print(len(all_ml_p),' ml-parameter options loaded')    
-    
+
+    print(len(all_ml_p),' ml-parameter options loaded')
+
     return all_ml_p
 
 def get_random_ml_p_from_db(path,key):
@@ -67,24 +70,24 @@ def get_random_ml_p_from_db(path,key):
     print('r:',r)
     responses = json.loads(r.text)
 
-    result = [] # returned value 
-    
+    result = [] # returned value
+
     ml = np.random.choice(responses) # random chosen ML model
     print('chosen ML:',ml)
-    
+
     hyperparams = ml['schema'].keys()
     hyperparam_dict = {}
-    
+
     # get a dictionary of hyperparameters and their values
     for h in hyperparams:
         hyperparam_dict.append({h: ml['schema'][h]['ui']['choices']})
-    
+
     sorted_hp = sorted(hyperparam_dict)
     # enumerate all possible hyperparameter combinations
-    all_hyperparam_combos = [dict(zip(sorted_hp,prod)) 
-                              for prod in it.product(*(hyperparam_dict[k] 
+    all_hyperparam_combos = [dict(zip(sorted_hp,prod))
+                              for prod in it.product(*(hyperparam_dict[k]
                               for k in sorted_hp))]
-                              
+
     # get random choice from all_hyperparam_combos:
     p = np.random.choice(all_hyperparam_combos)
 
@@ -92,7 +95,7 @@ def get_random_ml_p_from_db(path,key):
 
 def get_ml_id_dict(path,key):
     """Returns a dictionary for converting algorithm IDs to names."""
-    
+
     # get json from server
     payload = {'apikey':key}
     r = requests.post(path,data=json.dumps(payload), headers={'content-type':'application/json'})
