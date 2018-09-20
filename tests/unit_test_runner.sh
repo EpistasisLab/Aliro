8043aeaf05c8e3dc9c24efcc8817527341e4c762
hjwilli <hjwilli@gmail.com>
2018-09-20 18:32:53 -0400

    basic ai.py test
    References #91
    
    
    Former-commit-id: a2c8248e48010450d4637af3c22a34812084ee3b

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -5,6 +5,6 @@ mkdir -p target/test-reports/cobertura/html
 REPORT_PATH="target/test-reports"
 COVERAGE_PATH="target/test-reports/cobertura"
 
-nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" ai/tests/test_recommender.py machine/test/learn_tests.py
+nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" ai/tests/test_recommender.py ai/tests/test_ai.py machine/test/learn_tests.py
 
 rm .coverage
