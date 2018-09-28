26b163d0a05957afe37f94f8b6d622b25079df81
Weixuan Fu <weixuanf@pennmedicine.upenn.edu>
2018-09-28 09:58:15 -0400

    fix bugs and fix unit test
    Former-commit-id: fe5e0763a912d1434e96fa1faa6061664d00550c

--- machine/learn/driver.py
+++ machine/learn/driver.py
@@ -1,14 +1,11 @@
-import matplotlib as mpl
-mpl.use('Agg')
 from io_utils import Experiment, parse_args
 from skl_utils import generate_results
 
 def main(args):
     exp = Experiment(args)
-    tmpdir=exp.tmpdir
     input_data = exp.get_input()
     model, method_type = exp.get_model()
-    generate_results(model, input_data, tmpdir, args['_id'], mode=method_type)
+    generate_results(model, input_data, exp.tmpdir, args['_id'], mode=method_type)
 
 if __name__ == "__main__":
     args = parse_args()
--- machine/learn/skl_utils.py
+++ machine/learn/skl_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import os
 import json
