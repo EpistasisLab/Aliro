1c1d76bd9c7c82f5cfac716069d6236d1cbc3232
hjwilli <hjwilli@gmail.com>
2018-10-10 17:26:36 -0400

    test path update
    Former-commit-id: c236a0ae5f4d4fb509763262a782ffc68c017de3

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-rm -drf target
-mkdir -p target/test-reports/cobertura/html
 
+#rm -drf target
 REPORT_PATH="target/test-reports"
 COVERAGE_PATH="target/test-reports/cobertura"
+
+rm -f "${REPORT_PATH}/nose_xunit.xml"
+rm -drf "${COVERAGE_PATH}"
+
+mkdir -p target/test-reports/cobertura/html
 
 nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" ai/tests/test_recommender.py ai/tests/test_ai.py machine/test/learn_tests.py
 
