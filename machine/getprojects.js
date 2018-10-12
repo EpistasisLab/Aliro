d0dee1d3a0b8d4c3351ff70d3adbfcbd74442509
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2018-10-12 12:56:39 -0400

    put machine config into global config
    Former-commit-id: 08b712d8919737edf566ddcbd7836abecde2be60

--- machine/getprojects.js
+++ machine/getprojects.js
@@ -1,9 +1,11 @@
 //Generate a list of projects based on machine_config.json
 var fs = require("mz/fs");
+var machine_config_file = process.env.MACHINE_CONFIG
+console.log("Machine config:", machine_config_file)
 
 var getProjects = function() {
     var project_list = [];
-    var machine_config = JSON.parse(fs.readFileSync('./machine_config.json', 'utf-8'));
+    var machine_config = JSON.parse(fs.readFileSync(machine_config_file, 'utf-8'));
     var algorithms = machine_config["algorithms"]
 
     for (var i in algorithms) {
