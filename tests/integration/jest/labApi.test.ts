6e937bef423a07ac1e5762e12cb07c2a87dd0c9a
hjwilli <hjwilli@gmail.com>
2018-11-27 18:06:40 -0500

    dataset upload validation and tests
    lab put(api/v1/datasets) integration tests
    lab put(api/v1/datasets) parameter validation
    
    references #59

--- tests/integration/jest/labApi.test.ts
+++ tests/integration/jest/labApi.test.ts
@@ -43,6 +43,61 @@ describe('lab', () => {
 			});
 		});
 
+		it.skip('putDatasetGood', async () => {
+			var parms = {
+				_files:[],
+				_metadata:[]
+			}
+			var result = await labApi.putDataset(parms);
+
+			expect(result).toHaveProperty('message');
+			expect(result).toHaveProperty('dataset_id');
+
+			expect(result.message).toEqual("Files uploaded");
+		});
+
+		it('putDataset missing param _metadata', async () => {
+			expect.assertions(2);
+
+			var parms = {
+				_files:[]
+			}
+
+			try {
+				var result = await labApi.putDataset(parms);
+			} catch (e) {
+				console.log(`error: ${e}`)
+				console.log(e.response)
+				expect(e.response.status).toEqual(400)
+				var json = await e.response.json() // get the specific error description
+				expect(json.error).toEqual("Missing parameter _metadata")
+			}
+		});
+
+		it('putDataset missing param _metadata.filepath', async () => {
+			expect.assertions(2);
+
+			var parms = {
+				_files:[],
+				_metadata: JSON.stringify({
+					'name': 'datasetName',
+		            'username': 'testUser',
+		            'timestamp': Date.now(),
+		            'dependent_col' : 'class',
+	            })
+			}
+
+			try {
+				var result = await labApi.putDataset(parms);
+			} catch (e) {
+				console.log(`error: ${e}`)
+				console.log(e.response)
+				expect(e.response.status).toEqual(400)
+				var json = await e.response.json() // get the specific error description
+				expect(json.error).toEqual("Missing parameter _metadata.filepath")
+			}
+		});
+
 
 		it.skip('fetchDatasetMetafeatures', async () => {
 			expect.assertions(3);
