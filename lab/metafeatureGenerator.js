0d64e38a7c7309a4dce5a355347a1148f89ac6d6
hjwilli <hjwilli@gmail.com>
2019-01-17 19:54:48 -0500

    dataset refactor
    promisify generateMetafeatures.js
    split registerDataset() into Promise returning methods
    stageDatasetFile() and registerDataset()
    registerDataset() not returning correctly
    
    References #59, #122

--- lab/metafeatureGenerator.js
+++ lab/metafeatureGenerator.js
@@ -1,5 +1,6 @@
 var spawn = require("child_process").spawn;
 var spawnSync = require("child_process").spawnSync;
+var Promise = require("bluebird");
 
 /**
 * Spawn a python process that generates metafeatures for a given dataset and returns JSON formatted data on stdout.
@@ -7,9 +8,7 @@ var spawnSync = require("child_process").spawnSync;
 * 
 *
 * @param fileObj
-* @return Map - {success: Boolean, 
-*				 data: (if success) JSON
-*				 error: (if falure) String}
+* @return data: (if success) JSON
 */
 function generateFeatures(fileObj, filepath, dependent_col) {
 	console.log(`generateFeatures ('${fileObj.originalname}', '${filepath}', '${dependent_col}')`)
@@ -19,52 +18,37 @@ function generateFeatures(fileObj, filepath, dependent_col) {
 
 	console.log(`args: ${args}`)
 
-/*
-	// async
-    var metafeatureProc = spawn('python', 
-    	args, {
-            cwd: process.env.PROJECT_ROOT + '/ai/metalearning/generateTestMetafeatures.py'
-    })
-    .on("error", (err) => {
-    	console.log(`unable to spawn generateFeatures process: ${err}`)
-    })
 
-    metafeatureProc.stdout.on('data', (data) => {
-    	metafeatureJson = data
-	})
-
-	metafeatureProc.on('close', (code) => {
-	  console.log(`child process exited with code ${code}`);
-	});
-*/
-
-	var metafeatureProcObj = spawnSync('python', 
+	var metafeatureProc = spawnSync('python', 
     	args, {
             cwd: process.env.PROJECT_ROOT
     })
 
-	if (metafeatureProcObj.error != null) {
+	if (metafeatureProc.error != null) {
 		var error = `Error: unable to spawn generateFeatures process: ${err}`
 		console.log(error)
-		return {success:false, error:error}
+		throw new Error(error)
 	}
-	else if(metafeatureProcObj.status != 0) {
-		var error = `Error, generateFeatures process exited with status ${metafeatureProcObj.status}, stderr: '${metafeatureProcObj.stderr}'`
+	else if(metafeatureProc.status != 0) {
+		var error = `Error, generateFeatures process exited with status ${metafeatureProc.status}, stderr: '${metafeatureProc.stderr}'`
 		console.log(error)
-		return {success:false, error:error}
+		throw new Error(error)
 	}
 	else { 
-		try { var data = JSON.parse(metafeatureProcObj.stdout) }
+		try { var data = JSON.parse(metafeatureProc.stdout) }
 		catch (err) {
-			var error = `Error, generateFeatures process exited properly with status ${metafeatureProcObj.status}, but unable to parse stdout as JSON.  stdout: ${metafeatureProcObj.stdout}, err: ${err}`
+			var error = `Error, generateFeatures process exited properly with status ${metafeatureProc.status}, but unable to parse stdout as JSON.  stdout: ${metafeatureProc.stdout}, err: ${err}`
 			console.log(error)
-			return {success:false, error:error}
+			throw new Error(error)
 		} 
-		return {success:true, data:data}
+		return data
 	}
 }
 
+var generateFeaturesAsync = Promise.promisify(generateFeatures)
+
 
 module.exports = {
-	generateFeatures: generateFeatures
+	generateFeatures: generateFeatures,
+	generateFeaturesAsync: generateFeaturesAsync
 };
