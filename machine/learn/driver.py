b308c1faec792071805f16abe71212792041222b
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-02-06 11:17:13 -0500

    add ordinal map #121 and add unit tests #121

--- machine/learn/driver.py
+++ machine/learn/driver.py
@@ -10,17 +10,18 @@ if 'TIMEOUT' in environ:
 
 def main(args, timeout=timeout):
     exp = Experiment(args)
-    input_data, filename, target_name, categories, ordinals = exp.get_input()
-    model, method_type = exp.get_model()
+    input_data, data_info = exp.get_input()
+    model, method_type, encoding_strategy = exp.get_model()
     return_val = generate_results(model=model,
                     input_data=input_data,
                     tmpdir=exp.tmpdir,
-                    target_name=target_name,
+                    target_name=data_info['target_name'],
                     _id=args['_id'],
                     mode=method_type,
-                    filename=filename,
-                    categories=categories,
-                    ordinals=ordinals,
+                    filename=data_info['filename'],
+                    categories=data_info['categories'],
+                    ordinals=data_info['ordinals'],
+                    encoding_strategy=encoding_strategy,
                     timeout=timeout
                     )
     if return_val == "Timeout":
