0ee70e5703b4ffec48152db9cab62dba4a4d7d13
hjwilli <hjwilli@gmail.com>
2019-02-26 18:49:47 -0500

    pyutils validation for cat and ord datasets
    Need to update tests/validation to fail for strings in fields that have
    not been marked as cat/ord, and for ord cols with values that were not
    explictly provided
    
    References #121
    
    
    Former-commit-id: bc0ff561357c8d91ab20224b415d82872d02e7d0

--- lab/pyutils/validateDataset.py
+++ lab/pyutils/validateDataset.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import simplejson
 from sklearn.utils import check_X_y, check_array
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
 import os
 import os.path
 import pandas as pd
@@ -18,19 +20,73 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 
-def validate_data_from_server(file_id, target_field, **kwargs):
-    # Read the data set into memory
-    raw_data = get_file_from_server(file_id)
-    df = pd.read_csv(StringIO(raw_data), sep=None, engine='python',**kwargs)
-    return validate_data(df, target_field)
+def validate_data_from_server(file_id, target_field, categories = None, ordinals = None, **kwargs):
+	# Read the data set into memory
+	raw_data = get_file_from_server(file_id)
+	df = pd.read_csv(StringIO(raw_data), sep=None, engine='python',**kwargs)
+	return validate_data(df, target_field, categories, ordinals)
 
-def validate_data_from_filepath(file_id, target_field, **kwargs):
-    # Read the data set into memory
-    df = pd.read_csv(file_id, sep=None, engine='python',**kwargs)
-    return validate_data(df, target_field)
+def validate_data_from_filepath(file_id, target_field, categories = None, ordinals = None, **kwargs):
+	# Read the data set into memory
+	df = pd.read_csv(file_id, sep=None, engine='python',**kwargs)
+	return validate_data(df, target_field, categories, ordinals)
 
 
-def validate_data(df, target_column = None):
+def encode_data(df, target_column, categories, ordinals, encoding_strategy = "OneHotEncoder"):
+	'''
+	use OneHotEncoder or OrdinalEncoder to convert categorical features
+	See skl_utils
+	'''
+	#check_array(df, dtype=None, order="C", force_all_finite=True)
+
+	feature_names = np.array([x for x in df.columns.values if (target_column == None) or x != target_column])
+
+	if categories or ordinals:
+		transform_cols = []
+		feature_names_list = list(feature_names)
+		if categories:
+			col_idx = get_col_idx(feature_names_list, categories)
+			if encoding_strategy == "OneHotEncoder":
+				transform_cols.append(("categorical_encoder", OneHotEncoder(), col_idx))
+			elif encoding_strategy == "OrdinalEncoder":
+				transform_cols.append(("categorical_encoder", OrdinalEncoder(), col_idx))
+		if ordinals:
+			ordinal_features = sorted(list(ordinals.keys()))
+			col_idx = get_col_idx(feature_names_list, ordinal_features)
+			ordinal_map = [ordinals[k] for k in ordinal_features]
+			transform_cols.append(("ordinalencoder",
+									OrdinalEncoder(categories=ordinal_map),
+									col_idx))
+
+
+		ct = ColumnTransformer(
+								transformers=transform_cols,
+								 remainder='passthrough',
+								 sparse_threshold=0
+								 )
+		return ct.fit_transform(df)
+	else:
+		return df
+
+def get_col_idx(feature_names_list, columns):
+	"""get unique indexes of columns based on list of column names
+	see skl_utils
+	Parameters
+	----------
+
+	feature_names_list: list
+		list of column names
+	columns: list
+		list of selected column names
+
+	Returns
+	-------
+	col_idx: list
+		list of selected column indexes
+	"""
+	return [feature_names_list.index(c) for c in columns]
+
+def validate_data(df, target_column = None, categories = None, ordinals = None):
 	'''
 	Check that a datafile is valid
 
@@ -39,19 +95,15 @@ def validate_data(df, target_column = None):
 		boolean - validation result
 		string 	- message
 	'''
-	if (target_column != None):
-		
-		if not(target_column in df.columns):
-			return False, "Target column '" + target_column + "' not in data"
+	if (target_column != None) and not(target_column in df.columns):
+		return False, "Target column '" + target_column + "' not in data"
 
-		features = df.drop(target_column, axis=1).values
-		target = df[target_column].values
-
+	if categories or ordinals:
 		try:
-			features, target = check_X_y(features, target, dtype=np.float64, order="C", force_all_finite=True)
+			encode_data(df, target_column, categories, ordinals, "OneHotEncoder")
+			encode_data(df, target_column, categories, ordinals, "OrdinalEncoder")
 		except Exception as e:
-			return False, "sklearn.check_X_y() validation failed: " + str(e)
-
+			return False, "encode_data() failed: " + str(e)
 		return True, None
 
 	else:
@@ -63,68 +115,81 @@ def validate_data(df, target_column = None):
 		return True, None
 
 def get_file_from_server(file_id):
-    '''
-    Retrieve a file from the main PennAI server
-    '''
-    apiPath = 'http://' + os.environ['LAB_HOST'] + ':' + os.environ['LAB_PORT']
-    path = apiPath + "/api/v1/files/" + file_id
+	'''
+	Retrieve a file from the main PennAI server
+	'''
+	apiPath = 'http://' + os.environ['LAB_HOST'] + ':' + os.environ['LAB_PORT']
+	path = apiPath + "/api/v1/files/" + file_id
 
-    logger.debug("retrieving file:" + file_id)
-    logger.debug("api path: " + path)
+	logger.debug("retrieving file:" + file_id)
+	logger.debug("api path: " + path)
 
-    res = None
-    try:
-        res = requests.request('GET', path, timeout=15)
-    except:
-        logger.error("Unexpected error in get_file_from_server for path 'GET: " + str(path) + "': " + str(sys.exc_info()[0]))
-        raise
-    
-    if res.status_code != requests.codes.ok:
-        msg = "Request GET status_code not ok, path: '" + str(path) + "'' status code: '" + str(res.status_code) + "'' response text: " + str(res.text)
-        logger.error(msg)
-        raise RuntimeError(msg)
+	res = None
+	try:
+		res = requests.request('GET', path, timeout=15)
+	except:
+		logger.error("Unexpected error in get_file_from_server for path 'GET: " + str(path) + "': " + str(sys.exc_info()[0]))
+		raise
+	
+	if res.status_code != requests.codes.ok:
+		msg = "Request GET status_code not ok, path: '" + str(path) + "'' status code: '" + str(res.status_code) + "'' response text: " + str(res.text)
+		logger.error(msg)
+		raise RuntimeError(msg)
 
-    logger.info("File retrieved, file_id: '" + file_id + "', path: '" + path + "', status_code: " + str(res.status_code))
-    return res.text
+	logger.info("File retrieved, file_id: '" + file_id + "', path: '" + path + "', status_code: " + str(res.status_code))
+	return res.text
 
 
 def main():
-    meta_features_all = []
-    parser = argparse.ArgumentParser(description="Validate a dataset", add_help=False)
-    parser.add_argument('INPUT_FILE', type=str, help='Filepath or fileId.')
-    parser.add_argument('-target', action='store', dest='TARGET', type=str, default='class',
-                        help='Name of target column', required=False)
-    parser.add_argument('-identifier_type', action='store', dest='IDENTIFIER_TYPE', type=str, choices=['filepath', 'fileid'], default='filepath',
-                        help='Name of target column')
+	meta_features_all = []
+	parser = argparse.ArgumentParser(description="Validate a dataset", add_help=False)
+	parser.add_argument('INPUT_FILE', type=str, help='Filepath or fileId.')
+	parser.add_argument('-target', action='store', dest='TARGET', type=str, default='class',
+						help='Name of target column', required=False)
+	parser.add_argument('-identifier_type', action='store', dest='IDENTIFIER_TYPE', type=str, choices=['filepath', 'fileid'], default='filepath',
+						help='Name of target column')
+	parser.add_argument('-categorical_features', action='store', dest='JSON_CATEGORIES', type=str, required=False, default=None,
+						help='JSON list of categorical features')
+	parser.add_argument('-ordinal_features', action='store', dest='JSON_ORDINALS', type=str, required=False, default=None,
+						help='JSON dict of ordianl features and possible values')
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    # set up the file logger
-    logpath = os.path.join(os.environ['PROJECT_ROOT'], "target/logs")
-    if not os.path.exists(logpath):
-        os.makedirs(logpath)
+	# set up the file logger
+	logpath = os.path.join(os.environ['PROJECT_ROOT'], "target/logs")
+	if not os.path.exists(logpath):
+		os.makedirs(logpath)
 
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    fhandler = logging.FileHandler(os.path.join(logpath, 'validateDataset.log'))
-    fhandler.setFormatter(formatter)
-    logger.addHandler(fhandler)
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	fhandler = logging.FileHandler(os.path.join(logpath, 'validateDataset.log'))
+	fhandler.setFormatter(formatter)
+	logger.addHandler(fhandler)
 
-    success = None
-    errorMessage = None
-    meta_json = None
+	success = None
+	errorMessage = None
+	meta_json = None
 
-    try:
-        if(args.IDENTIFIER_TYPE == 'filepath'):
-            success, errorMessage = validate_data_from_filepath(args.INPUT_FILE, args.TARGET)
-        else:
-            success, errorMessage = validate_data_from_server(args.INPUT_FILE, args.TARGET)
-        meta_json = simplejson.dumps({"success":success, "errorMessage":errorMessage}, ignore_nan=True) #, ensure_ascii=False)
-    except Exception as e:
-        logger.error(traceback.format_exc())
-        meta_json = simplejson.dumps({"success":False, "errorMessage":"Exception: " + repr(e)}, ignore_nan=True) #, ensure_ascii=False)    
+	categories = None
+	ordinals = None
 
-    print(meta_json)
-    sys.stdout.flush()
+
+	try:
+		if args.JSON_CATEGORIES: categories = simplejson.loads(args.JSON_CATEGORIES)
+		if args.JSON_ORDINALS: ordinals = simplejson.loads(args.JSON_ORDINALS)
+		#print("categories: ")
+		#print(categories)
+
+		if(args.IDENTIFIER_TYPE == 'filepath'):
+			success, errorMessage = validate_data_from_filepath(args.INPUT_FILE, args.TARGET, categories, ordinals)
+		else:
+			success, errorMessage = validate_data_from_server(args.INPUT_FILE, args.TARGET, categories, ordinals)
+		meta_json = simplejson.dumps({"success":success, "errorMessage":errorMessage}, ignore_nan=True) #, ensure_ascii=False)
+	except Exception as e:
+		logger.error(traceback.format_exc())
+		meta_json = simplejson.dumps({"success":False, "errorMessage":"Exception: " + repr(e)}, ignore_nan=True) #, ensure_ascii=False)    
+
+	print(meta_json)
+	sys.stdout.flush()
 
 if __name__ == '__main__':
-    main()
+	main()
