4bc9200eebbf806ee6e8a7b97388f5c9a68bab9b
unknown <joshc0044@gmail.com>
2019-02-27 13:26:08 -0500

    basic styling changes, added test UI element to display server response after file upload

--- lab/webapp/src/components/FileUpload/index.js
+++ lab/webapp/src/components/FileUpload/index.js
@@ -4,7 +4,7 @@ import fetch from 'isomorphic-fetch';
 import React, { Component } from 'react';
 import SceneHeader from '../SceneHeader';
 import { put } from '../../utils/apiHelper';
-import { Button, Input, Form } from 'semantic-ui-react';
+import { Button, Input, Form, Segment } from 'semantic-ui-react';
 
 class FileUpload extends Component {
   constructor(props) {
@@ -36,61 +36,84 @@ class FileUpload extends Component {
     // only attempt upload if there is a selected file with a filename
     if(this.state.selectedFile && this.state.selectedFile.name) {
       let depCol = this.state.dependentCol;
-      let metadata =  JSON.stringify({
-                'name': this.state.selectedFile.name,
-                'username': 'testuser',
-                'timestamp': Date.now(),
-                'dependent_col' : depCol
-              });
-      data.append('_metadata', metadata);
+      if(depCol === '') {
+        window.console.log('please enter dependentCol value');
+      }else {
+        let metadata =  JSON.stringify({
+                  'name': this.state.selectedFile.name,
+                  'username': 'testuser',
+                  'timestamp': Date.now(),
+                  'dependent_col' : depCol
+                });
+        data.append('_metadata', metadata);
 
-      data.append('_files', this.state.selectedFile);
+        data.append('_files', this.state.selectedFile);
 
-      let myHeaders = new Headers();
-      myHeaders.append('Content-Type', 'multipart/form-data');
+        let myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'multipart/form-data');
 
-      fetch("/api/v1/datasets", {
-        method: 'PUT',
-        credentials: 'include',
-        body: data
-      }).then(response => {
-          return response.json();
-        })
-        .catch((err) => {
-               throw(err);
-        })
-        .then(json => this.setState({ serverFileUploadResp: json }));
+        fetch("/api/v1/datasets", {
+          method: 'PUT',
+          credentials: 'include',
+          body: data
+        }).then(response => {
+            return response.json();
+          })
+          .catch((err) => {
+                 throw(err);
+          })
+          .then(json => this.setState({ serverFileUploadResp: json }));
+      }
+
     } else {
       window.console.log('no file available');
     }
   }
 
   render() {
-    this.state.serverFileUploadResp ? window.console.log('file uploaded', this.state.serverFileUploadResp) : window.console.log('no dice');
+    let serverResp;
+    this.state.serverFileUploadResp ?
+      window.console.log('file uploaded', this.state.serverFileUploadResp) :
+      window.console.log('no dice');
+
+    this.state.serverFileUploadResp && this.state.serverFileUploadResp.dataset_id ?
+      serverResp = this.state.serverFileUploadResp.dataset_id : null;
+
+    let serverRespContent;
+    serverResp ? serverRespContent = (
+      <p className="file-upload-form-server-resp">
+        File uploaded: {serverResp}
+      </p>
+    ) : null;
     return (
       <div>
       <Form inverted>
-        <Input
-          type="file"
-          label="Select new dataset"
-          onChange={this.handleselectedFile}
-        />
-        <Form.Input
-          label="Dependent Column"
-          placeholder="class"
-          type="text"
-          onChange={this.handleDepColField}
-        />
-        <Button
-          inverted
-          color="blue"
-          compact
-          size="small"
-          icon="upload"
-          content="Upload dataset"
-          onClick={this.handleUpload}
-        />
+        <Segment className="file-upload-segment">
+          <Input
+            className="file-upload-form-text-input"
+            type="file"
+            label="Select new dataset"
+            onChange={this.handleselectedFile}
+          />
+          <Form.Input
+            label="Dependent Column"
+            placeholder="class"
+            type="text"
+            onChange={this.handleDepColField}
+          />
+          <Button
+            inverted
+            color="blue"
+            compact
+            size="small"
+            icon="upload"
+            content="Upload dataset"
+            onClick={this.handleUpload}
+          />
+        </Segment>
       </Form>
+      <br/>
+      {serverRespContent ? serverRespContent : ''}
       </div>
     );
   }
