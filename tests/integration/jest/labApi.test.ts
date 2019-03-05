97159411400ee87ebdfdceff20c49188b5d7333c
hjwilli <hjwilli@gmail.com>
2019-03-05 13:55:47 -0500

    api validation/tests

--- tests/integration/jest/labApi.test.ts
+++ tests/integration/jest/labApi.test.ts
@@ -14,11 +14,289 @@ const DATASET_PATH = '/appsrc/data/datasets/test/integration'
 
 describe('lab', () => {
 	describe('api', () => {
+		describe.each`
+			testname					| filename						| categorical	| ordinal
+			${'numeric'}				| ${'appendicitis_2.csv'}		| ${[]}			| ${{}}
+			${'categorical'}			| ${'appendicitis_cat.csv'}		| ${["cat"]}	| ${{}}
+			${'categorical_ordinal'}	| ${'appendicitis_cat_ord.csv'}	| ${["cat"]}	| ${ {"ord" : ["first", "second", "third"]} }
+			`("putDatasetGood", ({testname, filename, categorical, ordinal}) => {
+				it(`${testname}`, async () => {
+					let filepath = `${DATASET_PATH}/${filename}`
+
+					console.log(`${testname} ${filename} ${categorical} ${ordinal}`)
+					let form = new FormData();
+
+					let metadata =  JSON.stringify({
+							'name': filename,
+				            'username': 'testuser',
+				            'timestamp': Date.now(),
+				            'dependent_col' : 'target_class',
+				            'categorical_features' : categorical,
+				            'ordinal_features' : ordinal
+			            })
+					console.log(metadata)
+					form.append('_metadata', metadata)
+					form.append('_files', fs.createReadStream(filepath));
+
+					let result
+
+					try {
+						result = await labApi.putDataset(form);
+					}
+					catch (e) {
+						var json = await e.response.json()
+						expect(json.error).toBeUndefined()
+						expect(e).toBeUndefined()
+					}
+
+					expect(result).toHaveProperty('message');
+					expect(result).toHaveProperty('dataset_id');
+
+					expect(result.message).toEqual("Files uploaded");
+				} 
+			}
+		)
+
+
+		describe('putDatasetBad', () => {
+			it('string data in file', async () => {
+				expect.assertions(3);
+
+				let filepath = `${DATASET_PATH}/appendicitis_cat.csv`
+
+				let form = new FormData();
+
+				let metadata =  JSON.stringify({
+						'name': 'appendicitis_cat_datasetbad.csv',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'target_class',
+			            'categorical_features' : []		
+		            })
+
+				form.append('_metadata', metadata)
+				form.append('_files', fs.createReadStream(filepath));
+
+				let result
+
+				try {
+					result = await labApi.putDataset(form);
+				}
+				catch (e) {
+					var json = await e.response.json()
+					expect(json.error).toBeTruthy()
+					expect(e.response.status).toEqual(400)
+					expect(json.error).toEqual("Unable to upload files: Error: Error: Datafile validation failed: sklearn.check_array() validation failed: could not convert string to float: 'b'")
+				}
+			});
+
+			it('invalid metadata key', async () => {
+				expect.assertions(3);
+
+				let filepath = `${DATASET_PATH}/appendicitis_2.csv`
+
+				let form = new FormData();
+
+				let metadata =  JSON.stringify({
+						'name': 'appendicitis_2_datasetbad.csv',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'target_class',
+			            'foobar' : 'foo'
+		            })
+
+				form.append('_metadata', metadata)
+				form.append('_files', fs.createReadStream(filepath));
+
+				let result
+
+				try {
+					result = await labApi.putDataset(form);
+				}
+				catch (e) {
+					var json = await e.response.json()
+					expect(json.error).toBeTruthy()
+					expect(e.response.status).toEqual(400)
+					expect(json.error).toEqual("invalid _metadata key: foobar")
+				}
+			});
+
+			it('bad username', async () => {
+				expect.assertions(3);
+
+				let filepath = `${DATASET_PATH}/appendicitis_2.csv`
+
+				let form = new FormData();
+
+				let metadata =  JSON.stringify({
+						'name': 'appendicitis.csv',
+			            'username': 'testUser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'target_class'
+		            })
+
+				form.append('_metadata', metadata)
+				form.append('_files', fs.createReadStream(filepath));
+
+				let result
+
+				try {
+					result = await labApi.putDataset(form);
+				}
+				catch (e) {
+					var json = await e.response.json()
+					expect(json.error).toBeTruthy()
+					expect(e.response.status).toEqual(400)
+					expect(json.error).toEqual("Unable to upload files: Error: Error: Metadata validation failed, username 'testUser' does not exist.")
+				}
+			});
+
+			it('existing filename', async () => {
+				expect.assertions(3);
+
+				let filepath = `${DATASET_PATH}/banana.csv`
+
+				let form = new FormData();
+
+				let metadata =  JSON.stringify({
+						'name': 'banana',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'class'
+		            })
+
+				form.append('_metadata', metadata)
+				form.append('_files', fs.createReadStream(filepath));
+
+				let result
+
+				try {
+					result = await labApi.putDataset(form);
+				}
+				catch (e) {
+					var json = await e.response.json()
+					expect(json.error).toBeTruthy()
+					expect(e.response.status).toEqual(400)
+					expect(json.error).toEqual("Unable to upload files: Error: Error: Metadata validation failed, dataset with name 'banana' has already been registered, count: 1.")
+				}
+			});
+
+			it('missing param _metadata', async () => {
+				expect.assertions(2);
+
+				let form = new FormData();
+
+				try {
+					var result = await labApi.putDataset(form);
+				} catch (e) {
+					var json = await e.response.json() // get the specific error description
+					expect(e.response.status).toEqual(400)
+					expect(json.error).toEqual("Missing parameter _metadata")
+				}
+			});
+
+			it('empty files array', async () => {
+				expect.assertions(1);
+
+				var metadata = JSON.stringify({
+						'name': 'datasetName',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'class',
+		            })
+
+				let form = new FormData();
+				form.append('_metadata', metadata)
+
+				try {
+					var result = await labApi.putDataset(form);
+				} catch (e) {
+					var json = await e.response // get the specific error description
+					expect(e.response.status).toEqual(400)
+					//expect(json.error).toEqual("_files does not have length 1")
+				}
+			});
+
+			it('incorrect file object', async () => {
+				expect.assertions(1);
+
+				var metadata = JSON.stringify({
+						'name': 'datasetName',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'class',
+		            })
+
+				let form = new FormData();
+				form.append('_metadata', metadata)
+				form.append('_files', 'im not a file');
+
+				try {
+					var result = await labApi.putDataset(form);
+				} catch (e) {
+					var json = await e.response // get the specific error description
+					expect(e.response.status).toEqual(400)
+					//expect(json.error).toEqual("_files[0] is not of type File")
+				}
+			});
+
+			it('multiple files', async () => {
+				expect.assertions(1);
+
+				let filepath1 = `${DATASET_PATH}/appendicitis_cat.csv`
+				let filepath2 = `${DATASET_PATH}/banana.csv`
+
+				var metadata = JSON.stringify({
+						'name': 'datasetName',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'class',
+		            })
+
+				let form = new FormData();
+				form.append('_metadata', metadata)
+				form.append('_files', fs.createReadStream(filepath1));
+				form.append('_files', fs.createReadStream(filepath2));
+
+				try {
+					var result = await labApi.putDataset(form);
+				} catch (e) {
+					var json = await e.response // get the specific error description
+					expect(e.response.status).toEqual(500)
+				}
+			});
+
+			it('empty file', async () => {
+				expect.assertions(1);
+
+				let filepath = `${DATASET_PATH}/empty.csv`
+
+				var metadata = JSON.stringify({
+						'name': 'datasetName',
+			            'username': 'testuser',
+			            'timestamp': Date.now(),
+			            'dependent_col' : 'class',
+		            })
+
+				let form = new FormData();
+				form.append('_metadata', metadata)
+				form.append('_files', fs.createReadStream(filepath));
+
+				try {
+					var result = await labApi.putDataset(form);
+				} catch (e) {
+					var json = await e.response // get the specific error description
+					expect(e.response.status).toEqual(400)
+					//expect(json.error).toEqual("_files[0] has size 0")
+				}
+			});
+		};
+
 		// fetch datasets and check for metafeatures
 		it('fetchDatasets', () => {
 			expect.assertions(5);
 			return labApi.fetchDatasets().then((data) => {
-				expect(data.length).toEqual(EXPECTED_DATASET_COUNT);
+				expect(data.length).toBeGreaterThanOrEqual(EXPECTED_DATASET_COUNT);
 				var banana = data.find(function(element) {
 				  return element.name == 'banana';
 				});
@@ -38,237 +316,12 @@ describe('lab', () => {
 		 	expect(bananaId).toBeTruthy();
 
 		 	return labApi.fetchDataset(bananaId).then((data) => {
-		 		console.log(data)
+		 		//console.log(data)
 		  		expect(data.length).toEqual(1);
 		  		expect(data[0].metafeatures).toBeTruthy();
 				expect(data[0].metafeatures).toHaveProperty('n_rows')
 				expect(data[0].metafeatures).toHaveProperty('n_columns')
 			});
-		});
-
-		it('putDatasetGood', async () => {
-			let filepath = `${DATASET_PATH}/appendicitis_2.csv`
-
-			let form = new FormData();
-
-			let metadata =  JSON.stringify({
-					'name': 'appendicitis_2.csv',
-		            'username': 'testuser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'target_class'
-	            })
-
-			form.append('_metadata', metadata)
-			form.append('_files', fs.createReadStream(filepath));
-
-			let result
-
-			try {
-				result = await labApi.putDataset(form);
-			}
-			catch (e) {
-				var json = await e.response.json()
-				expect(json.error).toBeUndefined()
-				expect(e).toBeUndefined()
-			}
-
-			expect(result).toHaveProperty('message');
-			expect(result).toHaveProperty('dataset_id');
-
-			expect(result.message).toEqual("Files uploaded");
-		});
-
-		it('putDataset_badStringData', async () => {
-			expect.assertions(3);
-
-			let filepath = `${DATASET_PATH}/appendicitis_cat.csv`
-
-			let form = new FormData();
-
-			let metadata =  JSON.stringify({
-					'name': 'appendicitis_cat.csv',
-		            'username': 'testuser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'target_class'
-	            })
-
-			form.append('_metadata', metadata)
-			form.append('_files', fs.createReadStream(filepath));
-
-			let result
-
-			try {
-				result = await labApi.putDataset(form);
-			}
-			catch (e) {
-				var json = await e.response.json()
-				expect(json.error).toBeTruthy()
-				expect(e.response.status).toEqual(400)
-				expect(json.error).toEqual("Unable to upload files: Error: Error: Datafile validation failed: sklearn.check_array() validation failed: could not convert string to float: 'b'")
-			}
-		});
-
-		it('putDataset_badUsername', async () => {
-			expect.assertions(3);
-
-			let filepath = `${DATASET_PATH}/appendicitis_2.csv`
-
-			let form = new FormData();
-
-			let metadata =  JSON.stringify({
-					'name': 'appendicitis.csv',
-		            'username': 'testUser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'target_class'
-	            })
-
-			form.append('_metadata', metadata)
-			form.append('_files', fs.createReadStream(filepath));
-
-			let result
-
-			try {
-				result = await labApi.putDataset(form);
-			}
-			catch (e) {
-				var json = await e.response.json()
-				expect(json.error).toBeTruthy()
-				expect(e.response.status).toEqual(400)
-				expect(json.error).toEqual("Unable to upload files: Error: Error: Metadata validation failed, username 'testUser' does not exist.")
-			}
-		});
-
-		it('putDataset_bad_existingFilename', async () => {
-			expect.assertions(3);
-
-			let filepath = `${DATASET_PATH}/banana.csv`
-
-			let form = new FormData();
-
-			let metadata =  JSON.stringify({
-					'name': 'banana',
-		            'username': 'testuser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'class'
-	            })
-
-			form.append('_metadata', metadata)
-			form.append('_files', fs.createReadStream(filepath));
-
-			let result
-
-			try {
-				result = await labApi.putDataset(form);
-			}
-			catch (e) {
-				var json = await e.response.json()
-				expect(json.error).toBeTruthy()
-				expect(e.response.status).toEqual(400)
-				expect(json.error).toEqual("Unable to upload files: Error: Error: Metadata validation failed, dataset with name 'banana' has already been registered, count: 1.")
-			}
-		});
-
-		it('putDatasetGood_categorical', async () => {
-			let filepath = `${DATASET_PATH}/appendicitis_cat.csv`
-
-			let form = new FormData();
-
-			let metadata =  JSON.stringify({
-					'name': 'appendicitis_cat.csv',
-		            'username': 'testuser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'target_class',
-		            'categorical_features' : ["cat"]
-	            })
-
-			form.append('_metadata', metadata)
-			form.append('_files', fs.createReadStream(filepath));
-
-			let result
-
-			try {
-				result = await labApi.putDataset(form);
-			}
-			catch (e) {
-				var json = await e.response.json()
-				expect(json.error).toBeUndefined()
-				expect(e).toBeUndefined()
-			}
-
-			expect(result).toHaveProperty('message');
-			expect(result).toHaveProperty('dataset_id');
-
-			expect(result.message).toEqual("Files uploaded");
-		});
-
-		it('putDatasetGood_categorical_ordinal', async () => {
-			let filepath = `${DATASET_PATH}/appendicitis_cat_ord.csv`
-
-			let form = new FormData();
-
-			let metadata =  JSON.stringify({
-					'name': 'appendicitis_cat_ord.csv',
-		            'username': 'testuser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'target_class',
-		            'categorical_features' : ["cat"],
-		            'ordinal_features' : {"ord" : ["first", "second", "third"]
-	            })
-
-			form.append('_metadata', metadata)
-			form.append('_files', fs.createReadStream(filepath));
-
-			let result
-
-			try {
-				result = await labApi.putDataset(form);
-			}
-			catch (e) {
-				var json = await e.response.json()
-				expect(json.error).toBeUndefined()
-				expect(e).toBeUndefined()
-			}
-
-			expect(result).toHaveProperty('message');
-			expect(result).toHaveProperty('dataset_id');
-
-			expect(result.message).toEqual("Files uploaded");
-		});
-
-		it('putDataset missing param _metadata', async () => {
-			expect.assertions(2);
-
-			let form = new FormData();
-
-			try {
-				var result = await labApi.putDataset(form);
-			} catch (e) {
-				var json = await e.response.json() // get the specific error description
-				expect(json.error).toEqual("Missing parameter _metadata")
-				expect(e.response.status).toEqual(400)
-			}
-		});
-
-		it('putDataset empty file array', async () => {
-			expect.assertions(1);
-
-			var metadata = JSON.stringify({
-					'name': 'datasetName',
-		            'username': 'testuser',
-		            'timestamp': Date.now(),
-		            'dependent_col' : 'class',
-	            })
-
-			let form = new FormData();
-			form.append('_metadata', metadata)
-
-			try {
-				var result = await labApi.putDataset(form);
-			} catch (e) {
-				var json = await e.response // get the specific error description
-				expect(e.response.status).toEqual(400)
-				//expect(json.error).toEqual("_files does not have length 1")
-			}
 		});
 
 
