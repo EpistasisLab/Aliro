5d070fe3d932fc47e36e22c097a17b9ad417845e
hjwilli <hjwilli@gmail.com>
2019-03-29 11:09:28 -0400

    working on restartability, static scaling, stability
    ai engine -  bugfixes/better catch and report errors
    global config - remove global config variable/dependence on MACHINE_PORT
    lab server - during lab server init, clear any previously registered
    machine instances
    lab server - added specific call to register a machine instance
    machine.js - now always will register itself upon init, no longer uses a
    local file to attempt to assertain if it was registered previously
    integration tests - base container version updated
    
    References #53, #148

--- dockers/lab/files/entrypoint.sh
+++ dockers/lab/files/entrypoint.sh
@@ -19,8 +19,16 @@ echo "datasets loaded..."
 if [ ${AI_AUTOSTART} -eq 1 ]; then
     echo "autostarting ai..."
 
-    echo "waiting for machine to be responsive..."
-    /root/wait-for-it.sh -t 40 ${MACHINE_HOST}:${MACHINE_PORT} -- echo "machine wait over"
+#   TODO: split ai to a seperate docker container; should be dependent on lab, db, and machine.
+#       For now, wait for the singular machine to be active, or wait a set amount of time in the case of 
+#       machine scaling.
+
+#    echo "waiting for machine to be responsive..."
+#    /root/wait-for-it.sh -t 40 ${MACHINE_HOST}:${MACHINE_PORT} -- echo "machine wait over"
+
+    echo "waiting set time to allow machines to start..."
+    sleep 10
+      
 
     echo "starting ai..."
 
