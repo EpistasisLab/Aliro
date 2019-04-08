3143e5fb564b8b635b48451df99500b969b785af
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-04-08 15:45:54 -0400

    add machine_utils and unit tests #150
    Former-commit-id: 9df8a1713c29ec46ca124dcef39fc301a3ff70ee

--- machine/test/test.js
+++ machine/test/test.js
@@ -1,9 +1,13 @@
 process.env.MACHINE_CONFIG = './test/test_machine_config.json'
-const getProjects = require('../getprojects.js');
+const machine_utils = require('../machine_utils.js');
 const assert = require('assert');
 
 var algorithms = ["DecisionTreeClassifier","GradientBoostingClassifier","KNeighborsClassifier"]
-var project_list = getProjects(algorithms)
+var project_list = machine_utils.getProjects(algorithms)
+var projects = {};
+project_list.forEach(function (value, i) {
+    projects[algorithms[i]] = value;
+});
 
 describe('Mocha Test for getProjects function', function () {
   it('Test the total number of algorithms is the same with config.', function () {
@@ -16,6 +20,32 @@ describe('Mocha Test for getProjects function', function () {
             var algo_conf = algorithms[i];
             assert.equal(algo_conf, algo);
         }
+    });
+
+  });
+
+describe('Mocha Test for getCapacity function', function () {
+  it('Test getCapacity returns the correct capacity.', function () {
+        assert.equal(machine_utils.getCapacity("DecisionTreeClassifier", 1, projects), 1);
+    });
+
+  it('Test getCapacity returns the correct capacity when maxCapacity=2.', function () {
+        assert.equal(machine_utils.getCapacity("GradientBoostingClassifier", 2, projects), 2);
+    });
+  });
+
+
+describe('Mocha Test for getCapacity function', function () {
+  it('Test checkCapacity returns the correct values.', function () {
+        ret = machine_utils.checkCapacity("DecisionTreeClassifier", 0, projects);
+        assert.equal(ret.capacity, 0);
+        assert.equal(ret.error_msg.error, "No capacity available");
+    });
+
+  it('Test checkCapacity returns the correct values when projId is not defined', function () {
+        ret = machine_utils.checkCapacity("DecisionTreeClassifie", 2, projects);
+        assert.equal(ret.capacity, 0);
+        assert.equal(ret.error_msg.error, "Project 'DecisionTreeClassifie' does not exist");
     });
 
   });
