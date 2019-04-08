fb30f9cbafc182e643ea8c1a52da21dd8a6350b0
unknown <joshc0044@gmail.com>
2019-04-08 16:50:35 -0400

    adding missing CSS styling and modification to SceneHeader component

--- lab/webapp/src/components/SceneHeader/index.jsx
+++ lab/webapp/src/components/SceneHeader/index.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Header, Button } from 'semantic-ui-react';
+import { Link } from 'react-router';
 
 function SceneHeader({
-  header, 
-  subheader, 
-  btnText, 
-  btnIcon, 
-  //btnAction
+  header,
+  subheader,
+  btnText,
+  btnIcon,
+  linkText
 }) {
   return (
     <div className="scene-header">
-      <Header 
-        inverted 
-        size="huge" 
+      <Header
+        inverted
+        size="huge"
         content={header}
         subheader={subheader}
       />
       {btnText &&
-        <Button 
-          inverted 
-          color="blue" 
-          compact 
-          size="small" 
+        <Button
+          inverted
+          color="blue"
+          compact
+          size="small"
           icon={btnIcon}
           content={btnText}
+          as={ Link }
+          to={linkText}
         />
       }
     </div>
