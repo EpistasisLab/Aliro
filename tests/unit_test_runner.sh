c5e577a457a806370aed7d0581afaf7a765e5a6f
unknown <joshc0044@gmail.com>
2019-04-18 16:14:11 -0400

    minor color change for test graph on result page and added beginnings of adding unit tests to existing automated testing pieces
    Former-commit-id: c3029c8518f9308058483ddae0f80d546d8a21dd

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -5,7 +5,7 @@ COVERAGE_PATH="target/test-reports/cobertura"
 
 NOSE_TESTS="ai/tests/test_recommender.py ai/tests/test_ai.py ai/tests/test_ai_and_api_utils.py machine/test/learn_tests.py ai/tests/test_knowledgebase_loader.py ai/metalearning/tests/test_dataset_describe.py lab/pyutils/tests/test_loadInitialDatasets.py lab/pyutils/tests/test_validateDataset.py"
 MOCHA_TESTS="machine/test/test.js"
-
+#lab/webapp/src/components/FileUpload/FileUpload.test.js
 rm -f "${REPORT_PATH}/nose_xunit.xml"
 rm -f "${REPORT_PATH}/html/nose.html"
 rm -f "${REPORT_PATH}/mocha_xunit.xml"
@@ -13,8 +13,12 @@ rm -drf "${COVERAGE_PATH}"
 
 mkdir -p target/test-reports/cobertura/html
 
-nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS
+#nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS
 #nosetests -s --with-html --html-file="${REPORT_PATH}/html/nose.html" $NOSE_TESTS
 mocha --reporter xunit --reporter-options output="${REPORT_PATH}/mocha_xunit.xml" $MOCHA_TESTS
 
-rm .coverage
+#rm .coverage
+
+cd "lab/webapp/"
+npm install
+npm run test
