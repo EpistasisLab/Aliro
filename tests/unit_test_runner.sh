9c417a9aa52bea7e75a661fbeb8ddab8346c8f51
unknown <joshc0044@gmail.com>
2019-04-18 17:18:43 -0400

    attempting to output generated test reports in specified location

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -15,10 +15,13 @@ mkdir -p target/test-reports/cobertura/html
 
 #nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS
 #nosetests -s --with-html --html-file="${REPORT_PATH}/html/nose.html" $NOSE_TESTS
-mocha --reporter xunit --reporter-options output="${REPORT_PATH}/mocha_xunit.xml" $MOCHA_TESTS
+#mocha --reporter xunit --reporter-options output="${REPORT_PATH}/mocha_xunit.xml" $MOCHA_TESTS
 
 #rm .coverage
 
 cd "lab/webapp/"
-npm install
+
+export JEST_HTML_REPORTER_OUTPUT_PATH="${REPORT_PATH}/html/unit_webapp_jest_test_report.html"
+export JEST_JUNIT_OUTPUT_DIRECTORY="${REPORT_PATH}"
+
 npm run test
