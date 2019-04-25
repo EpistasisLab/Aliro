d4b74d8a439c533fb8ad26be7ee0031d4893df2f
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-04-25 15:25:24 -0400

    better fmt

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -12,7 +12,11 @@ rm -drf "${COVERAGE_PATH}"
 
 mkdir -p target/test-reports/cobertura/html
 
-nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS
+nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" \
+--with-coverage --cover-inclusive \
+--cover-package=. --cover-xml \
+--cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" \
+--cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS
 cd /appsrc/machine && npm install --silent --progress=false --production --no-save && cd /appsrc/
 mocha --reporter xunit --reporter-options output="${REPORT_PATH}/mocha_xunit.xml" $MOCHA_TESTS
 
