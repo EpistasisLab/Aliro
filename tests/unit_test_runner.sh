7e2678b062e00d810105b5b6703bce0f838919e0
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-04-25 14:33:58 -0400

    fix issue #174

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -7,14 +7,13 @@ NOSE_TESTS="ai/tests/test_recommender.py ai/tests/test_ai.py ai/tests/test_ai_an
 MOCHA_TESTS="machine/test/test.js"
 
 rm -f "${REPORT_PATH}/nose_xunit.xml"
-rm -f "${REPORT_PATH}/html/nose.html"
 rm -f "${REPORT_PATH}/mocha_xunit.xml"
 rm -drf "${COVERAGE_PATH}"
 
 mkdir -p target/test-reports/cobertura/html
 
 nosetests -s --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" --with-coverage  --cover-inclusive --cover-package=. --cover-xml --cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" --cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS
-#nosetests -s --with-html --html-file="${REPORT_PATH}/html/nose.html" $NOSE_TESTS
+
 mocha --reporter xunit --reporter-options output="${REPORT_PATH}/mocha_xunit.xml" $MOCHA_TESTS
 
 rm .coverage
