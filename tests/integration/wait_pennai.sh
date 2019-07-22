38a46dce17c3fee07a28f8782c581c0bf8adac04
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-07-22 15:53:19 -0400

    clean codes
    Former-commit-id: 73575cfff2f0c71b35d67f4e66f2c131b1223579

--- tests/integration/wait_pennai.sh
+++ tests/integration/wait_pennai.sh
@@ -6,7 +6,6 @@ echo "waiting for lab to be responsive..."
 echo "waiting for machine to be responsive..."
 /opt/wait-for-it.sh -t 30 machine:5081 -- echo "machine wait over"
 
-#npm test -- --json --outputFile="/results/jest.json"
 
 # for now, hardcode some time for the datasets to get loaded
 echo "hardcoded sleep to load datasets..."
