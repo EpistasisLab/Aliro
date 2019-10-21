86a06d84b4b01e8f0fab2684dc5f994e29fc20fd
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-10-21 13:50:18 -0400

    add more regression algorithms #229
    Former-commit-id: 0ce6e17190da763922fcdcadc369e32b7ec93ab3

--- tests/integration/jest/util/testUtils.js
+++ tests/integration/jest/util/testUtils.js
@@ -2,7 +2,7 @@
 var os = require("os");
 
 
-export const EXPECTED_MACHINE_ALGO_COUNT = 8; // number of algorithms registered with a machine instance
+export const EXPECTED_MACHINE_ALGO_COUNT = 12; // number of algorithms registered with a machine instance
 export const MIN_EXPECTED_LAB_ALGO_COUNT = 10; // min number of algorithms registered with in the server
 export const MIN_EXPECTED_DATASET_COUNT = 5; // min number of datasets registered with the lab server, more may be added w. tests
 
