666247e51b8a61f3fa5ea15d731d32ec5a550d4a
weixuanfu <weixuanf@pennmedicine.upenn.edu>
2019-12-02 13:54:04 -0500

    merge qq plot and resi plot #230
    Former-commit-id: c7f9f5958d657663ff202464cf176d1258036388

--- machine/learn/skl_utils.py
+++ machine/learn/skl_utils.py
@@ -763,7 +763,7 @@ def plot_cv_pred(tmpdir, _id, X, y, cv_scores):
         pred_y[test] = est.predict(X[test])
         resi_y[test] = pred_y[test] - y[test]
 
-    fig, ax = plt.subplots(figsize=(6,6), dpi=300)
+    fig, ax = plt.subplots(figsize=(8,6), dpi=300)
     ax.set_title("Cross-Validated Predictions")
     ax.scatter(y, pred_y, edgecolors=(0, 0, 0))
     ax.set_xlabel('Observed Values')
@@ -776,33 +776,28 @@ def plot_cv_pred(tmpdir, _id, X, y, cv_scores):
     plt.savefig(tmpdir + _id + '/reg_cv_pred_' + _id + '.png')
     plt.close()
 
-    fig, ax = plt.subplots(figsize=(6,6), dpi=300)
-    ax.set_title("Cross-Validated Residuals")
-    ax.scatter(pred_y, resi_y, edgecolors=(0, 0, 0))
-    ax.set_xlabel('Predicted Values')
-    ax.set_ylabel('Residuals')
-    plt.axhline(y=0.0,
+    fig, ax = plt.subplots(1, 2, figsize=(8,6), dpi=300)
+    ax[0].set_title("Cross-Validated Residuals")
+    ax[0].scatter(pred_y, resi_y, edgecolors=(0, 0, 0))
+    ax[0].set_xlabel('Predicted Values')
+    ax[0].set_ylabel('Residuals')
+    ax[0].axhline(y=0.0,
                 color="red",
                 linestyle='dashed')
-    plt.tight_layout()
-    plt.savefig(tmpdir + _id + '/reg_cv_resi_' + _id + '.png')
-    plt.close()
     # add q-q plot for normalized CV residuals
     from scipy.stats import probplot
     z = (resi_y-np.mean(resi_y))/np.std(resi_y)
-    fig, ax = plt.subplots(figsize=(6,6), dpi=300)
     series1  = probplot(z, dist="norm")
-    ax.scatter(series1[0][0],series1[0][1], edgecolors=(0, 0, 0))
-    ax.set_title("Q-Q Plot for Normalized Residuals")
-    x = np.linspace(*ax.get_xlim())
-    ax.plot(x, x,
+    ax[1].scatter(series1[0][0],series1[0][1], edgecolors=(0, 0, 0))
+    ax[1].set_title("Q-Q Plot for Normalized Residuals")
+    x = np.linspace(*ax[1].get_xlim())
+    ax[1].plot(x, x,
             color="red",
             linestyle='dashed')
-    ax.set_xlabel('Theoretical Quantiles')
-    ax.set_ylabel('Ordered Normalized Residuals')
-
+    ax[1].set_xlabel('Theoretical Quantiles')
+    ax[1].set_ylabel('Ordered Normalized Residuals')
     plt.tight_layout()
-    plt.savefig(tmpdir + _id + '/qq_plot_cv_resi_' + _id + '.png')
+    plt.savefig(tmpdir + _id + '/reg_cv_resi_' + _id + '.png')
     plt.close()
 
 
