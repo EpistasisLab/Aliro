ff59deaf7ae2554d8065e1298e38fd67c61149de
hjwilli <hjwilli@gmail.com>
2020-01-17 11:52:22 -0500

    Production release pipeline
    add script to generate production release
    start script to automate deployment of production release
    
    References #221
    
    
    Former-commit-id: a994a6d90780586cdb84d5531814ec6df8e8e572

---
+++ release/deploy_production_release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PROD_BASE_DIR="target/production"
+
+# get the current version
+source .env 
+echo "tag: $TAG"
+
+# create and zip production folder
+PROD_BUILD_DIR="${PROD_BASE_DIR}/pennai-${TAG}"
+PROD_ZIP_FILENAME="${PROD_BASE_DIR}/pennai-${TAG}.zip"
+
+
+# make sure the docker images and production zip exist
+docker image inspect moorelab/pennai_lab:${TAG}
+docker image inspect moorelab/pennai_machine:${TAG}
+docker image inspect moorelab/pennai_dbmongo:${TAG}
+
+echo "Found docker images"
+
+if [ ! -f ${PROD_ZIP_FILENAME} ]; then
+	echo "ERROR: File '${PROD_ZIP_FILENAME}' not found.  Exiting."
+	exit 1
+else
+	echo "Found release file '${PROD_ZIP_FILENAME}'"
+fi
+
+
+# create git relase and attach zip
+echo "Creating Github release and pushing production zip"
+
+
+# push to dockerhub
+echo "Pushing to images to DockerHub"
+#docker push moorelab/pennai_lab:${TAG}
+#docker push moorelab/pennai_machine:${TAG}
+#docker push moorelab/pennai_dbmongo:${TAG}
