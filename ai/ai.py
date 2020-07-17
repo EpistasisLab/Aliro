241cd027ccacd6f03ad23a2aad79114b456d5e61
hjwilli <hjwilli@gmail.com>
2020-07-17 10:15:05 -0400

    bugfix
    Ref #241
    
    
    Former-commit-id: 2bc4f069ef2ff8b040c9c7f77f74e8e6ae4206fc

--- ai/ai.py
+++ ai/ai.py
@@ -209,11 +209,12 @@ class AI():
             logger.debug("initializing engine")
             recArgs = self.DEFAULT_REC_ARGS[pred_type]
             recArgs['ml_p'] = ml_p
+
             recArgs['serialized_rec_directory'] = 'data/recommenders/'
             recArgs['load_serialized_rec'] = "if_exists" 
 
             if kb is not None:
-                recArgs['serialized_recomender_knowledgebase'] = kb['resultsData'][pred_type]
+                recArgs['serialized_rec_knowledgebase'] = kb['resultsData'][pred_type]
 
             if (rec_class):
                 self.rec_engines[pred_type] = rec_class(**recArgs)
