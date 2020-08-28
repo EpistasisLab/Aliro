afdec7533b2221511e030a1088cd545d429806a9
lacava <williamlacava@gmail.com>
2020-08-28 10:41:36 -0400

    adds headers to source files

--- utils/make_source_headers.py
+++ utils/make_source_headers.py
@@ -1,3 +1,27 @@
+"""This file is part of the PennAI library.
+
+Copyright (C) 2017 Epistasis Lab, University of Pennsylvania
+
+PennAI is maintained by:
+    - Heather Williams (hwilli@upenn.edu)
+    - Weixuan Fu (weixuanf@pennmedicine.upenn.edu)
+    - William La Cava (lacava@upenn.edu)
+    - and many other generous open source contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
 import os
 from pathlib import Path
 
@@ -18,15 +42,24 @@ print('json_header',json_header)
 
 # recurse thru files and add header w/ appropriate block comments
 for f in python_files:
-    print('updating',f)
     with open(f, 'r') as original: 
         data = original.read()
+    if "This file is part of the PennAI library" in data:
+        print('skipping',f,', already has header')
+        continue
+    print('updating',f)
     with open(f, 'w') as modified: 
         modified.write(python_header + data)
 
 for f in json_files:
-    print('updating',f)
+    if any([jf in str(f) for jf in ['node_modules','dist','src']]):
+        print('skipping',f)
+        continue
     with open(f, 'r') as original: 
         data = original.read()
+    if "This file is part of the PennAI library" in data:
+        print('skipping',f,', already has header')
+        continue
+    print('updating',f)
     with open(f, 'w') as modified: 
         modified.write(json_header + data)
