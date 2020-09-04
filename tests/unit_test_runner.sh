eaf431603fc43b5b903d9d65689fac4bb40a6107
hjwilli <hjwilli@gmail.com>
2020-09-04 09:32:11 -0400

    travis/coveralls
    update python coverage package to 5.2.1
    update unit test runners to generate updated coverage reports
    
    Ref #280
    
    
    Former-commit-id: c8003b544614b6f971d7ce8717e469c2804a7647

--- tests/unit_test_runner.sh
+++ tests/unit_test_runner.sh
@@ -15,7 +15,7 @@ NOSE_TESTS="${NOSE_TESTS} ai/metalearning/tests/test_get_metafeatures.py"
 NOSE_TESTS="${NOSE_TESTS} machine/test/learn_tests.py"
 NOSE_TESTS="${NOSE_TESTS} lab/pyutils/tests/test_loadInitialDatasets.py"
 NOSE_TESTS="${NOSE_TESTS} lab/pyutils/tests/test_validateDataset.py"
-#NOSE_TESTS="${NOSE_TESTS} ai/tests/test_knowledgebase_utils_profile.py"
+##NOSE_TESTS="${NOSE_TESTS} ai/tests/test_knowledgebase_utils_profile.py"
 
 MOCHA_TESTS="machine/test/test.js"
 
@@ -28,15 +28,16 @@ mkdir -p target/test-reports/html
 
 echo "starting nosetest"
 echo $NOSE_TESTS
-nosetests -s \
+
+coverage run -m nose -s \
 --with-xunit --xunit-file="${REPORT_PATH}/nose_xunit.xml" \
 --with-html --html-file="${REPORT_PATH}/html/nose.html" \
---with-coverage --cover-inclusive \
---cover-package=. --cover-xml \
---cover-xml-file="${COVERAGE_PATH}/nose_cover.xml" \
---cover-html --cover-html-dir="${COVERAGE_PATH}/html" $NOSE_TESTS \
+-v $NOSE_TESTS \
 #--verbosity=4
 #--verbosity=4 > $NOSE_LOG_PATH 2>&1
+
+coverage html -d "${COVERAGE_PATH}/html"
+coverage xml -o "${COVERAGE_PATH}/nose_cover.xml"
 
 # rm .coverage
 
