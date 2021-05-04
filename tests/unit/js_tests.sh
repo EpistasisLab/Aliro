a685a2e25e385d43dee55ea305b0905ca2afd62e
Michael Stauffer <mgstauff@gmail.com>
2021-05-04 18:03:25 -0400

    Fix unit test runner and js_tests to exit with appropriate exit codes
    Needed for proper error handling in Github Actions
    NO Enzyme Snapshot for js_tests.sh

--- tests/unit/js_tests.sh
+++ tests/unit/js_tests.sh
@@ -7,10 +7,16 @@ export JEST_JUNIT_OUTPUT_DIR="${REPORT_PATH}"
 echo "starting '/lab' jest reports"
 cd "/appsrc/lab/"
 npm run test
-
+labres=$?
+echo "==== labres unit tests result "$labres
 
 echo "starting '/lab/webapp' jest reports"
 cd "/appsrc/lab/webapp/"
 npm run test
+webappres=$?
+echo "==== webappres unit tests result "$webappres
 
 cd "/appsrc"
+
+# exit with error code 1 if either test fails
+if [ $labres -eq 0 -a $webappres -eq 0 ]; then exit 0; else exit 1; fi
