c94b6865bfa5a7cbd7499d50218711343202a76a
Michael Stauffer <mgstauff@gmail.com>
2021-05-24 17:25:57 -0400

    Revert unit_test_runner.sh changes
    Remove the untested method of fine control via env vars. Move to
    separate branch to work on separately.

--- tests/unit/unit_test_runner.sh
+++ tests/unit/unit_test_runner.sh
@@ -4,22 +4,14 @@ dos2unix /appsrc/tests/unit/py_tests.sh
 dos2unix /appsrc/tests/unit/js_tests.sh
 
 # run via sh to avoid permissions problem
-if [ -z "${PENNAI_UNIT_TESTS_JS_ONLY}" ]; then
-  sh /appsrc/tests/unit/py_tests.sh
-  pyres=$?
-  # echo "==== py_tests unit tests result "$pyres
-else
-  echo "$0 - skipped py_tests"
-fi
+sh /appsrc/tests/unit/py_tests.sh
+pyres=$?
+# echo "==== py_tests unit tests result "$pyres
 
 # run via sh to avoid permissions problem
-if [ -z "${PENNAI_UNIT_TESTS_PY_ONLY}" ]; then
-  sh /appsrc/tests/unit/js_tests.sh
-  jsres=$?
-  # echo "==== js_tests unit tests result "$jsres
-else
-  echo "$0 - skipped js_tests"
-fi
+sh /appsrc/tests/unit/js_tests.sh
+jsres=$?
+# echo "==== js_tests unit tests result "$jsres
 
 # exit with code 1 if either test fails
 # If we don't exit with a non-zero value, github actions won't catch the error
