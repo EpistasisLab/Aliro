577e7c05af6c616ad1098d96b6a5f01db6cd6f33
jay-m-dev <jay-moran@outlook.com>
2022-12-08 19:04:55 +0000

    Deploying to gh-pages from  @ 3976a396d4bdd8666850b6c8100dfbf34f12a63b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'rpi-0.18',
+    VERSION: '0.18',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
