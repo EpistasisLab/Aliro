14c207180b7ee215863b7d6ec2958a84cc0de1d1
Jay Moran <jay-moran@outlook.com>
2023-01-12 13:15:38 -0800

    removed yml, .sh files and commented out .gitmodules

--- runIntTest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-docker system prune -a -f --volumes
-docker compose -f docker-compose-int-test.yml up --abort-on-container-exit -V
--- runUnitTest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-docker system prune -a -f --volumes
-docker compose -f docker-compose-unit-test.yml up --abort-on-container-exit -V
